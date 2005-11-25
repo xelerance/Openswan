3595947b7f4fadbe389a8808860f3bbdca057a02
Michael Richardson <mcr@xelerance.com>
2005-11-25 12:59:10 -0500

    rename west files to north

--- testing/pluto/l2tp-01/northinit.sh
+++ testing/pluto/l2tp-01/northinit.sh
@@ -1,5 +1,5 @@
 TESTNAME=l2tp-01
-source /testing/pluto/bin/westlocal.sh
+source /testing/pluto/bin/northlocal.sh
 
 if [ -f /var/run/l2tpd.pid ]; then kill `cat /var/run/l2tpd.pid`; fi
 
@@ -16,19 +16,18 @@ iptables -A OUTPUT -o eth1 -d 192.1.2.23 -p udp --dport 1701 -j REJECT
 
 # confirm with a ping to east-in
 ping -n -c 4 192.0.2.254
-telnet east-out 3 | wc -l
 
 ipsec setup restart
-ipsec auto --add west--east-l2tp
-ipsec auto --add west--east-pass
+ipsec auto --add north--east-l2tp
+ipsec auto --add north--east-pass
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --route west--east-pass
+ipsec auto --route north--east-pass
 ipsec whack --debug-control --debug-controlmore --debug-parsing --debug-crypt
 
 mount --bind /testing/pluto/l2tp-01 /etc/ppp
-l2tpd -c west.l2tpd.conf -D &
+l2tpd -c north.l2tpd.conf -D &
 
-ipsec auto --route west--east-l2tp
+ipsec auto --route north--east-l2tp
 
 echo done
 
