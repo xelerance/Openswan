9fc9a63b6ce93c508ff402c5522620d648f99d68
Ken Bantoft <ken@xelerance.com>
2005-11-26 18:08:36 -0500

    Use spi1-console26.txt as REF26_CONSOLE_OUTPUT

--- testing/klips/west-icmp-01/testparams.sh
+++ testing/klips/west-icmp-01/testparams.sh
@@ -12,7 +12,7 @@ REF_PRIV_OUTPUT=spi1-cleartext.txt
 # THREEEIGHT=true
 
 REF_CONSOLE_OUTPUT=spi1-console.txt
-REF26_CONSOLE_OUTPUT=spi1-console.txt
+REF26_CONSOLE_OUTPUT=spi1-console26.txt
 REF_CONSOLE_FIXUPS="kern-list-fixups.sed nocr.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS script-only.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS klips-spi-sanitize.sed"
