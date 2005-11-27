e50156db76bc6d3a4c5378d0dfd675e116534b15
Michael Richardson <mcr@xelerance.com>
2005-11-27 15:33:30 -0500

    better script to update openswan copy of kernel code

--- packaging/utils/kerngit2osw.sh
+++ packaging/utils/kerngit2osw.sh
@@ -3,7 +3,7 @@
 klips_git=/mara1/git/klips
 openswan_git=/mara6/openswan/public.git
 
-find $klips_git/net/ipsec $klips_git/include/openswan* $klips_git/include/pfkey* -type f | cpio -pd $openswan_git/linux
+(cd $klips_git && find net/ipsec include/openswan* include/pfkey* -type f | cpio -pd $openswan_git/linux )
 
 cd $openswan_git/linux/net/ipsec
 if [ -f Makefile ]; then mv Makefile Makefile.fs2_6; fi
