266a5a5a22c62d639750148e3975d8d9b6234464
Michael Richardson <mcr@xelerance.com>
2005-11-27 22:49:36 -0500

    make sure that iptables listing does not need DNS

--- testing/pluto/nat-pluto-01/nicinit.sh
+++ testing/pluto/nat-pluto-01/nicinit.sh
@@ -10,7 +10,7 @@ iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -j 
 iptables -I OUTPUT 1 --proto 50 -j DROP
 
 # Display the table, so we know it's correct.
-iptables -t nat -L
-iptables -L
+iptables -t nat -L -n
+iptables -L -n
 
 echo done.
