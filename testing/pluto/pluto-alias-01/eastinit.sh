40027908cfe80e4295066f2b16a0920fe1802dc0
Ken Bantoft <ken@xelerance.com>
2005-11-29 00:22:56 -0500

    Fix test name

--- testing/pluto/pluto-alias-01/eastinit.sh
+++ testing/pluto/pluto-alias-01/eastinit.sh
@@ -1,7 +1,8 @@
 : ==== start ====
-TESTNAME=pluto-aliases-01
+TESTNAME=pluto-alias-01
 source /testing/pluto/bin/eastlocal.sh
 ifconfig eth0:0 192.168.99.1
 ifconfig eth0:1 172.16.0.1
+ifconfig
 ipsec setup start
 netstat -ln
