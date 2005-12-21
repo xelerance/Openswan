0f34c639159acd2271ff363e69d6605ae0982b03
Michael Richardson <mcr@xelerance.com>
2005-12-20 23:09:34 -0500

    make l2tpd test cases more deterministic

--- testing/pluto/l2tp-01/northrun.sh
+++ testing/pluto/l2tp-01/northrun.sh
@@ -1,9 +1,9 @@
 ipsec auto --replace north--east-l2tp
 ipsec auto --up north--east-l2tp
 echo "c server" >/var/run/l2tp-control
-
-ipsec look
 sleep 5
+ipsec look
+ping -c 4 -n 192.0.2.254
 telnet 192.0.2.254 2 | wc -l
 ifconfig ppp0 | grep 'inet addr'
 echo done
