72a8a1e4ab1b2ba778a822bd7a17eec6a2e946f3
Michael Richardson <mcr@xelerance.com>
2005-12-27 23:18:11 -0500

    update KLIPS test cases for new saref code

--- testing/klips/east-saref-pfkey-01/spi1.sh
+++ testing/klips/east-saref-pfkey-01/spi1.sh
@@ -8,20 +8,8 @@ ipsec eroute --clear
 enckey=0x4043434545464649494a4a4c4c4f4f515152525454575758
 authkey=0x87658765876587658765876587658765
 
-#ipsec klipsdebug --set pfkey
-
 ipsec spi --saref --af inet --edst 192.1.2.45 --spi 0x12345678 --proto esp --src 192.1.2.23 --esp 3des-md5-96 --enckey $enckey --authkey $authkey
 
 ipsec spi --saref --af inet --edst 192.1.2.45 --spi 0x12345678 --proto tun --src 192.1.2.23 --dst 192.1.2.45 --ip4
 
-#ipsec spigrp inet 192.1.2.45 0x12345678 tun inet 192.1.2.45 0x12345678 esp 
-
-#ipsec eroute --add --eraf inet --src 192.0.2.0/24 --dst 192.0.1.0/24 --said tun0x12345678@192.1.2.45
-
-
-#ipsec look
-
-# monitor upbound reply messages
-#ipsec pf_key --debug --daemon /var/run/pluto/pf_key.pid >/tmp/pfkey.txt
-echo start now
 
--- testing/klips/saref-alloc-01/testparams.sh
+++ testing/klips/saref-alloc-01/testparams.sh
@@ -7,7 +7,7 @@ TEST_TYPE=ctltest
 TESTNAME=saref-alloc-01
 TESTHOST=east
 
-REFCONSOLEOUTPUT=saref-console.txt
+REF_CONSOLE_OUTPUT=saref-console.txt
 REF_CONSOLE_FIXUPS="kern-list-fixups.sed nocr.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS klips-spi-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ipsec-look-sanitize.sed"
@@ -20,6 +20,7 @@ INIT_SCRIPT=alloctest3.sh
 # do not store this in CVS, but generate it the first time.
 if [ ! -f alloctest2.sh ]
 then
+    pwd
     perl genallocfile.pl >alloctest2.sh
 fi
 
