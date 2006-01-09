cbd5f33d873741c2f74f8bee8dc0b128a2080015
Michael Richardson <mcr@xelerance.com>
2006-01-09 15:00:13 -0500

    update to klips-git -> openswan conversion script

--- packaging/utils/kerngitpull.sh
+++ packaging/utils/kerngitpull.sh
@@ -6,6 +6,8 @@ patchdir=$openswan_git/linux/patches
 
 # generate set of patches against last time we did this, and apply
 # the patches to the linux/ subtree.
+rm -rf $patchdir
+
 cd $klips_git
 
 mkdir -p $patchdir
@@ -27,16 +29,17 @@ do
 	i="$1"
 	git-mailinfo -k .dotest/msg .dotest/patch <$i >.dotest/info
 	git-stripspace <.dotest/msg >.dotest/msg-clean
-	sed -e 's|^\+\+\+ b/net|+++ b/linux/net|' -e 's|^--- a/net|--- a/linux/net|' <.dotest/patch >.dotest/patch-osw
+	sed -e 's|^\+\+\+ b/|+++ b/linux/|' -e 's|^--- a/|--- a/linux/|' <.dotest/patch >.dotest/patch-osw
 	cat .dotest/patch-osw
+	read ans
 	(cd $openswan_git && git-applypatch linux/.dotest/msg-clean linux/.dotest/patch-osw linux/.dotest/info "$SIGNOFF" )
 	shift
     done
 done
 
-cd $klips_git
-cg-switch openswan_klips
-git pull . klipsNG
-cg-switch klipsNG
+#cd $klips_git
+#cg-switch openswan_klips
+#git pull . klipsNG
+#cg-switch klipsNG
 
 
