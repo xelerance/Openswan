924b2216a8570fedf0a43501a9ee2931957eaf9b
Michael Richardson <mcr@xelerance.com>
2006-01-14 11:49:53 -0500

    added test case for aggressive mode + certificate transmission

---
+++ testing/pluto/aggr-pluto-03/eastinit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+: ==== start ====
+
+TESTNAME=aggr-pluto-03
+source /testing/pluto/bin/eastlocal.sh
+
+rm /tmp/$TESTNAME/ipsec.d/crls/nic.crl
+
+iptables -A INPUT -i eth1 -s 192.0.3.0/24 -d 0.0.0.0/0 -j DROP
+
+arp -s 192.0.2.1 10:00:00:dc:bc:01
+
+ipsec setup start
+/testing/pluto/bin/wait-until-pluto-started
+ipsec auto --add north-east-aggr-pluto-03
+
+echo done
+
+
---
+++ testing/pluto/aggr-pluto-03/northrun.sh
@@ -0,0 +1,3 @@
+ipsec auto --up north-east-aggr-pluto-03
+echo done
+
