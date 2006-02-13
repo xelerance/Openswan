fc52bb8b068a83296594fa6a5d2fa56a0aa02c37
Michael Richardson <mcr@xelerance.com>
2006-02-13 16:54:05 -0500

    test case now completed for situation where we manually key a transport mode SA, and use options to showpolicy to return a received packet on a particular SA, based upon the SA on which it arrived

--- testing/klips/west-mast-03/testparams.sh
+++ testing/klips/west-mast-03/testparams.sh
@@ -6,12 +6,12 @@ TESTHOST=west
 EXITONEMPTY=--exitonempty
 
 PRIVATE_ARPREPLY=true
-PUB_INPUT=../inputs/02-sunrise-sunset-esp.pcap
-REF_PRIV_OUTPUT=spi1-cleartext.txt
+PUB_INPUT=../inputs/02-east-west-transport.pcap
+REF_PUB_OUTPUT=west-transport_reply.txt
 # THREEEIGHT=true
 
-REF_CONSOLE_OUTPUT=mast1-console.txt
-REF26_CONSOLE_OUTPUT=mast1-console.txt
+REF_CONSOLE_OUTPUT=mast3-console.txt
+REF26_CONSOLE_OUTPUT=mast3-console.txt
 
 REF_CONSOLE_FIXUPS="kern-list-fixups.sed nocr.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS script-only.sed"
@@ -20,8 +20,8 @@ REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ipsec-look-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS east-prompt-splitline.pl"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS klips-debug-sanitize.sed"
 
-INIT_SCRIPT=mast1in.sh
-RUN_SCRIPT=mast1run.sh
+INIT_SCRIPT=mast3in.sh
+RUN_SCRIPT=mast3run.sh
 
 #NETJIGDEBUG=true
 
