82f13b11e45ae0fcf663fabd168dbf09fb63d29e
Michael Richardson <mcr@xelerance.com>
2006-02-22 12:46:21 -0500

    added ref/refhim members to kernel sa structure adjust add_sa to have non-const kernel_sa adjust "op" parameter to use enum

--- programs/pluto/kernel.h
+++ programs/pluto/kernel.h
@@ -80,6 +80,9 @@ struct kernel_sa {
 	unsigned enckeylen;
 	unsigned char *enckey;
 
+	IPsecSAref_t ref;
+	IPsecSAref_t refhim;
+
 	int encapsulation;
 #ifdef NAT_TRAVERSAL
 	u_int16_t natt_sport, natt_dport;
@@ -132,12 +135,12 @@ struct kernel_ops {
 			 , enum pluto_sadb_operations op
 			 , const char *opname);
     bool (*sag_eroute)(struct state *st, struct spd_route *sr
-		       , unsigned op, const char *opname);
+		       , enum pluto_sadb_operations op, const char *opname);
     bool (*eroute_idle)(struct state *st, time_t idle_max);
     void (*remove_orphaned_holds)(int transportproto
 				  , const ip_subnet *ours
 				  , const ip_subnet *his);
-    bool (*add_sa)(const struct kernel_sa *sa, bool replace);
+    bool (*add_sa)(struct kernel_sa *sa, bool replace);
     bool (*grp_sa)(const struct kernel_sa *sa_outer,
 		   const struct kernel_sa *sa_inner);
     bool (*del_sa)(const struct kernel_sa *sa);
