7157cb855206caf000780b5ce3edbf79b8887da9
Michael Richardson <mcr@xelerance.com>
2006-04-05 22:30:12 -0400

    add warning about 1DES

--- programs/pluto/ike_alg.c
+++ programs/pluto/ike_alg.c
@@ -466,9 +466,11 @@ bool ike_alg_ok_final(int ealg, unsigned key_len, int aalg, int group, struct al
 						 (ike_info->ike_eklen==key_len)) &&
 						(ike_info->ike_halg == aalg) &&
 						(ike_info->ike_modp == group)) {
+#ifndef USE_1DES
 					if (ealg_insecure) 
 						loglog(RC_LOG_SERIOUS, "You should NOT use insecure IKE algorithms (%s)!"
 								, enum_name(&oakley_enc_names, ealg));
+#endif
 					return TRUE;
 				}
 			}
