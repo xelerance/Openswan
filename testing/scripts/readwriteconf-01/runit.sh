d5b8d10995181771dd578d5aa647686ac3208afe
Michael Richardson <mcr@xelerance.com>
2006-07-29 21:08:41 -0400

    Converted readwriteconf-01 to unittest
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/scripts/readwriteconf-01/runit.sh
+++ testing/scripts/readwriteconf-01/runit.sh
@@ -1,22 +1,12 @@
 #!/bin/sh
 
-MAKE=${MAKE-make}
+# assumes that 
+#          ROOTDIR=    set to root of source code.
+#          OBJDIRTOP=  set to location of object files
+#
 
-rootdir=`cd ../../..; pwd`
-mkdir -p OUTPUT
 echo "file $rootdir/OBJ.linux.i386/programs/readwriteconf/readwriteconf" >.gdbinit
 echo "set args --rootdir=$rootdir/testing/baseconfigs/all --config $rootdir/testing/baseconfigs/west/etc/ipsec.conf >OUTPUT/west-flat.conf-out" >>.gdbinit
 
-(cd $rootdir/programs/readwriteconf && ${MAKE} programs )
+${OBJDIRTOP}/programs/readwriteconf/readwriteconf --rootdir=${ROOTDIR}/testing/baseconfigs/all --config ${ROOTDIR}/testing/baseconfigs/west/etc/ipsec.conf 
 
-$rootdir/OBJ.linux.i386/programs/readwriteconf/readwriteconf --rootdir=$rootdir/testing/baseconfigs/all --config $rootdir/testing/baseconfigs/west/etc/ipsec.conf >OUTPUT/west-flat.conf-out
-
-sed -f ../fixups/confwritesanity.sed OUTPUT/west-flat.conf-out >OUTPUT/west-flat.fixed
-
-if diff -u west-flat.conf OUTPUT/west-flat.fixed >OUTPUT/west-flat.diff
-then
-	echo Success
-else
-	echo Failed
-	exit 30
-fi
