cd529f9b9a32324f8f4d6217f6cfb741ca1b3614
Michael Richardson <mcr@xelerance.com>
2006-08-10 10:14:37 -0400

    fixed test case
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/klips/west-ah-icmp-01/testparams.sh
+++ testing/klips/west-ah-icmp-01/testparams.sh
@@ -4,12 +4,14 @@ TEST_TYPE=klipstest
 TESTNAME=west-ah-icmp-01
 TESTHOST=west
 EXITONEMPTY=--exitonempty
-ARPREPLY=--arpreply 
+
+PRIVATE_ARPREPLY=--arpreply 
 
 PUB_INPUT=../inputs/08-sunrise-sunset-ah-md5.pcap
 REF_PRIV_OUTPUT=ah1-cleartext.txt
 REF_CONSOLE_OUTPUT=ah1-console.txt
 REF_CONSOLE_FIXUPS="kern-list-fixups.sed nocr.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS script-only.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS klips-spi-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ipsec-look-sanitize.sed"
 REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS east-prompt-splitline.pl"
