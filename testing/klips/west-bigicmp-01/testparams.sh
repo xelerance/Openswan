b93344c0940671755eb4a24ceaa38091663f00f2
Michael Richardson <mcr@xelerance.com>
2006-08-15 16:29:16 -0400

    added west-bigicmp-01 to list
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/klips/west-bigicmp-01/testparams.sh
+++ testing/klips/west-bigicmp-01/testparams.sh
@@ -11,10 +11,12 @@ ARPREPLY=--arpreply
 PUB_INPUT=../inputs/espfrags1.pcap
 REF_PRIV_OUTPUT=spi1-cleartext.txt
 REF_CONSOLE_OUTPUT=spi1-console.txt
+REF26_CONSOLE_OUTPUT=spi1-console.txt
 REF_CONSOLE_FIXUPS="kern-list-fixups.sed nocr.sed"
-REF_CONSOLE_FIXUPS="$REFCONSOLEFIXUPS klips-spi-sanitize.sed"
-REF_CONSOLE_FIXUPS="$REFCONSOLEFIXUPS klips-debug-sanitize.sed"
-REF_CONSOLE_FIXUPS="$REFCONSOLEFIXUPS ipsec-look-sanitize.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS script-only.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS klips-spi-sanitize.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS klips-debug-sanitize.sed"
+REF_CONSOLE_FIXUPS="$REF_CONSOLE_FIXUPS ipsec-look-sanitize.sed"
 TCPDUMPFLAGS="-n -E 3des-cbc-hmac96:0x4043434545464649494a4a4c4c4f4f515152525454575758 -v"
 INIT_SCRIPT=spi1-in.sh
 
