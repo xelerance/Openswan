d842509527614eaaa6a636348fc60321ec03ba29
Michael Richardson <mcr@xelerance.com>
2006-08-30 12:53:10 -0400

    final fixes for X.509 basic connection to work, includes timing fixes
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/pluto/basic-pluto-06/eastinit.sh
+++ testing/pluto/basic-pluto-06/eastinit.sh
@@ -3,6 +3,6 @@
 iptables -A INPUT -i eth1 -s 192.0.3.0/24 -d 0.0.0.0/0 -j DROP
 
 ipsec setup start
+/testing/pluto/basic-pluto-01/eroutewait.sh trap
 ipsec whack --debug-all
 ipsec auto --add westnet-eastnet-x509
-/testing/pluto/basic-pluto-01/eroutewait.sh trap
--- testing/pluto/basic-pluto-06/westinit.sh
+++ testing/pluto/basic-pluto-06/westinit.sh
@@ -7,9 +7,9 @@ iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 ping -n -c 4 192.0.2.254
 
 ipsec setup start
+/testing/pluto/basic-pluto-01/eroutewait.sh trap
 ipsec whack --debug-all
 ipsec auto --add westnet-eastnet-x509
-/testing/pluto/basic-pluto-01/eroutewait.sh trap
 ipsec auto --up  westnet-eastnet-x509
 
 echo done
