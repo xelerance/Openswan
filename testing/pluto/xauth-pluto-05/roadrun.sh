525743c36c2c1bb2bda47620bc5ca76fdf573a5c
Michael Richardson <mcr@xelerance.com>
2006-11-25 17:01:39 -0500

    updated test case for modecfg operation.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/pluto/xauth-pluto-05/roadrun.sh
+++ testing/pluto/xauth-pluto-05/roadrun.sh
@@ -1,4 +1,5 @@
 ipsec auto --replace modecfg-road--eastnet-psk
+ipsec whack --status | grep modecfg-road--eastnet-psk
 ipsec whack --xauthname 'use3' --xauthpass 'use1pass' --name modecfg-road--eastnet-psk --initiate 
 
 echo done.
