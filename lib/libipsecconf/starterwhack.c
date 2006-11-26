544da231b03ca764d07441c09f98ba49ea64e736
Michael Richardson <mcr@xelerance.com>
2006-11-25 20:19:12 -0500

    turn on DPD if both dpddelay and dpdtimeout is set, and default dpdaction properly
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- lib/libipsecconf/starterwhack.c
+++ lib/libipsecconf/starterwhack.c
@@ -433,14 +433,28 @@ int starter_whack_add_conn (struct starter_config *cfg, struct starter_conn *con
 
 	msg.policy = conn->policy;
 
-	if(conn->options_set[KBF_DPDACTION]) {
-		msg.dpd_action = conn->options[KBF_DPDACTION];
+	if(conn->options_set[KBF_DPDDELAY] &&
+	   conn->options_set[KBF_DPDTIMEOUT]) {
+		msg.dpd_delay   = conn->options[KBF_DPDDELAY];
+		msg.dpd_timeout = conn->options[KBF_DPDTIMEOUT];
 
-		if(conn->options_set[KBF_DPDDELAY]) {
-			msg.dpd_delay  = conn->options[KBF_DPDDELAY];
+		if(conn->options_set[KBF_DPDACTION]) {
+			msg.dpd_action = conn->options[KBF_DPDACTION];
+		} else {
+			/*
+			 * there is a default DPD action, but DPD is only
+			 * enabled if there is a dpd delay set.
+			 */
+			msg.dpd_action = DPD_ACTION_HOLD;
 		}
-		if(conn->options_set[KBF_DPDTIMEOUT]) {
-			msg.dpd_timeout  = conn->options[KBF_DPDTIMEOUT];
+
+	} else {
+		if(conn->options_set[KBF_DPDDELAY]  ||
+		   conn->options_set[KBF_DPDTIMEOUT]||
+		   conn->options_set[KBF_DPDACTION])
+		{
+			starter_log(LOG_LEVEL_ERR, "conn: \"%s\" warning dpd settings are not ignored unless dpdtimeout= and dpddelay= are set"
+				    , conn->name);
 		}
 	}
 
