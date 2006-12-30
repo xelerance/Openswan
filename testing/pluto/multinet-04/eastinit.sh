059f605cdadb0661ae3439b6ab11cc00ffda8324
Michael Richardson <mcr@xelerance.com>
2006-12-30 12:28:38 -0500

    updated test case to do as documented. Seems to work.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/pluto/multinet-04/eastinit.sh
+++ testing/pluto/multinet-04/eastinit.sh
@@ -3,6 +3,6 @@ TESTNAME=multinet-04
 source /testing/pluto/bin/eastlocal.sh
 
 ipsec setup start
-ipsec auto --add westnet-eastnet-subnets
+ipsec auto --add westnets-eastnet
 /testing/pluto/bin/wait-until-pluto-started
 
