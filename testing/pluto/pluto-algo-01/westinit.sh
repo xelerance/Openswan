de3f003f44a445819242c148898e4c6d17922a32
Michael Richardson <mcr@xelerance.com>
2007-04-28 08:29:12 -0400

    updated test case --- reviewed items and confirm it is correct.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/pluto/pluto-algo-01/westinit.sh
+++ testing/pluto/pluto-algo-01/westinit.sh
@@ -4,7 +4,7 @@ source /testing/pluto/bin/westlocal.sh
 
 ipsec setup start
 ipsec auto --add westnet-eastnet-cross
-/testing/pluto/basic-pluto-01/eroutewait.sh trap
+/testing/pluto/bin/wait-until-pluto-started
 ipsec auto --up  westnet-eastnet-cross
 
 echo done
