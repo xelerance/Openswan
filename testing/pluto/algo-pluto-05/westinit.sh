e6313582d97228046b91dde94ba921b5d6f07a1e
Michael Richardson <mcr@xelerance.com>
2007-04-30 01:12:44 -0400

    do not load any conn during init.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/pluto/algo-pluto-05/westinit.sh
+++ testing/pluto/algo-pluto-05/westinit.sh
@@ -3,7 +3,6 @@ TESTNAME=algo-pluto-05
 source /testing/pluto/bin/westlocal.sh
 
 ipsec setup start
-ipsec auto --add westnet-eastnet-aes256
 /testing/pluto/bin/wait-until-pluto-started 
 
 echo done
