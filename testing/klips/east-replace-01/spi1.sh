63f2971aee73e52fe20b6f88aeb023b3be673201
Michael Richardson <mcr@xelerance.com>
2007-07-24 14:16:12 -0500

    test case (working) for ipsec eroute --replace.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/klips/east-replace-01/spi1.sh
+++ testing/klips/east-replace-01/spi1.sh
@@ -17,7 +17,7 @@ ipsec spigrp inet 192.1.2.45 0x12345678 tun inet 192.1.2.45 0x12345678 esp
 
 # make second SA. 
 ipsec spi --saref --af inet --edst 192.1.2.44 --spi 0x23456789 --proto esp --src 192.1.2.23 --esp 3des-md5-96 --enckey $enckey --authkey $authkey
-ipsec spi --saref --af inet --edst 192.1.2.45 --spi 0x23456789 --proto tun --src 192.1.2.23 --dst 192.1.2.44 --ip4
+ipsec spi --saref --af inet --edst 192.1.2.44 --spi 0x23456789 --proto tun --src 192.1.2.23 --dst 192.1.2.44 --ip4
 ipsec spigrp inet 192.1.2.44 0x23456789 tun inet 192.1.2.44 0x23456789 esp 
 
 
@@ -34,6 +34,7 @@ ipsec eroute
 ipsec eroute --add --eraf inet --src 192.0.2.0/24 --dst 192.0.1.0/24 --said tun0x12345678@192.1.2.45
 : added
 ipsec eroute
+
 
 ipsec eroute --replace --eraf inet --src 192.0.2.0/24 --dst 192.0.1.0/24 --said tun0x23456789@192.1.2.44
 : replaced
