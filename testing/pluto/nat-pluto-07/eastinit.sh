c2c99d307434c521e4ccd01e92fe0db9ad2c6f7a
Michael Richardson <mcr@xelerance.com>
2007-07-26 01:03:06 -0500

    test case for nat-traversal with IP address changes mid-way. However, NIC machine does not NAT the data to a new IP properly.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/pluto/nat-pluto-07/eastinit.sh
+++ testing/pluto/nat-pluto-07/eastinit.sh
@@ -6,6 +6,8 @@ TESTNAME=nat-pluto-07
 source /testing/pluto/bin/eastlocal.sh
 
 arp -s 192.0.2.1 10:00:00:dc:bc:01
+route delete -net default 
+route add -net default gw 192.1.2.1
 
 ipsec setup start
 ipsec auto --add northnet--eastnet-nat
--- testing/pluto/nat-pluto-07/nicinit.sh
+++ testing/pluto/nat-pluto-07/nicinit.sh
@@ -4,10 +4,12 @@ ifconfig eth0:1 inet 192.1.2.245 netmask 255.255.255.0
 
 # NAT North's IP to ours
 iptables -t nat -F POSTROUTING 
-iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -p udp -m udp --dport 500 -j SNAT --to-source 192.1.2.254:11000-12000
-iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -p udp -m udp --dport 4500 -j SNAT --to-source 192.1.2.245:14000-16000
+iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -o eth0+ -p udp -m udp --dport 500  -j SNAT --to-source 192.1.2.254:11000-12000
+iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -o eth0+ -p udp -m udp --dport 4500 -j SNAT --to-source 192.1.2.111:14000-16000
+
+iptables -A OUTPUT -d 192.0.2.0/24 -j DROP
 
 # Display the table, so we know it's correct.
-iptables -t nat -L
+iptables -t nat -L -v
 
 echo done.
