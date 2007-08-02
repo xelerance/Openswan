8d24a341a505ead9a6adbc60575f6f066e700533
Paul Wouters <paul@xelerance.com>
2007-08-02 17:28:11 -0400

    CTL_TABLE_PARENT patch for 2.6.21+ by sergeil

--- linux/include/openswan/ipsec_kversion.h
+++ linux/include/openswan/ipsec_kversion.h
@@ -153,6 +153,13 @@
 /* skb->nfmark changed to skb->mark in 2.6.20 */
 #define nfmark mark
 #endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,21)
+#define ipsec_register_sysctl_table(a,b) register_sysctl_table(a)
+#define CTL_TABLE_PARENT
+#else
+#define ipsec_register_sysctl_table(a,b) register_sysctl_table(a,b)
+#endif
  
 #if __KERNEL__
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,0)
