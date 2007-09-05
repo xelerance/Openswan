39e46a5055f11485f15d1e4b99ed7bb4fe51d5f1
Michael Richardson <mcr@xelerance.com>
2007-09-05 00:36:07 -0400

    remove eroute-wait, since these tests do not assume OE is on anymore.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/pluto/aggr-pluto-01/eastinit.sh
+++ testing/pluto/aggr-pluto-01/eastinit.sh
@@ -7,5 +7,5 @@ TESTNAME=aggr-pluto-01
 source /testing/pluto/bin/eastlocal.sh
 
 ipsec setup start
-/testing/pluto/basic-pluto-01/eroutewait.sh trap
+/testing/pluto/bin/wait-until-pluto-started
 ipsec auto --add westnet-eastnet-aggr
