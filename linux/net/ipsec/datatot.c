51a5192e42d21100800e714265e18d30d4a61fac
Paul Wouters <paul@xelerance.com>
2007-10-04 23:07:57 -0400

    Missing assert.h (gave issues on OSX)

--- linux/net/ipsec/datatot.c
+++ linux/net/ipsec/datatot.c
@@ -15,6 +15,7 @@
  * RCSID $Id: datatot.c,v 1.9 2005/08/30 21:15:26 mcr Exp $
  */
 #include "openswan.h"
+#include "assert.h"
 
 static void convert(const char *src, size_t nreal, int format, char *out);
 
