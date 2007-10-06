dc849c7914cae47b0facd3b3a92b4540544a1e95
Paul Wouters <paul@xelerance.com>
2007-10-06 00:57:55 -0400

    Fix references from nic.crl to ca.crl. Fix openssl commands used in der format conversion.

--- testing/pluto/x509-pluto-03/eastinit.sh
+++ testing/pluto/x509-pluto-03/eastinit.sh
@@ -3,7 +3,7 @@ TESTNAME=x509-pluto-03
 source /testing/pluto/bin/eastlocal.sh
 
 rm /tmp/$TESTNAME/ipsec.d/certs/west*
-rm /tmp/$TESTNAME/ipsec.d/crls/nic.crl
+rm /tmp/$TESTNAME/ipsec.d/crls/ca.crl
 
 iptables -A INPUT -i eth1 -s 192.0.3.0/24 -d 0.0.0.0/0 -j DROP
 
