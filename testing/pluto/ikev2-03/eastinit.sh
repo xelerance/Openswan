e9c2b282684fc3c4ac31577539d6424057ab4ce9
Michael Richardson <mcr@xelerance.com>
2007-10-25 23:48:21 -0400

    #864 - create whackrecord for responding side.

--- testing/pluto/ikev2-03/eastinit.sh
+++ testing/pluto/ikev2-03/eastinit.sh
@@ -3,6 +3,7 @@ TESTNAME=ikev2-01
 source /testing/pluto/bin/eastlocal.sh
 
 ipsec setup start
+ipsec whack --whackrecord /var/tmp/ikev2.record
 ipsec auto --add  westnet--eastnet-ikev2
 ipsec whack --debug-control --debug-controlmore --debug-crypt
 /testing/pluto/bin/wait-until-pluto-started
