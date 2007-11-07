f2215f54153b22e74853dfb115aec19095e32e49
Ken Bantoft <ken@xelerance.com>
2007-11-07 07:37:59 -0500

    Fix NAT-T consts

--- programs/pluto/vendor.h
+++ programs/pluto/vendor.h
@@ -75,8 +75,8 @@ enum known_vendorid {
   VID_NATT_IETF_02           =107,
   VID_NATT_IETF_03           =108,
   VID_NATT_RFC               =109,               
-  VID_NATT_DRAFT_IETF_IPSEC_NAT_T_IKE,
-  VID_NATT_IETF_05,
+  VID_NATT_DRAFT_IETF_IPSEC_NAT_T_IKE =110,
+  VID_NATT_IETF_05           =111,
 
 /* 
   While searching (strings) in /usr/sbin/racoon on Max OS X 10.3.3, I found it :
