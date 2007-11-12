4b8e58d7b2f1641d37214cd651d34ccebade2a58
Michael Richardson <mcr@xelerance.com>
2007-11-11 21:59:16 -0500

    #845 - unit test case for SKEYSEED calculation.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

---
+++ testing/crypto/v2-dh-06/tc2.c
@@ -0,0 +1,72 @@
+/*
+ * while the rest of this file is covered under the GPL, the following
+ * constant values, being inputs and outputs of a mathematical formula
+ * are hereby placed in the public domain, including the expression of them
+ * in the form of this C code.
+ *
+ * I.e. please rip off my test data so that the world will be a better place.
+ *
+ */
+
+struct encrypt_desc *tc2_encrypter = &crypto_encrypter_3des;
+#include "../../lib/libpluto/seam_gi.c"
+
+static void perform_t2_test(void)
+{
+	struct pluto_crypto_req r;
+	struct pcr_skeyid_r *skr = &r.pcr_d.dhr;
+	struct pcr_skeyid_q *skq = &r.pcr_d.dhq;
+
+	skq->thespace.start = 0;
+	skq->thespace.len   = sizeof(skq->space);
+	skq->auth = tc2_auth;
+	skq->hash = tc2_hash;
+	skq->oakley_group = tc2_oakleygroup;
+	skq->init = tc2_init;
+	skq->keysize = tc2_encrypter->keydeflen/BITS_PER_BYTE;
+
+#define copydatlen(field, data, len) do { \
+		chunk_t tchunk;           \
+		setchunk(tchunk, data, len); \
+		pluto_crypto_copychunk(&skq->thespace, skq->space \
+				       , &skq->field, tchunk); }   \
+		while(0)
+
+	copydatlen(ni, tc2_ni, tc2_ni_len);
+	copydatlen(nr, tc2_nr, tc2_nr_len);
+	copydatlen(gi, tc2_gi, tc2_gi_len);
+	copydatlen(gr, tc2_gr, tc2_gr_len);
+	copydatlen(secret,  tc2_secret,  tc2_secret_len);
+	copydatlen(icookie, tc2_icookie, tc2_icookie_len);
+	copydatlen(rcookie, tc2_rcookie, tc2_rcookie_len);
+
+#define dumpdat(field) \
+	openswan_DBG_dump(#field,	\
+			  wire_chunk_ptr(skq, &skq->field), \
+			  skq->field.len);
+
+	dumpdat(icookie);
+	dumpdat(rcookie);
+	dumpdat(ni);
+	dumpdat(nr);
+	dumpdat(gi);
+	dumpdat(gr);
+	dumpdat(secret);
+
+	fflush(stdout);
+	fflush(stderr);
+	
+	calc_dh_v2(&r);
+
+	printf("\noutput:\n");
+
+	fflush(stdout);
+	fflush(stderr);
+
+	{
+		void *shared = wire_chunk_ptr(skr, &skr->shared);
+
+		openswan_DBG_dump("shared", shared, skr->shared.len);
+	}
+	
+}
