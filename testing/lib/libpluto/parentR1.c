a0db14bda9d1fdd0c94693fb8c25891b60d8eba8
Michael Richardson <mcr@xelerance.com>
2007-11-14 16:32:48 -0500

    #835 - refactored out recv_pcap_packet() from parentR1.c
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/lib/libpluto/parentR1.c
+++ testing/lib/libpluto/parentR1.c
@@ -35,79 +35,16 @@
 #include "seam_natt.c"
 #include "seam_gi.c"
 
-u_int8_t reply_buffer[MAX_OUTPUT_UDP_SIZE];
-bool nat_traversal_support_non_ike = FALSE;
-bool nat_traversal_support_port_floating = FALSE;
+#include "seam_commhandle.c"
 
 void recv_pcap_packet(u_char *user
 		      , const struct pcap_pkthdr *h
 		      , const u_char *bytes)
 {
-    struct pcr_kenonce *kn = &r->pcr_d.kn;
-    struct msg_digest *md;
-    u_int32_t *dlt;
-    struct iphdr  *ip;
-    struct udphdr *udp;
-    u_char    *ike;
     struct state *st;
-    const struct iface_port *ifp = &if1;
-    int packet_len;
-    err_t from_ugh;
-    union
-    {
-	struct sockaddr sa;
-	struct sockaddr_in sa_in4;
-	struct sockaddr_in6 sa_in6;
-    } from;
+    struct pcr_kenonce *kn = &r->pcr_d.kn;
 
-    md = alloc_md();
-    dlt = (u_int32_t *)bytes;
-    if(*dlt != PF_INET) return;
-
-    ip  = (struct iphdr *)(dlt + 1);
-    udp = (struct udphdr *)(dlt + ip->ihl + 1);
-    ike = (u_char *)(udp+1);
-
-    from.sa_in4.sin_addr.s_addr = ip->saddr;
-    from.sa_in4.sin_port        = udp->source;
-
-    md->iface = ifp;
-    packet_len = h->len - (ike-bytes);
-
-    happy(anyaddr(addrtypeof(&ifp->ip_addr), &md->sender));
-
-    from_ugh = initaddr((void *) &from.sa_in4.sin_addr
-			, sizeof(from.sa_in4.sin_addr)
-			, AF_INET, &md->sender);
-    setportof(from.sa_in4.sin_port, &md->sender);
-    md->sender_port = ntohs(from.sa_in4.sin_port);
-
-    cur_from      = &md->sender;
-    cur_from_port = md->sender_port;
-
-    /* Clone actual message contents
-     * and set up md->packet_pbs to describe it.
-     */
-    init_pbs(&md->packet_pbs
-	     , clone_bytes(ike, packet_len, "message buffer in comm_handle()")
-	     , packet_len, "packet");
-
-    DBG_log("*received %d bytes from %s:%u on %s (port=%d)"
-	    , (int) pbs_room(&md->packet_pbs)
-	    , ip_str(&md->sender), (unsigned) md->sender_port
-	    , ifp->ip_dev->id_rname
-	    , ifp->port);
-
-    DBG_dump("", md->packet_pbs.start, pbs_room(&md->packet_pbs));
-
-    process_packet(&md);
-
-    if (md != NULL)
-	release_md(md);
-
-    cur_state = NULL;
-    reset_cur_connection();
-    cur_from = NULL;
+    recv_pcap_packet_gen(user, h, bytes);
 
     /* find st involved */
     st = state_with_serialno(1);
---
+++ testing/lib/libpluto/seam_commhandle.c
@@ -0,0 +1,76 @@
+u_int8_t reply_buffer[MAX_OUTPUT_UDP_SIZE];
+bool nat_traversal_support_non_ike = FALSE;
+bool nat_traversal_support_port_floating = FALSE;
+
+void recv_pcap_packet_gen(u_char *user
+			  , const struct pcap_pkthdr *h
+			  , const u_char *bytes)
+{
+    struct msg_digest *md;
+    u_int32_t *dlt;
+    struct iphdr  *ip;
+    struct udphdr *udp;
+    u_char    *ike;
+    const struct iface_port *ifp = &if1;
+    int packet_len;
+    err_t from_ugh;
+    union
+    {
+	struct sockaddr sa;
+	struct sockaddr_in sa_in4;
+	struct sockaddr_in6 sa_in6;
+    } from;
+
+    md = alloc_md();
+    dlt = (u_int32_t *)bytes;
+    if(*dlt != PF_INET) return;
+
+    ip  = (struct iphdr *)(dlt + 1);
+    udp = (struct udphdr *)(dlt + ip->ihl + 1);
+    ike = (u_char *)(udp+1);
+
+    from.sa_in4.sin_addr.s_addr = ip->saddr;
+    from.sa_in4.sin_port        = udp->source;
+
+    md->iface = ifp;
+    packet_len = h->len - (ike-bytes);
+
+    happy(anyaddr(addrtypeof(&ifp->ip_addr), &md->sender));
+
+    from_ugh = initaddr((void *) &from.sa_in4.sin_addr
+			, sizeof(from.sa_in4.sin_addr)
+			, AF_INET, &md->sender);
+    setportof(from.sa_in4.sin_port, &md->sender);
+    md->sender_port = ntohs(from.sa_in4.sin_port);
+
+    cur_from      = &md->sender;
+    cur_from_port = md->sender_port;
+
+    /* Clone actual message contents
+     * and set up md->packet_pbs to describe it.
+     */
+    init_pbs(&md->packet_pbs
+	     , clone_bytes(ike, packet_len, "message buffer in comm_handle()")
+	     , packet_len, "packet");
+
+    DBG_log("*received %d bytes from %s:%u on %s (port=%d)"
+	    , (int) pbs_room(&md->packet_pbs)
+	    , ip_str(&md->sender), (unsigned) md->sender_port
+	    , ifp->ip_dev->id_rname
+	    , ifp->port);
+
+    DBG_dump("", md->packet_pbs.start, pbs_room(&md->packet_pbs));
+
+    process_packet(&md);
+
+    if (md != NULL)
+	release_md(md);
+
+    cur_state = NULL;
+    reset_cur_connection();
+    cur_from = NULL;
+}
+
+
+
+
