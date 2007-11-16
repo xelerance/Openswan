9ffe58229fdd8183fcaa0cd669c647696fd235ef
Ken Bantoft <ken@xelerance.com>
2007-11-16 05:48:53 -0500

    Use fixup for RSA keyid: lines

--- testing/scripts/showhostkey-01/testparams.sh
+++ testing/scripts/showhostkey-01/testparams.sh
@@ -14,6 +14,7 @@ REF26_EAST_CONSOLE_OUTPUT=keys-console.txt
 
 REF_CONSOLE_FIXUPS="script-only.sed "
 REF_CONSOLE_FIXUPS+="east-prompt-splitline.pl"
+REF_CONSOLE_FIXUPS+="private-key-sanitize.sed"
 
 EASTHOST=east
 EAST_INIT_SCRIPT=keys.sh
