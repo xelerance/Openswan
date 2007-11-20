abfe3a0832c6199883bc15802d4457005fdefbc9
Michael Richardson <mcr@xelerance.com>
2007-11-20 08:06:33 -0500

    #835 - skeleton for parentR2 test case.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

---
+++ testing/lib/libpluto/lib-parentR2/testlist.sh
@@ -0,0 +1,8 @@
+(../parentR2 ../lib-parentI1/ikev2.record westnet--eastnet-ikev2 parentI2.pcap
+    echo TCPDUMP output
+    tcpdump -v -v -s 1600 -n -r parentR2.pcap) 2>&1 | sed -f sanity.sed
+
+
+
+
+
