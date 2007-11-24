5cb1cb66a5b82a3d70edd4fe94b72263aaf1786d
Michael Richardson <mcr@xelerance.com>
2007-11-24 14:19:49 -0500

    #835 - use tcpdump 4 to decrypt I2 packet to confirm contents are correct.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/lib/libpluto/lib-parentI2/testlist.sh
+++ testing/lib/libpluto/lib-parentI2/testlist.sh
@@ -1,7 +1,7 @@
 (../parentI2 ../lib-parentI1/ikev2.record westnet--eastnet-ikev2 parentR1.pcap 2>&1 | tee secrets.raw
     grep '^\.\./parentI2 ikev2 [IR]' secrets.raw | cut -c13- >ike-secrets.txt
     echo TCPDUMP output
-    tcpdump -t -v -v -s 1600 -n -E ike-secrets.txt -r parentI2.pcap ) 2>&1 | sed -f sanity.sed
+    tcpdump -t -v -v -s 1600 -n -E 'file ike-secrets.txt' -r parentI2.pcap ) 2>&1 | sed -f sanity.sed
 
 
 
