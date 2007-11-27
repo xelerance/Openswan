6ed83219163ef7a42841ce2626f90b90c4f9c7a5
Paul Wouters <paul@xelerance.com>
2007-11-26 20:31:05 -0500

    Added testcase for ipsec showhostkey to properly not die on reading PSK's or other non-RSA entries in the secrets file while trying to grab the default RSA key for an "ipsec showhostkey --left" operation. Somehow, PSK_PSK is confusing showhostkey (which tells us it is PSK_XAUTH)

--- testing/scripts/showhostkey-03/keys.sh
+++ testing/scripts/showhostkey-03/keys.sh
@@ -2,18 +2,9 @@
 #!/bin/sh
 TZ=GMT export TZ
 
-# test argument processing and help
-ipsec showhostkey
-ipsec showhostkey --help
+# Test ignoring of PSK's and XAUTH's and PIN's
+ipsec showhostkey --dump
 
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --id east --left
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --id west --right
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --left
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --right
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --key
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --txt 192.168.1.3
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --x509self
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --x509req
-ipsec showhostkey --file /testing/baseconfigs/west/etc/ipsec.secrets-via.include --x509cert
+ipsec showhostkey --file /testing/pluto/scripts/showhostkey-03/multiple.secrets --left
 
 : ==== end ====
