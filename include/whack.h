93e4f9e9e9f42e9432a14a97f11a8435de917750
Michael Richardson <mcr@xelerance.com>
2007-11-29 14:56:48 -0500

    #835 - moved readwhackmsg() to libpluto.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- include/whack.h
+++ include/whack.h
@@ -287,6 +287,8 @@ extern size_t whack_get_secret(char *buf, size_t bufsize);
 extern int whack_get_value(char *buf, size_t bufsize);
 
 extern bool osw_alias_cmp(const char *needle, const char *haystack);
+extern void whack_process(int whackfd, struct whack_message msg);
+
 #endif /* _WHACK_H */
 
 /*
