5fc348285c37a069077d1ac897068d7bfcc0be7c
Paul Wouters <paul@xelerance.com>
2007-11-29 23:31:44 -0500

    testcase for modecfgdns1,modecfgdns2,modecfgwins1,modecfgwins2

---
+++ testing/pluto/xauth-pluto-12/eastinit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+: ==== start ====
+
+ipsec setup stop
+umount /var/tmp; mount /var/tmp
+umount /usr/local; mount /usr/local
+
+export TESTNAME=xauth-pluto-12
+source /testing/pluto/bin/eastlocal.sh
+
+ipsec setup start
+ipsec auto --add modecfg-road--eastnet-psk
+/testing/pluto/bin/wait-until-pluto-started
+
+echo done.
+
+
+
+
