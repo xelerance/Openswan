a8b2a9f15d72250b970f8aefce64ad11eca1cf7d
Michael Richardson <mcr@xelerance.com>
2007-12-02 13:47:47 -0500

    #835 - refactored v1 SIG/RSA key finding code to permit a different underlying 	signature checking algorithm
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- programs/pluto/ikev1_main.c
+++ programs/pluto/ikev1_main.c
@@ -456,9 +456,9 @@ RSA_sign_hash(struct connection *c
  * different) computation for encryption.
  */
 static err_t
-try_RSA_signature(const u_char hash_val[MAX_DIGEST_LEN], size_t hash_len
-, const pb_stream *sig_pbs, struct pubkey *kr
-, struct state *st)
+try_RSA_signature_v1(const u_char hash_val[MAX_DIGEST_LEN], size_t hash_len
+		     , const pb_stream *sig_pbs, struct pubkey *kr
+		     , struct state *st)
 {
     const u_char *sig_val = sig_pbs->cur;
     size_t sig_len = pbs_left(sig_pbs);
@@ -564,244 +564,25 @@ try_RSA_signature(const u_char hash_val[MAX_DIGEST_LEN], size_t hash_len
     return NULL;    /* happy happy */
 }
 
-/* Check signature against all RSA public keys we can find.
- * If we need keys from DNS KEY records, and they haven't been fetched,
- * return STF_SUSPEND to ask for asynch DNS lookup.
- *
- * Note: parameter keys_from_dns contains results of DNS lookup for key
- * or is NULL indicating lookup not yet tried.
- *
- * take_a_crack is a helper function.  Mostly forensic.
- * If only we had coroutines.
- */
-struct tac_state {
-    /* RSA_check_signature's args that take_a_crack needs */
-    struct state *st;
-    const u_char *hash_val;
-    size_t hash_len;
-    const pb_stream *sig_pbs;
-
-    /* state carried between calls */
-    err_t best_ugh;	/* most successful failure */
-    int tried_cnt;	/* number of keys tried */
-    char tried[50];	/* keyids of tried public keys */
-    char *tn;	/* roof of tried[] */
-};
-
-static bool
-take_a_crack(struct tac_state *s
-, struct pubkey *kr
-, const char *story USED_BY_DEBUG)
-{
-    err_t ugh = try_RSA_signature(s->hash_val, s->hash_len, s->sig_pbs
-	, kr, s->st);
-    const struct RSA_public_key *k = &kr->u.rsa;
-
-    s->tried_cnt++;
-    if (ugh == NULL)
-    {
-	DBG(DBG_CRYPT | DBG_CONTROL
-	    , DBG_log("an RSA Sig check passed with *%s [%s]"
-		, k->keyid, story));
-	return TRUE;
-    }
-    else
-    {
-	DBG(DBG_CRYPT
-	    , DBG_log("an RSA Sig check failure %s with *%s [%s]"
-		, ugh + 1, k->keyid, story));
-	if (s->best_ugh == NULL || s->best_ugh[0] < ugh[0])
-	    s->best_ugh = ugh;
-	if (ugh[0] > '0'
-	&& s->tn - s->tried + KEYID_BUF + 2 < (ptrdiff_t)sizeof(s->tried))
-	{
-	    strcpy(s->tn, " *");
-	    strcpy(s->tn + 2, k->keyid);
-	    s->tn += strlen(s->tn);
-	}
-	return FALSE;
-    }
-}
-
 static stf_status
 RSA_check_signature(struct state *st
-, const u_char hash_val[MAX_DIGEST_LEN]
-, size_t hash_len
-, const pb_stream *sig_pbs
+		    , const u_char hash_val[MAX_DIGEST_LEN]
+		    , size_t hash_len
+		    , const pb_stream *sig_pbs
 #ifdef USE_KEYRR
-, const struct pubkey_list *keys_from_dns
+		    , const struct pubkey_list *keys_from_dns
 #endif /* USE_KEYRR */
-, const struct gw_info *gateways_from_dns
+		    , const struct gw_info *gateways_from_dns
 )
 {
-    const struct connection *c = st->st_connection;
-    struct tac_state s;
-    err_t dns_ugh = NULL;
-
-    s.st = st;
-    s.hash_val = hash_val;
-    s.hash_len = hash_len;
-    s.sig_pbs = sig_pbs;
-
-    s.best_ugh = NULL;
-    s.tried_cnt = 0;
-    s.tn = s.tried;
-
-    /* try all gateway records hung off c */
-    if ((c->policy & POLICY_OPPO))
-    {
-	struct gw_info *gw;
-
-	for (gw = c->gw_info; gw != NULL; gw = gw->next)
-	{
-	    /* only consider entries that have a key and are for our peer */
-	    if (gw->gw_key_present
-	    && same_id(&gw->gw_id, &c->spd.that.id)
-	    && take_a_crack(&s, gw->key, "key saved from DNS TXT"))
-		return STF_OK;
-	}
-    }
-
-    /* try all appropriate Public keys */
-    {
-	struct pubkey_list *p, **pp;
-	int pathlen;
-
-	pp = &pluto_pubkeys;
-	pathlen = pathlen;      /* make sure it used even with !X509 */
-
-	{
-	  char buf[IDTOA_BUF];
-	  
-	  DBG(DBG_CONTROL,
-	      dntoa_or_null(buf, IDTOA_BUF, c->spd.that.ca, "%any");
-	      DBG_log("required CA is '%s'", buf));
-	}
-  
-	for (p = pluto_pubkeys; p != NULL; p = *pp)
-	{
-	    struct pubkey *key = p->key;
-
-	    if (key->alg == PUBKEY_ALG_RSA
-		&& same_id(&c->spd.that.id, &key->id)
-		&& trusted_ca(key->issuer, c->spd.that.ca, &pathlen))
-	    {
-		time_t tnow;
-
-		{
-		  char buf[IDTOA_BUF];
-		  
-		  DBG(DBG_CONTROL,
-		      dntoa_or_null(buf, IDTOA_BUF, key->issuer, "%any");
-		      DBG_log("key issuer CA is '%s'", buf));
-		}
-
-		/* check if found public key has expired */
-		time(&tnow);
-		if (key->until_time != UNDEFINED_TIME && key->until_time < tnow)
-		{
-		    loglog(RC_LOG_SERIOUS,
-			"cached RSA public key has expired and has been deleted");
-		    *pp = free_public_keyentry(p);
-		    continue; /* continue with next public key */
-		}
-
-		if (take_a_crack(&s, key, "preloaded key"))
-		return STF_OK;
-	    }
-	    pp = &p->next;
-	}
-   }
-
-    /* if no key was found (evidenced by best_ugh == NULL)
-     * and that side of connection is key_from_DNS_on_demand
-     * then go search DNS for keys for peer.
-     */
-    if (s.best_ugh == NULL && c->spd.that.key_from_DNS_on_demand)
-    {
-	if (gateways_from_dns != NULL)
-	{
-	    /* TXT keys */
-	    const struct gw_info *gwp;
-
-	    for (gwp = gateways_from_dns; gwp != NULL; gwp = gwp->next)
-		if (gwp->gw_key_present
-		&& take_a_crack(&s, gwp->key, "key from DNS TXT"))
-		    return STF_OK;
-	}
+    return RSA_check_signature_gen(st, hash_val, hash_len
+				   , sig_pbs
 #ifdef USE_KEYRR
-	else if (keys_from_dns != NULL)
-	{
-	    /* KEY keys */
-	    const struct pubkey_list *kr;
-
-	    for (kr = keys_from_dns; kr != NULL; kr = kr->next)
-		if (kr->key->alg == PUBKEY_ALG_RSA
-		&& take_a_crack(&s, kr->key, "key from DNS KEY"))
-		    return STF_OK;
-	}
-#endif /* USE_KEYRR */
-	else
-	{
-	    /* nothing yet: ask for asynch DNS lookup */
-	    return STF_SUSPEND;
-	}
-    }
-
-    /* no acceptable key was found: diagnose */
-    {
-	char id_buf[IDTOA_BUF];	/* arbitrary limit on length of ID reported */
-
-	(void) idtoa(&st->st_connection->spd.that.id, id_buf, sizeof(id_buf));
-
-	if (s.best_ugh == NULL)
-	{
-	    if (dns_ugh == NULL)
-		loglog(RC_LOG_SERIOUS, "no RSA public key known for '%s'"
-		    , id_buf);
-	    else
-		loglog(RC_LOG_SERIOUS, "no RSA public key known for '%s'"
-		    "; DNS search for KEY failed (%s)"
-		    , id_buf, dns_ugh);
-
-	    /* ??? is this the best code there is? */
-	    return STF_FAIL + INVALID_KEY_INFORMATION;
-	}
-
-	if (s.best_ugh[0] == '9')
-	{
-	    loglog(RC_LOG_SERIOUS, "%s", s.best_ugh + 1);
-	    /* XXX Could send notification back */
-	    return STF_FAIL + INVALID_HASH_INFORMATION;
-	}
-	else
-	{
-	    if (s.tried_cnt == 1)
-	    {
-		loglog(RC_LOG_SERIOUS
-		    , "Signature check (on %s) failed (wrong key?); tried%s"
-		    , id_buf, s.tried);
-		DBG(DBG_CONTROL,
-		    DBG_log("public key for %s failed:"
-			" decrypted SIG payload into a malformed ECB (%s)"
-			, id_buf, s.best_ugh + 1));
-	    }
-	    else
-	    {
-		loglog(RC_LOG_SERIOUS
-		    , "Signature check (on %s) failed:"
-		      " tried%s keys but none worked."
-		    , id_buf, s.tried);
-		DBG(DBG_CONTROL,
-		    DBG_log("all %d public keys for %s failed:"
-			" best decrypted SIG payload into a malformed ECB (%s)"
-			, s.tried_cnt, id_buf, s.best_ugh + 1));
-	    }
-	    return STF_FAIL + INVALID_KEY_INFORMATION;
-	}
-    }
+				   , keys_from_dns
+#endif
+				   , gateways_from_dns
+				   , try_RSA_signature_v1);
 }
-
 
 notification_t
 accept_v1_nonce(struct msg_digest *md, chunk_t *dest, const char *name)
