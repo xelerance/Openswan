f3c4a6daee3117f9fb4eeadfdd605533ccc5aba1
Michael Richardson <mcr@xelerance.com>
2007-12-09 12:27:20 -0500

    #841 - test for inserting traffic selectors into I2
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/lib/libpluto/seam_kernel.c
+++ testing/lib/libpluto/seam_kernel.c
@@ -2,5 +2,18 @@ void delete_ipsec_sa(struct state *st USED_BY_KLIPS, bool inbound_only USED_BY_K
 
 bool install_inbound_ipsec_sa(struct state *st) { return TRUE; }
 
+ipsec_spi_t spis[4]={ 0x12345678,
+		      0x34567812,
+		      0x56781234,
+		      0x78123456};
+static int spinext=0;
+ipsec_spi_t get_ipsec_spi(ipsec_spi_t avoid UNUSED
+			  , int proto UNUSED, struct spd_route *sr UNUSED
+			  , bool tunnel UNUSED)
+{
+	if(spinext == 4) spinext=0;
+	return spis[spinext++];
+}
+
 
 
