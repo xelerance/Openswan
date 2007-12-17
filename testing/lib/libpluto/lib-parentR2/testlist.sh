0358b4f87129acb733be164f76c7b24c40f8f088
Michael Richardson <mcr@xelerance.com>
2007-12-17 01:02:01 -0500

    #838 - adjustments to unit tests to reflect changes to debugging and 	host/network presentation of SPI#
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/lib/libpluto/lib-parentR2/testlist.sh
+++ testing/lib/libpluto/lib-parentR2/testlist.sh
@@ -1,5 +1,5 @@
 (../parentR2 ../lib-parentR1/ikev2.record westnet--eastnet-ikev2 parentI2.pcap 2>&1 | tee secrets.raw
-    grep '^\.\./parentR2 ikev2 [IR]' secrets.raw | cut -c13- >ike-secrets.txt
+    grep '^| ikev2 [IR]' secrets.raw | cut -c3- >ike-secrets.txt
     echo TCPDUMP output
     tcpdump -v -v -s 1600 -n -E 'file ike-secrets.txt' -r parentR2.pcap) 2>&1 | sed -f sanity.sed
 
