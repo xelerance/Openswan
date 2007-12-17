dfb6b9e6cff4e4d480a0cf08d7c152768a7a286c
Ken Bantoft <ken@cyclops.xelerance.com>
2007-12-17 13:50:35 -0500

    make sure to set socket family for originating port number.

--- programs/ikeping/ikeping.c
+++ programs/ikeping/ikeping.c
@@ -405,6 +405,7 @@ main(int argc, char **argv)
 
   switch(afamily) {
   case AF_INET:
+	  laddr.u.v4.sin_family= AF_INET;
 	  laddr.u.v4.sin_port = htons(lport);
 	  if(bind(s, (struct sockaddr *)&laddr.u.v4, sizeof(laddr.u.v4)) < 0) {
 		  perror("v4 bind");
@@ -413,13 +414,13 @@ main(int argc, char **argv)
 	  break;
 	  
   case AF_INET6:
-      laddr.u.v6.sin6_family = afamily;
-      laddr.u.v6.sin6_port = htons(lport);
-      if(bind(s, (struct sockaddr *)&laddr.u.v6, sizeof(laddr.u.v6)) < 0) {
-	  perror("v6 bind");
-	  exit(5);
-      }
-      break;
+	  laddr.u.v6.sin6_family= AF_INET6;
+	  laddr.u.v6.sin6_port = htons(lport);
+	  if(bind(s, (struct sockaddr *)&laddr.u.v6, sizeof(laddr.u.v6)) < 0) {
+		  perror("v6 bind");
+		  exit(5);
+	  }
+	  break;
   }
 
   if(natt) {
