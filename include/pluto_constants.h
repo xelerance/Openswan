2f1a9cc1195310984eb45a86ef7790a4b746354f
Ken Bantoft <ken@cyclops.xelerance.com>
2007-12-17 15:10:42 -0500

    new policy keywords for dns/wins options. (cherry picked from commit 41a55abae5238d2197216c4dc5d41d4e60fa31ab)

--- include/pluto_constants.h
+++ include/pluto_constants.h
@@ -442,6 +442,11 @@ enum pluto_policy {
 	POLICY_IKEV2_ALLOW   = LELEM(25), /* accept IKEv2?   0x0200 0000 */
 	POLICY_IKEV2_PROPOSE = LELEM(26), /* propose IKEv2?  0x0400 0000 */
 	POLICY_IKEV2_MASK = POLICY_IKEV1_DISABLE|POLICY_IKEV2_ALLOW|POLICY_IKEV2_PROPOSE,
+
+	POLICY_MODECFGDNS1  = LELEM(27),   /* should we offer a DNS server IP */
+	POLICY_MODECFGDNS2  = LELEM(28),   /* should we offer another DNS server IP */
+	POLICY_MODECFGWINS1 = LELEM(29),   /* should we offer a WINS server IP */
+	POLICY_MODECFGWINS2 = LELEM(30),   /* should we offer another WINS server IP */
 };
 
 /* Any IPsec policy?  If not, a connection description
