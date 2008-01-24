6ed9c2a59d9f05253d088a1e457ceb492ec24bab
Antony Antony <antony@xelerance.com>
2008-01-24 05:41:35 -0500

    use psk pcap file for psk test case.

--- testing/lib/libpluto/lib-parentR2psk/testlist.sh
+++ testing/lib/libpluto/lib-parentR2psk/testlist.sh
@@ -1,7 +1,7 @@
-(../parentR2 ../lib-parentR1/ikev2.record westnet--eastnet-ikev2 parentI2.pcap 2>&1 | tee secrets.raw
+(../parentR2psk ../lib-parentR1psk/ikev2.record westnet--eastnet-ikev2 ../lib-parentI2psk/parentI2psk.pcap 2>&1 | tee secrets.raw
     grep '^| ikev2 [IR]' secrets.raw | cut -c3- >ike-secrets.txt
     echo TCPDUMP output
-    tcpdump -v -v -s 1600 -n -E 'file ike-secrets.txt' -r parentR2.pcap) 2>&1 | sed -f sanity.sed
+    tcpdump -v -v -s 1600 -n -E 'file ike-secrets.txt' -r parentR2psk.pcap) 2>&1 | sed -f sanity.sed
 
 
 
