13bda2c1cb8b6a35a3842dc042d433478fcf4ffb
Paul Wouters <paul@xelerance.com>
2008-01-25 19:33:16 +0100

    Fixed expected output for parentI1psk.

--- testing/lib/libpluto/lib-parentI1psk/testlist.sh
+++ testing/lib/libpluto/lib-parentI1psk/testlist.sh
@@ -1,6 +1,6 @@
 (../parentI1psk ikev2.record westnet--eastnet-ikev2
     echo TCPDUMP output
-    tcpdump -t -v -v -s 1600 -n -r parentI1.pcap ) 2>&1 | sed -f ../lib-parentI1/sanity.sed
+    tcpdump -t -v -v -s 1600 -n -r parentI1psk.pcap ) 2>&1 | sed -f ../lib-parentI1/sanity.sed
 
 
 
