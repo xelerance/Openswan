fb6d4cf68c07d395bc5c0e882cdf5a99d4d3eb34
Antony Antony <antony@xelerance.com>
2008-01-30 16:40:08 -0500

    factor out x509 bits from ikev2_parent.c

---
+++ programs/pluto/ikev2_x509.c
@@ -0,0 +1,68 @@
+/* do PSK operations for IKEv2
+ *
+ * Copyright (C) 2008 Antony Antony <antony@xelerance.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/gpl.txt>.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <openswan.h>
+
+#include "sysdep.h"
+#include "constants.h"
+#include "oswlog.h"
+
+#include "defs.h"
+#include "cookie.h"
+#include "id.h"
+#include "x509.h"
+#include "pgp.h"
+#include "certs.h"
+#include "smartcard.h"
+#ifdef XAUTH_USEPAM
+#include <security/pam_appl.h>
+#endif
+#include "connections.h"	/* needs id.h */
+#include "state.h"
+#include "packet.h"
+#include "md5.h"
+#include "sha1.h"
+#include "crypto.h" /* requires sha1.h and md5.h */
+#include "ike_alg.h"
+#include "log.h"
+#include "demux.h"	/* needs packet.h */
+#include "ikev2.h"
+#include "server.h"
+#include "vendor.h"
+#include "dpd.h"
+#include "keys.h"
+
+#ifdef HAVE_OCF
+#include "ocf_pk.h"
+#endif
+
+/*
+ * Local Variables:
+ * c-basic-offset:4
+ * c-style: pluto
+ * End:
+ */
