f45532eae841ab5ac5d8c6407a8806c581877423
Michael Richardson <mcr@xelerance.com>
2008-02-06 22:57:30 -0500

    test case for receiving duplicates on responder, in R2 	- need to keep received counter up to date on "parent"
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

---
+++ testing/lib/libpluto/lib-parentR2duplicate/testlist.sh
@@ -0,0 +1,5 @@
+(../parentR2duplicate ../lib-parentR1/ikev2.record westnet--eastnet-ikev2 ../lib-parentR2/parentI2.pcap 2>&1 | tee secrets.raw
+    grep '^| ikev2 [IR]' secrets.raw | cut -c3- >ike-secrets.txt
+    echo TCPDUMP output
+    tcpdump -v -v -s 1600 -n -E 'file ike-secrets.txt' -r parentR2.pcap) 2>&1 | sed -f ../lib-parentR2/sanity.sed
+
