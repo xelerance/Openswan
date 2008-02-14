1a2b0fa95c43f6d9848ba666fffebd74ec1899f3
Paul Wouters <paul@xelerance.com>
2008-02-14 14:58:50 -0500

    Various fixes to path names for racoon configs.

--- testing/pluto/interop-ikev2-racoon-03/eastinit.sh
+++ testing/pluto/interop-ikev2-racoon-03/eastinit.sh
@@ -1,7 +1,10 @@
 : ==== start ====
 TESTNAME=interop-ikev2-racoon-03
-source /testing/pluto/bin/eastlocal.sh
 
+#source /testing/pluto/bin/eastlocal.sh
+mkdir /tmp/racoon2
+cp -r /testing/pluto/$TESTNAME/east-racoon/* /tmp/racoon2/
+chmod 700 /tmp/racoon2/psk/test.psk   
 /usr/local/racoon2/etc/init.d/spmd start
 /usr/local/racoon2/etc/init.d/iked start
 
