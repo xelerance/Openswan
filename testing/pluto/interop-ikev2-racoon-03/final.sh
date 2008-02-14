90cf4659e2648215b19591ec30501a6c14b5cc78
Paul Wouters <paul@xelerance.com>
2008-02-14 15:46:44 -0500

    Fix fina.sh to display racoon instead of pluto information if that is in use.

--- testing/pluto/interop-ikev2-racoon-03/final.sh
+++ testing/pluto/interop-ikev2-racoon-03/final.sh
@@ -1,6 +1,13 @@
 : ==== cut ====
-ipsec auto --status
-cat /tmp/pluto.log
+if [ -n "`pidof pluto`" ]
+then
+	ipsec auto --status
+	cat /tmp/pluto.log
+fi
+if [ -n "`pidof iked`" ]
+then
+	cat /tmp/racoon.log
+fi
 : ==== tuc ====
 if [ -f /tmp/core ]; then echo CORE FOUND; mv /tmp/core /var/tmp; fi
 : ==== end ====
