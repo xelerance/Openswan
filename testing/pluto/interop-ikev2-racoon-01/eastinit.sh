6eb7e0009db1b7c10702a04be42968c29337840d
Paul Wouters <paul@xelerance.com>
2008-02-14 19:40:20 -0500

    copy racoon configs to proper place in eastinit.sh

--- testing/pluto/interop-ikev2-racoon-01/eastinit.sh
+++ testing/pluto/interop-ikev2-racoon-01/eastinit.sh
@@ -1,6 +1,9 @@
 : ==== start ====
 TESTNAME=interop-ikev2-racoon-01
-source /testing/pluto/bin/eastlocal.sh
+
+mkdir /tmp/racoon2
+cp -r /testing/pluto/$TESTNAME/east-racoon/* /tmp/racoon2/
+chmod 700 /tmp/racoon2/psk/test.psk   
 
 #racoon way of starting
 /usr/local/racoon2/etc/init.d/spmd start
