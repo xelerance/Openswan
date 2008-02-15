e3ef9e557cb0cee3f6c3d7589225771406fac9f9
Paul Wouters <paul@xelerance.com>
2008-02-15 22:36:37 +0100

    strongswan-01 interop testcases - should show crasher

--- testing/pluto/interop-ikev2-strongswan-01/eastinit.sh
+++ testing/pluto/interop-ikev2-strongswan-01/eastinit.sh
@@ -1,9 +1,10 @@
 : ==== start ====
 TESTNAME=interop-ikev2-strongswan-01
-source /testing/pluto/bin/eastlocal.sh
 
 #strongswan way of starting
 /usr/local/strongswan/sbin/ipsec start
 # no conns are loaded
-# 
+#
+# note: ikev2 is done by charon, but ikev1 by pluto, so hopefully
+# the below script is still good to use 
 /testing/pluto/bin/wait-until-pluto-started
