937b8c7ecdbd93d81da77d089d90b93197a3bd40
Paul Wouters <paul@xelerance.com>
2008-02-19 10:58:28 -0500

    updated eastlocal.sh and westlocal.sh to support WEST_USERLAND= and EAST_USERLAND= options. Currently supported "strongswan", "racoon2". Everything else leads to the old behaviour (pluto/openswan)

--- testing/pluto/bin/eastlocal.sh
+++ testing/pluto/bin/eastlocal.sh
@@ -5,25 +5,41 @@
 
 TESTING=${TESTING-/testing}
 
-mkdir -p /tmp/$TESTNAME
-mkdir -p /tmp/$TESTNAME/ipsec.d/cacerts
-mkdir -p /tmp/$TESTNAME/ipsec.d/crls
-mkdir -p /tmp/$TESTNAME/ipsec.d/certs
-mkdir -p /tmp/$TESTNAME/ipsec.d/private
-
-cp ${TESTING}/pluto/$TESTNAME/east.conf /tmp/$TESTNAME/ipsec.conf
-cp /etc/ipsec.secrets                    /tmp/$TESTNAME
-if [ -f ${TESTING}/pluto/$TESTNAME/east.secrets ] 
+if [ "$EAST_USERLAND" == "strongswan" ]
 then
-    cat ${TESTING}/pluto/$TESTNAME/east.secrets >>/tmp/$TESTNAME/ipsec.secrets
-fi
+        # setup strongswan
+        mkdir -p /tmp/strongswan/etc/ipsec.d/certs /tmp/strongswan/etc/ipsec.d/cacerts /tmp/strongswan/etc/ipsec.d/aacerts /tmp/strongswan/etc/ipsec.d/ocspcerts /tmp/strongswan/etc/ipsec.d/crls
+        cp /testing/pluto/$TESTNAME/east.conf /tmp/strongswan/etc/ipsec.conf
+        cp /testing/pluto/$TESTNAME/east.secrets /tmp/strongswan/etc/ipsec.secrets
+        chmod 600 /tmp/strongswan/etc/ipsec.secrets
+        touch /tmp/strongswan/etc/ipsec.secrets
 
-if [ -f ${TESTING}/pluto/$TESTNAME/east.tpm.tcl ] 
+elif [ "$WEST_USERLAND" == "racoon2" ]
 then
-    cp ${TESTING}/pluto/$TESTNAME/east.tpm.tcl /tmp/$TESTNAME/ipsec.d/tpm.tcl
+        # setup racoon
+        # note: tests do this manual - needs to be merged in
+else
+	# setup openswan
+	mkdir -p /tmp/$TESTNAME
+	mkdir -p /tmp/$TESTNAME/ipsec.d/cacerts
+	mkdir -p /tmp/$TESTNAME/ipsec.d/crls
+	mkdir -p /tmp/$TESTNAME/ipsec.d/certs
+	mkdir -p /tmp/$TESTNAME/ipsec.d/private
+
+	cp ${TESTING}/pluto/$TESTNAME/east.conf /tmp/$TESTNAME/ipsec.conf
+	cp /etc/ipsec.secrets                    /tmp/$TESTNAME
+	if [ -f ${TESTING}/pluto/$TESTNAME/east.secrets ] 
+	then
+    		cat ${TESTING}/pluto/$TESTNAME/east.secrets >>/tmp/$TESTNAME/ipsec.secrets
+	fi
+
+	if [ -f ${TESTING}/pluto/$TESTNAME/east.tpm.tcl ] 
+	then
+    		cp ${TESTING}/pluto/$TESTNAME/east.tpm.tcl /tmp/$TESTNAME/ipsec.d/tpm.tcl
+	fi
+
+	mkdir -p /tmp/$TESTNAME/ipsec.d/policies
+	cp -r /etc/ipsec.d/*          /tmp/$TESTNAME/ipsec.d
+
+	IPSEC_CONFS=/tmp/$TESTNAME export IPSEC_CONFS
 fi
-
-mkdir -p /tmp/$TESTNAME/ipsec.d/policies
-cp -r /etc/ipsec.d/*          /tmp/$TESTNAME/ipsec.d
-
-IPSEC_CONFS=/tmp/$TESTNAME export IPSEC_CONFS
--- testing/pluto/bin/westlocal.sh
+++ testing/pluto/bin/westlocal.sh
@@ -1,22 +1,38 @@
 #!/bin/sh
 
 # this script is used by "west" UMLs that want to have per-test
-# configuration files, and will be hitting sunrise-oe to test with.
+# configuration files. 
 
-mkdir -p /tmp/$TESTNAME
-mkdir -p /tmp/$TESTNAME/ipsec.d/cacerts
-mkdir -p /tmp/$TESTNAME/ipsec.d/crls
-mkdir -p /tmp/$TESTNAME/ipsec.d/certs
-mkdir -p /tmp/$TESTNAME/ipsec.d/private
-
-cp /testing/pluto/$TESTNAME/west.conf /tmp/$TESTNAME/ipsec.conf
-cp /etc/ipsec.secrets                    /tmp/$TESTNAME
-if [ -f /testing/pluto/$TESTNAME/west.secrets ] 
+if [ "$WEST_USERLAND" == "strongswan" ]
 then
-    cat /testing/pluto/$TESTNAME/west.secrets >>/tmp/$TESTNAME/ipsec.secrets
+	# setup strongswan
+	mkdir -p /tmp/strongswan/etc/ipsec.d/certs /tmp/strongswan/etc/ipsec.d/cacerts /tmp/strongswan/etc/ipsec.d/aacerts /tmp/strongswan/etc/ipsec.d/ocspcerts /tmp/strongswan/etc/ipsec.d/crls
+	cp /testing/pluto/$TESTNAME/west.conf /tmp/strongswan/etc/ipsec.conf
+	cp /testing/pluto/$TESTNAME/west.secrets /tmp/strongswan/etc/ipsec.secrets
+	chmod 600 /tmp/strongswan/etc/ipsec.secrets
+	touch /tmp/strongswan/etc/ipsec.secrets
+
+elif [ "$WEST_USERLAND" == "racoon2" ]
+then
+	# setup racoon
+	# note: tests do this manual - needs to be merged in
+else
+	# setup pluto
+	mkdir -p /tmp/$TESTNAME
+	mkdir -p /tmp/$TESTNAME/ipsec.d/cacerts
+	mkdir -p /tmp/$TESTNAME/ipsec.d/crls
+	mkdir -p /tmp/$TESTNAME/ipsec.d/certs
+	mkdir -p /tmp/$TESTNAME/ipsec.d/private
+
+	cp /testing/pluto/$TESTNAME/west.conf /tmp/$TESTNAME/ipsec.conf
+	cp /etc/ipsec.secrets                    /tmp/$TESTNAME
+	if [ -f /testing/pluto/$TESTNAME/west.secrets ] 
+	then
+	    	cat /testing/pluto/$TESTNAME/west.secrets >>/tmp/$TESTNAME/ipsec.secrets
+	fi
+
+	mkdir -p /tmp/$TESTNAME/ipsec.d/policies
+	cp -r /etc/ipsec.d/*          /tmp/$TESTNAME/ipsec.d
+
+	IPSEC_CONFS=/tmp/$TESTNAME export IPSEC_CONFS
 fi
-
-mkdir -p /tmp/$TESTNAME/ipsec.d/policies
-cp -r /etc/ipsec.d/*          /tmp/$TESTNAME/ipsec.d
-
-IPSEC_CONFS=/tmp/$TESTNAME export IPSEC_CONFS
