8810f9c481e4ce0520ccf8514caac0e0529cb6db
Paul Wouters <paul@xelerance.com>
2008-02-21 18:27:50 -0500

    Updates to ikev2-04-basic-x509. Does certificates from disk (out of band PKIX). RSA sig error?

--- testing/pluto/ikev2-04-basic-x509/westinit.sh
+++ testing/pluto/ikev2-04-basic-x509/westinit.sh
@@ -1,6 +1,6 @@
 : ==== start ====
 TESTNAME=ikev2-04-basic-x509
-source /testing/pluto/bin/westlocal.sh
+source /testing/pluto/bin/westnlocal.sh
 
 ipsec setup start
 ipsec whack --whackrecord /var/tmp/ikev2.record
