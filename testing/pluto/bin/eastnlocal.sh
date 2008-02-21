e465116e0be8352ce01495eff1fd8c51908ca387
Paul Wouters <paul@xelerance.com>
2008-02-21 18:19:02 -0500

    Added eastnlocal.sh, like westnlocal.sh which copies not appends, the ipsec configs

---
+++ testing/pluto/bin/eastnlocal.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# this script is used by "east" UMLs that want to have per-test
+# configuration files
+# differs from eastlocal.sh in that it copies, not appends
+
+TESTING=${TESTING-/testing}
+
+if [ "$EAST_USERLAND" == "strongswan" ]
+then
+        # setup strongswan
+        mkdir -p /tmp/strongswan/etc/ipsec.d/certs /tmp/strongswan/etc/ipsec.d/cacerts /tmp/strongswan/etc/ipsec.d/aacerts /tmp/strongswan/etc/ipsec.d/ocspcerts /tmp/strongswan/etc/ipsec.d/crls
+        cp /testing/pluto/$TESTNAME/east.conf /tmp/strongswan/etc/ipsec.conf
+        cp /testing/pluto/$TESTNAME/east.secrets /tmp/strongswan/etc/ipsec.secrets
+        chmod 600 /tmp/strongswan/etc/ipsec.secrets
+        touch /tmp/strongswan/etc/ipsec.secrets
+
+elif [ "$WEST_USERLAND" == "racoon2" ]
+then
+        # setup racoon
+        # note: tests do this manual - needs to be merged in
+	echo "racoon2 not yet merged into local scripts"
+else
+	# setup openswan
+	mkdir -p /tmp/$TESTNAME
+	mkdir -p /tmp/$TESTNAME/ipsec.d/cacerts
+	mkdir -p /tmp/$TESTNAME/ipsec.d/crls
+	mkdir -p /tmp/$TESTNAME/ipsec.d/certs
+	mkdir -p /tmp/$TESTNAME/ipsec.d/private
+
+	cp ${TESTING}/pluto/$TESTNAME/east.conf /tmp/$TESTNAME/ipsec.conf
+	cp /etc/ipsec.secrets                    /tmp/$TESTNAME
+	if [ -f ${TESTING}/pluto/$TESTNAME/east.secrets ] 
+	then
+    		cp ${TESTING}/pluto/$TESTNAME/east.secrets /tmp/$TESTNAME/ipsec.secrets
+	fi
+
+	if [ -f ${TESTING}/pluto/$TESTNAME/east.tpm.tcl ] 
+	then
+    		cp ${TESTING}/pluto/$TESTNAME/east.tpm.tcl /tmp/$TESTNAME/ipsec.d/tpm.tcl
+	fi
+
+	mkdir -p /tmp/$TESTNAME/ipsec.d/policies
+	cp -r /etc/ipsec.d/*          /tmp/$TESTNAME/ipsec.d
+
+	IPSEC_CONFS=/tmp/$TESTNAME export IPSEC_CONFS
+fi
