a68c3173b3456a771863e7980295882cbe901a21
Antony Antony <antony@xelerance.com>
2008-02-22 05:12:42 -0500

    interop: child and parent SA established. need to check if the tunnel is correct.

--- testing/pluto/interop-ikev2-racoon-02-psk-responder/eastinit.sh
+++ testing/pluto/interop-ikev2-racoon-02-psk-responder/eastinit.sh
@@ -6,6 +6,8 @@ mkdir /tmp/racoon2 /var/run/racoon2
 chmod 700 /var/run/racoon2
 cp -r /testing/pluto/$TESTNAME/east-racoon/* /tmp/racoon2/
 chmod 700 /tmp/racoon2/psk/test.psk /tmp/racoon2/spmd.pwd
+chmod 600 /tmp/racoon2/spmd.pwd
+
 
 /usr/local/racoon2/etc/init.d/spmd start
 /usr/local/racoon2/etc/init.d/iked start
