a5870ca21d1bea671f46cb5042766462de58ef54
Paul Wouters <paul@xelerance.com>
2008-03-04 18:29:10 +0100

    Add conditional defines for s6_addr16 and s6_addr32 - OSX does not define these.

--- ports/darwin/include/sysdep.h
+++ ports/darwin/include/sysdep.h
@@ -4,3 +4,12 @@
 #include <limits.h>
 /* POSIX 1003.1-2001 says <unistd.h> defines this */
 #define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
+
+#ifndef s6_addr16
+# define s6_addr16 __u6_addr.__u6_addr16
+#endif
+
+#ifndef s6_addr32
+# define s6_addr32 __u6_addr.__u6_addr32
+#endif
+
