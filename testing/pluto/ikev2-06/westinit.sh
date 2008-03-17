de22c080873d2926391520eccb6cf4ed46b3bb0d
Michael Richardson <mcr@xelerance.com>
2008-03-16 21:22:41 -0400

    test case for BID-DOWN attack response.
    Signed-off-by: Michael Richardson <mcr@xelerance.com>

--- testing/pluto/ikev2-06/westinit.sh
+++ testing/pluto/ikev2-06/westinit.sh
@@ -9,6 +9,7 @@ export PLUTO_MAXIMUM_RETRANSMISSIONS_INITIAL=4
 iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 
 # drop a bunch of IKE packets
+iptables -F OUTPUT
 iptables -A OUTPUT -o eth1 -p udp --dport 500 -m recent --rcheck --hitcount 6 -j ACCEPT
 iptables -A OUTPUT -o eth1 -p udp --dport 500 -m recent --set -j DROP
 
@@ -16,7 +17,7 @@ ipsec setup start
 ipsec whack --whackrecord /var/tmp/ikev2.record
 ipsec auto --add westnet--eastnet-ikev2
 ipsec auto --status
-ipsec whack --debug-control --debug-controlmore --debug-parsing --debug-crypt
+ipsec whack --debug-control --debug-controlmore 
 /testing/pluto/bin/wait-until-pluto-started
 
 echo done
