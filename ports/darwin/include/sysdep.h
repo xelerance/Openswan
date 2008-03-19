3594bee06fffe098b4c6b0dc587011ba521db12b
Paul Wouters <paul@xelerance.com>
2008-03-19 14:40:29 +0100

    Only define HOST_NAME_MAX when not already defined in the darwin sysdep.

--- ports/darwin/include/sysdep.h
+++ ports/darwin/include/sysdep.h
@@ -3,7 +3,9 @@
 
 #include <limits.h>
 /* POSIX 1003.1-2001 says <unistd.h> defines this */
-#define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
+#ifndef HOST_NAME_MAX
+# define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
+#endif
 
 #ifndef s6_addr16
 # define s6_addr16 __u6_addr.__u6_addr16
