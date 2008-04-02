58f094a3d3a5c813f46c3f6652bbcc845dd8d550
Paul Wouters <paul@xelerance.com>
2008-04-02 15:40:52 -0400

    It seems HOST_NAME_MAX vanished on Linux, so do the same sysdep.h check and define it to _POSIX_HOST_NAME_MAX

--- ports/linux/include/sysdep.h
+++ ports/linux/include/sysdep.h
@@ -4,3 +4,10 @@
 #define u8 __u8
 
 #define TimeZoneOffset timezone
+
+#include <limits.h>
+/* POSIX 1003.1-2001 says <unistd.h> defines this */
+#ifndef HOST_NAME_MAX
+# define HOST_NAME_MAX _POSIX_HOST_NAME_MAX
+#endif
+
