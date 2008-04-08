57cce69422e0d0f4cc478675f36061e92af571ba
Paul Wouters <paul@xelerance.com>
2008-04-08 02:06:35 -0400

    EF_FREE_WIPES is not in ElectricFence-2.2.2.

--- testing/lib/libpluto/efencedef.h
+++ testing/lib/libpluto/efencedef.h
@@ -4,4 +4,8 @@ extern int EF_ALIGNMENT;
 extern int EF_PROTECT_BELOW;
 extern int EF_PROTECT_FREE;
 extern int EF_ALLOW_MALLOC_0;
-extern int EF_FREE_WIPES;
+/*
+ * Paul: I cannot find this in efence
+ * extern int EF_FREE_WIPES;
+ */
+int EF_FREE_WIPES = 0;
