adafe428a62d2d53105562948f8c9bad632ab35d
Paul Wouters <paul@xelerance.com>
2008-04-07 22:44:08 -0400

    Updated test results to show new STF_FAIL+NO_PORPOSAL_CHOSEN output. for interop-ikev2-racoon-01-noconn and interop-ikev2-strongswan-01-noconn

--- testing/pluto/interop-ikev2-racoon-01-noconn/westinit.sh
+++ testing/pluto/interop-ikev2-racoon-01-noconn/westinit.sh
@@ -1,9 +1,8 @@
 : ==== start ====
 TESTNAME=interop-ikev2-racoon-01-noconn
+export PLUTO_EVENT_RETRANSMIT_DELAY=3
+export PLUTO_MAXIMUM_RETRANSMISSIONS_INITIAL=4
 source /testing/pluto/bin/westnlocal.sh
-
-# make sure that clear text does not get through
-iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
 
 ipsec setup start
 ipsec whack --whackrecord /var/tmp/ikev2.record
