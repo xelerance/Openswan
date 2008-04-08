154568b1943a87728d97211f504793884ed18e52
Paul Wouters <paul@xelerance.com>
2008-04-08 02:38:28 -0400

    Shorten pluto retransmit values for test known to fail and cause retransmits

--- testing/pluto/interop-ikev2-strongswan-01-noconn/westinit.sh
+++ testing/pluto/interop-ikev2-strongswan-01-noconn/westinit.sh
@@ -1,5 +1,8 @@
 : ==== start ====
 TESTNAME=interop-ikev2-strongswan-01-noconn
+PLUTO_EVENT_RETRANSMIT_DELAY=3
+PLUTO_MAXIMUM_RETRANSMISSIONS_INITIAL=4
+
 source /testing/pluto/bin/westnlocal.sh
 
 # make sure that clear text does not get through
