dc18ef52485204ca052b10012859a5ac1ed6a41e
Jose Quaresma <josequaresma@gmail.com>
2009-05-03 02:34:18 +0200

    added osx application

---
+++ osxApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Openswan
+//
+//  Created by Jose Quaresma on 15/4/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
