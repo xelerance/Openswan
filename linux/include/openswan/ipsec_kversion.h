ad337e0c9384a0cc00ee5329df0be387cbe25c7c
Paul Wouters <paul@xelerance.com>
2009-05-08 15:23:50 -0400

    Fix for compiling klips on rhel/centos 5.3. Patch by Mark Keir. This completes #1031.

--- linux/include/openswan/ipsec_kversion.h
+++ linux/include/openswan/ipsec_kversion.h
@@ -257,7 +257,7 @@
 #else
 /* internals of struct skbuff changed */
 /* but RedHat ported some of this back to their RHEL kernel, so check for that */
-# if !defined(RHEL_MAJOR) || !defined(RHEL_MINOR) || !(RHEL_MAJOR == 5 && RHEL_MINOR == 2)
+# if !defined(RHEL_MAJOR) || !defined(RHEL_MINOR) || !(RHEL_MAJOR == 5 && RHEL_MINOR >= 2)
 #  define        HAVE_DEV_NEXT
 #  define ip_hdr(skb)  ((skb)->nh.iph)
 #  define skb_tail_pointer(skb)  ((skb)->tail)
