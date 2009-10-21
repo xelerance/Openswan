94ff367131817202ca697c065c89a1c74df554fb
Paul Wouters <paul@xelerance.com>
2009-10-21 11:33:13 -0400

    Revert "Remove pexpect case which was never hit before NETKEY DPD fix."
    This reverts commit 20bcfd2ca8cf0dcfd21b257785f2434c3a7ae849.

--- programs/pluto/kernel.c
+++ programs/pluto/kernel.c
@@ -164,10 +164,7 @@ record_and_initiate_opportunistic(const ip_subnet *ours
         initiate_ondemand(&src, &dst, transport_proto, TRUE, NULL_FD, "acquire");
     }
 
-    /*
-     * hold state with netkey (XFRM) hit this:
-     * pexpect(kernel_ops->remove_orphaned_holds != NULL);
-     */
+    pexpect(kernel_ops->remove_orphaned_holds != NULL);
     if(kernel_ops->remove_orphaned_holds) {
 	(*kernel_ops->remove_orphaned_holds)(transport_proto, ours, his);
     }
