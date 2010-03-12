467bc06e7388e823110647fbdabae60fd784eedb
Paul Wouters <paul@xelerance.com>
2010-03-12 11:57:35 -0500

    containe -> contain

--- programs/pluto/ikev1_aggr.c
+++ programs/pluto/ikev1_aggr.c
@@ -998,7 +998,7 @@ aggr_outI1(int whack_sock,
 	* should tell the user to add a proper proposal policy
 	*/
 	loglog(RC_AGGRALGO, "no IKE proposal policy specified in config!  Can not initiate aggressive mode.  A policy must be specified in the
-configuration and should containe at most one DH group (mod1024, mod1536, mod2048).  Only the first DH group will be honored.");
+configuration and should contain at most one DH group (mod1024, mod1536, mod2048).  Only the first DH group will be honored.");
 	reset_globals();
 	return STF_FAIL;
     }
