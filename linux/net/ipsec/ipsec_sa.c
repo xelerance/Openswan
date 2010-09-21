f045e5fe4b259d7b45aef9da00d146f422f75bf6
Paul Wouters <paul@xelerance.com>
2010-09-21 00:05:20 -0400

    Added back an CONFIG_KLIPS_DEBUG wrapper around a debug line in ipsec_sa_init()

--- linux/net/ipsec/ipsec_sa.c
+++ linux/net/ipsec/ipsec_sa.c
@@ -14,8 +14,6 @@
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
  *
- * RCSID $Id: ipsec_sa.c,v 1.31 2005/11/11 04:38:56 paul Exp $
- *
  * This is the file formerly known as "ipsec_xform.h"
  *
  */
@@ -1168,8 +1166,10 @@ int ipsec_sa_init(struct ipsec_sa *ipsp)
         int error = 0;
         char sa[SATOT_BUF];
 	size_t sa_len;
+#ifdef CONFIG_KLIPS_DEBUG
 	char ipaddr_txt[ADDRTOA_BUF];
 	char ipaddr2_txt[ADDRTOA_BUF];
+#endif
 #if defined (CONFIG_KLIPS_AUTH_HMAC_MD5) || defined (CONFIG_KLIPS_AUTH_HMAC_SHA1)
 	unsigned char kb[AHMD596_BLKLEN];
 	int i;
@@ -1198,6 +1198,7 @@ int ipsec_sa_init(struct ipsec_sa *ipsp)
 #ifdef CONFIG_KLIPS_IPIP
 	case IPPROTO_IPIP: {
 		ipsp->ips_xformfuncs = ipip_xform_funcs;
+#ifdef CONFIG_KLIPS_DEBUG
 		addrtoa(((struct sockaddr_in*)(ipsp->ips_addr_s))->sin_addr,
 			0,
 			ipaddr_txt, sizeof(ipaddr_txt));
@@ -1209,6 +1210,7 @@ int ipsec_sa_init(struct ipsec_sa *ipsp)
 			    "(pfkey defined) IPIP ipsec_sa set for %s->%s.\n",
 			    ipaddr_txt,
 			    ipaddr2_txt);
+#endif
 	}
 	break;
 #endif /* !CONFIG_KLIPS_IPIP */
