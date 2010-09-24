b848645e32c713554e796cef066630f4d9ddd892
Paul Wouters <paul@xelerance.com>
2010-09-24 12:57:25 -0400

    remove unused pfkey_v2_build_c_version[]

--- linux/net/ipsec/pfkey_v2_build.c
+++ linux/net/ipsec/pfkey_v2_build.c
@@ -17,8 +17,6 @@
  *		Template from klips/net/ipsec/ipsec/ipsec_parser.c.
  */
 
-char pfkey_v2_build_c_version[] = "$Id: pfkey_v2_build.c,v 1.53 2005/11/09 00:30:37 mcr Exp $";
-
 /*
  * Some ugly stuff to allow consistent debugging code for use in the
  * kernel and in user space
