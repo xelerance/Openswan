e453c360df341d72b20457bf9f89bb3c83b26b3a
Paul Wouters <paul@xelerance.com>
2010-10-13 15:56:25 -0400

    remove rcsid

--- lib/libopenswan/alloc.c
+++ lib/libopenswan/alloc.c
@@ -11,7 +11,6 @@
  * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * for more details.
  *
- * RCSID $Id: alloc.c,v 1.2 2004/10/16 23:42:13 mcr Exp $
  */
 
 #include <stdlib.h>
