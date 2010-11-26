eab580291de5a86f228edadb4d0476dd7808fa0c
Paul Wouters <paul@xelerance.com>
2010-11-26 14:19:41 -0500

    Added nat-pluto-08 and nat-pluto-09 testcase to test vnet: keyword and vnet: per-conn subnet addition.

---
+++ testing/pluto/nat-pluto-08/nicinit.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+iptables -t nat -F
+iptables -F
+
+# NAT North's IP to ours
+iptables -t nat -A POSTROUTING --source 192.1.3.0/24 --destination 0.0.0.0/0 -j SNAT --to-source 192.1.2.254
+
+# make sure that we never acidentially let ESP through.
+iptables -I FORWARD 1 --proto 50 -j DROP
+iptables -I FORWARD 2 --destination 192.0.2.0/24 -j DROP
+iptables -I FORWARD 3 --source 192.0.2.0/24 -j DROP
+
+# route
+iptables -I INPUT 1 --destination 192.0.2.0/24 -j DROP
+
+# Display the table, so we know it's correct.
+iptables -t nat -L -n
+iptables -L -n
+
+echo done.
