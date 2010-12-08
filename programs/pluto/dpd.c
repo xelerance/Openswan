194283441253199a53a290ab777c53d7a56a8fcb
Paul Wouters <paul@xelerance.com>
2010-12-07 23:33:10 -0500

    DPD: DPD_ACTION_RESTART would always execute DPD_ACTION_RESTART_BY_PEER
    A wrongly placed break statement caused these two actions to always happen.
    
    Also a cleanup of the switch statement removing checks and use a
    bad_case() instead.

--- programs/pluto/dpd.c
+++ programs/pluto/dpd.c
@@ -549,17 +549,9 @@ dpd_inR(struct state *p1st
 void
 dpd_timeout(struct state *st)
 {
-    int action;
     struct connection *c = st->st_connection;
-    action = st->st_connection->dpd_action;
+    int action = c->dpd_action;
     
-    /* probably wrong thing to assert here */
-    passert(action == DPD_ACTION_HOLD
-	    || action == DPD_ACTION_CLEAR
-	    || action == DPD_ACTION_RESTART
-	    || action == DPD_ACTION_RESTART_BY_PEER
-	    );
-        
     /** delete the state, which is probably in phase 2 */
     set_cur_connection(c);
 
@@ -610,14 +602,16 @@ dpd_timeout(struct state *st)
 	delete_event(st);
 	delete_dpd_event(st);
 	event_schedule(EVENT_SA_REPLACE, 0, st);
+	break;
 
-	case DPD_ACTION_RESTART_BY_PEER:
+    case DPD_ACTION_RESTART_BY_PEER:
 	/* dpdaction=restart_by_peer - immediately renegotiate connections to the same peer. */
 	openswan_log("DPD: Restarting all connections that share this peer");
 	restart_connections_by_peer(c);
 	break;
 
-	break;
+    default:
+	bad_case(action);
     }
     reset_cur_connection();
 }
