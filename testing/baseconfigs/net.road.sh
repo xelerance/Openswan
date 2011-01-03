c66b32a4573bde30cb499f9247dea3c9c5f2a30d
Paul Wouters <paul@xelerance.com>
2011-01-03 04:32:40 -0500

    Fix uml MAC's to have the "2nd rightmost bit" set to 1, so uml_* tools won't whine about it being a global address. The 2nd rightmost bit is the third bit from the right....

--- testing/baseconfigs/net.road.sh
+++ testing/baseconfigs/net.road.sh
@@ -3,9 +3,9 @@
 #
 if [ -n "$UML_northpublic_CTL" ]
 then
-    net_eth0="eth0=daemon,10:00:00:ab:cd:02,unix,$UML_northpublic_CTL,$UML_northpublic_DATA";
+    net_eth0="eth0=daemon,12:00:00:ab:cd:02,unix,$UML_northpublic_CTL,$UML_northpublic_DATA";
 else
-    net_eth0="eth0=mcast,10:00:00:96:96:A1,239.192.3.2,31205"
+    net_eth0="eth0=mcast,12:00:00:96:96:A1,239.192.3.2,31205"
 fi
 
 net="$net_eth0"
--- testing/baseconfigs/net.sunset.sh
+++ testing/baseconfigs/net.sunset.sh
@@ -3,12 +3,12 @@
 #
 if [ -n "$UML_west_CTL" ]
 then
-    net_eth0="eth0=daemon,10:00:00:ab:cd:01,unix,$UML_west_CTL,$UML_west_DATA";
+    net_eth0="eth0=daemon,12:00:00:ab:cd:01,unix,$UML_west_CTL,$UML_west_DATA";
 elif [ -n "$UML_private_CTL" ]
 then
-    net_eth0="eth0=daemon,10:00:00:ab:cd:01,unix,$UML_private_CTL,$UML_private_DATA";
+    net_eth0="eth0=daemon,12:00:00:ab:cd:01,unix,$UML_private_CTL,$UML_private_DATA";
 else
-    net_eth0="eth0=mcast,10:00:00:ab:cd:01,239.192.0.2,40800"
+    net_eth0="eth0=mcast,12:00:00:ab:cd:01,239.192.0.2,40800"
 fi
 
 net="$net_eth0"
