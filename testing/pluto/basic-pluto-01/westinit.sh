c02851a76db64d9baef4e9f4a6daf3d00d6ae7f2
Paul Wouters <paul@xelerance.com>
2011-03-21 03:43:59 -0400

    use LOGDROP to doubly make sure at the end of the test when we check the kernel logs, we see the packets filtered.

--- testing/pluto/basic-pluto-01/westinit.sh
+++ testing/pluto/basic-pluto-01/westinit.sh
@@ -5,7 +5,7 @@ source /testing/pluto/bin/westlocal.sh
 # confirm that the network is alive
 ping -n -c 4 192.0.2.254
 # make sure that clear text does not get through
-iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j DROP
+iptables -A INPUT -i eth1 -s 192.0.2.0/24 -j LOGDROP
 # confirm with a ping to east-in
 ping -n -c 4 192.0.2.254
 
