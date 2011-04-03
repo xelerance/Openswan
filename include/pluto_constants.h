58202792882e2c010517b788e0e689ecb9163d50
Avesh Agarwal <avagarwa@redhat.com>
2011-04-03 16:24:55 +0200

    Fix duplicate defines for various encapsulation modes (pluto vs ietf constants)
    Signed-off-by: Paul Wouters <paul@xelerance.com>

--- include/pluto_constants.h
+++ include/pluto_constants.h
@@ -556,16 +556,6 @@ enum pluto_policy {
 
 #define SA_LIFE_DURATION_K_DEFAULT  0xFFFFFFFFlu
 
-/* Encapsulation Mode attribute */
-
-#define ENCAPSULATION_MODE_UNSPECIFIED             0	/* not legal -- used internally */
-#define ENCAPSULATION_MODE_TUNNEL                  1
-#define ENCAPSULATION_MODE_TRANSPORT               2
-#define ENCAPSULATION_MODE_UDP_TUNNEL_RFC          3
-#define ENCAPSULATION_MODE_UDP_TRANSPORT_RFC       4
-#define ENCAPSULATION_MODE_UDP_TUNNEL_DRAFTS       61443
-#define ENCAPSULATION_MODE_UDP_TRANSPORT_DRAFTS    61444
-
 /* Oakley Lifetime Type attribute
  * draft-ietf-ipsec-ike-01.txt appendix A
  * As far as I can see, there is not specification for
