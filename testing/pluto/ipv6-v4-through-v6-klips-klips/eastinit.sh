75b69685900246b9960c608fa61d46da4f00de39
Paul Wouters <paul@xelerance.com>
2011-04-19 00:28:17 -0400

    Temporary workaround for the parser not understanding if we want to do ipv4 tunneling via ipv6 endpoints

--- testing/pluto/ipv6-v4-through-v6-klips-klips/eastinit.sh
+++ testing/pluto/ipv6-v4-through-v6-klips-klips/eastinit.sh
@@ -4,5 +4,12 @@ source /testing/pluto/bin/eastlocal.sh
 
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
-ipsec auto --add west-east-4in6
+
+# Temporary disabled and workaround for broken parser
+# ipsec auto --add west-east-4in6
+ipsec whack --name west-east-4in6 --ipv6 --tunnelipv4 --id @west --host 2001:db8:1:2::45 --client 192.0.1.0/24 --to --id @east --host 2001:db8:1:2::23 --client 192.0.2.0/2 --tunnel --psk --encrypt --pfs
+echo '@west @east: PSK "test"' >> /tmp/$TESTNAME/ipsec.secrets
+ipsec secrets
+# end Temp
+
 ipsec whack --debug-control --debug-controlmore --debug-crypt
