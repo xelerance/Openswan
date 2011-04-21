1eaf903840e0d47820a4f86813552a357cd184ed
Paul Wouters <paul@xelerance.com>
2011-04-21 16:28:39 -0400

    log my two failed methods for filtering unencrypted pings with netkey.

--- testing/pluto/ipv6-v6-through-v6-netkey-netkey/westinit.sh
+++ testing/pluto/ipv6-v6-through-v6-netkey-netkey/westinit.sh
@@ -6,7 +6,15 @@ source /testing/pluto/bin/westlocal.sh
 # confirm that the network is alive
 ping6 -n -c 4 -I 2001:db8:0:1::254 2001:db8:0:2::254
 # make sure that clear text does not get through
-ip6tables -A INPUT -i eth1 -s 2001:db8:0:2::254 -j DROP
+# Both these alternatives fail to block the unencrypted icmp ?
+## ip6tables -I FORWARD -o eth1 -m policy --dir out --pol ipsec -j ACCEPT
+## ip6tables -A FORWARD -o eth1 -m policy --dir out --pol none -p icmp  -j DROP
+### ip6tables -t mangle -I PREROUTING -i eth1 -p esp -j MARK --set-mark 50
+### ip6tables -t mangle -I INPUT -i eth1 -p esp -j MARK --set-mark 50
+### ip6tables -I FORWARD -m mark --mark 50 -j ACCEPT
+### ip6tables -A FORWARD -p icmp -j DROP
+### ip6tables -I INPUT -m mark --mark 50 -j ACCEPT
+### ip6tables -A INPUT -p icmp -j DROP
 # confirm with a ping to east-in
 ping6 -n -c 4 -I 2001:db8:0:1::254 2001:db8:0:2::254
 
