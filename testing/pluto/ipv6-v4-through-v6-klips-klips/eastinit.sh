1f711b281703572b876c01e6256d0c04ceb07bac
Paul Wouters <paul@xelerance.com>
2011-04-21 23:42:26 -0400

    Since ipsec.conf.common now loads properly, don't use manual whack for the 6in4 4in6 testcases anymore. I left the whack workaround in comments, it might be useful.

--- testing/pluto/ipv6-v4-through-v6-klips-klips/eastinit.sh
+++ testing/pluto/ipv6-v4-through-v6-klips-klips/eastinit.sh
@@ -5,11 +5,10 @@ source /testing/pluto/bin/eastlocal.sh
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 
-# Temporary disabled and workaround for broken parser
-# ipsec auto --add west-east-4in6
-ipsec whack --name west-east-4in6 --ipv6 --tunnelipv4 --id @west --host 2001:db8:1:2::45 --client 192.0.1.0/24 --to --id @east --host 2001:db8:1:2::23 --client 192.0.2.0/2 --tunnel --psk --encrypt --pfs
-echo '@west @east: PSK "test"' >> /tmp/$TESTNAME/ipsec.secrets
-ipsec secrets
-# end Temp
+ipsec auto --add west-east-4in6
+# Alternatively, direct whack
+# ipsec whack --name west-east-4in6 --ipv6 --tunnelipv4 --id @west --host 2001:db8:1:2::45 --client 192.0.1.0/24 --to --id @east --host 2001:db8:1:2::23 --client 192.0.2.0/2 --tunnel --psk --encrypt --pfs
+# echo '@west @east: PSK "test"' >> /tmp/$TESTNAME/ipsec.secrets
+# ipsec secrets
 
 ipsec whack --debug-control --debug-controlmore --debug-crypt
--- testing/pluto/ipv6-v4-through-v6-klips-klips/westinit.sh
+++ testing/pluto/ipv6-v4-through-v6-klips-klips/westinit.sh
@@ -12,12 +12,11 @@ ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 
-# Temporary disabled and workaround for broken parser
-# ipsec auto --add west-east-4in6
-ipsec whack --name west-east-4in6 --ipv6 --tunnelipv4 --id @west --host 2001:db8:1:2::45 --client 192.0.1.0/24 --to --id @east --host 2001:db8:1:2::23 --client 192.0.2.0/2 --tunnel --psk --encrypt --pfs 
-echo '@west @east: PSK "test"' >> /tmp/$TESTNAME/ipsec.secrets
-ipsec secrets
-# end Temp
+ipsec auto --add west-east-4in6
+# alternatively, direct whack
+# ipsec whack --name west-east-4in6 --ipv6 --tunnelipv4 --id @west --host 2001:db8:1:2::45 --client 192.0.1.0/24 --to --id @east --host 2001:db8:1:2::23 --client 192.0.2.0/2 --tunnel --psk --encrypt --pfs 
+# echo '@west @east: PSK "test"' >> /tmp/$TESTNAME/ipsec.secrets
+# ipsec secrets
 
 ipsec whack --debug-control --debug-controlmore --debug-parsing --debug-crypt
 
