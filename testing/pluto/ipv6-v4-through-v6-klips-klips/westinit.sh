37b36e400ddc9490a69857b839acb3eff5eaa717
Paul Wouters <paul@xelerance.com>
2011-04-25 19:35:12 -0400

    Fix conn name for ipv6-v4-through-v6-klips-klips

--- testing/pluto/ipv6-v4-through-v6-klips-klips/westinit.sh
+++ testing/pluto/ipv6-v4-through-v6-klips-klips/westinit.sh
@@ -12,7 +12,7 @@ ping -n -c 4 -I 192.0.1.254 192.0.2.254
 ipsec setup start
 /testing/pluto/bin/wait-until-pluto-started
 
-ipsec auto --add west-east-4in6
+ipsec auto --add westnet-eastnet-4in6
 # alternatively, direct whack
 # ipsec whack --name west-east-4in6 --ipv6 --tunnelipv4 --id @west --host 2001:db8:1:2::45 --client 192.0.1.0/24 --to --id @east --host 2001:db8:1:2::23 --client 192.0.2.0/2 --tunnel --psk --encrypt --pfs 
 # echo '@west @east: PSK "test"' >> /tmp/$TESTNAME/ipsec.secrets
