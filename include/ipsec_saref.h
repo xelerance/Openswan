bb100e0f0ad857631706e977ff76f81b2f19434d
Paul Wouters <paul@xelerance.com>
2011-10-18 20:48:13 -0400

    * update IP_IPSEC_REFINFO / IP_IPSEC_BINDREF from 22/23 to 30/31

--- include/ipsec_saref.h
+++ include/ipsec_saref.h
@@ -1,8 +1,15 @@
+/* Older patches used 22/23, but the kernel started using those, so we
+ * bumped it to 30/31. Make sure you use the same number in the kernel
+ * SAref patches and in the xl2tpd 'saref refinfo ' option
+ */
+
 #ifndef IP_IPSEC_REFINFO
-#define IP_IPSEC_REFINFO 22
+/* #define IP_IPSEC_REFINFO 22 */
+#define IP_IPSEC_REFINFO 30
 #endif
 
 #ifndef IP_IPSEC_BINDREF
-#define IP_IPSEC_BINDREF 23
+/* #define IP_IPSEC_BINDREF 23 */
+#define IP_IPSEC_BINDREF 31
 #endif
 
