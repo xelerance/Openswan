b03409f5a86740bc6a94c6da6caf0a691ca3e840
Avesh Agarwal <avagarwa@redhat.com>
2011-11-03 13:06:10 -0400

    * Add PLUTO_IS_PEER_CISCO= to updown

--- programs/pluto/kernel.c
+++ programs/pluto/kernel.c
@@ -436,6 +436,7 @@ fmt_common_shell_out(char *buf, int blen, struct connection *c
 #endif
 		    "%s "           /* PLUTO_MY_SRCIP - if any */
 #ifdef XAUTH
+		    "PLUTO_IS_PEER_CISCO='%u' "
 		    "PLUTO_CISCO_DNS_INFO='%s' "
 		    "PLUTO_CISCO_DOMAIN_INFO='%s' "
 		    "PLUTO_PEER_BANNER='%s' "
@@ -472,6 +473,7 @@ fmt_common_shell_out(char *buf, int blen, struct connection *c
 #endif
 		    , srcip_str
 #ifdef XAUTH
+		    , c->remotepeertype
 		    , c->cisco_dns_info ? c->cisco_dns_info : ""
 		    , c->cisco_domain_info ? c->cisco_domain_info : ""
 		    , c->cisco_banner ? c->cisco_banner : ""
