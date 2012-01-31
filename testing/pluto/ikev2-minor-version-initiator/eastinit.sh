8de58065845f1e74b55280f3cb7000c8e990022b
Paul Wouters <pwouters@redhat.com>
2012-01-30 21:58:59 -0500

    * Add 4 test cases for major/minor version number mismatches in IKEv2
    This is to test http://tools.ietf.org/html/rfc5996#section-2.5
    
    It uses two new options to pluto's whack:
    
    --impair-major-version-bump
    --impair-minor-version-bump
    
    These options will make pluto increase the major or minor number.
    Tests run for major/minor increase on both responder and initiator.
    
    No actual new message type for the hypothetical new version is tested.
    Once IKEv2.1 actually is releases, such a test should be added.

---
+++ testing/pluto/ikev2-minor-version-initiator/eastinit.sh
@@ -0,0 +1,9 @@
+: ==== start ====
+TESTNAME=ikev2-minor-version-initiator
+source /testing/pluto/bin/eastlocal.sh
+
+ipsec setup start
+ipsec whack --whackrecord /var/tmp/ikev2.record
+ipsec auto --add  westnet--eastnet-ikev2
+ipsec whack --debug-control --debug-controlmore --debug-crypt
+/testing/pluto/bin/wait-until-pluto-started
