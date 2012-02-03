ec23e39d96e2c636223b436aad91e6245bae7354
Paul Wouters <pwouters@redhat.com>
2012-02-03 00:42:48 -0500

    * IKEv2: Don't try to out_raw() a v2 notification payload that's empty

--- programs/pluto/ikev2_parent.c
+++ programs/pluto/ikev2_parent.c
@@ -2245,11 +2245,13 @@ bool ship_v2N (unsigned int np, u_int8_t  critical,
 
    		}
     }
-   	if (!out_raw(n_data->ptr, n_data->len, &n_pbs, "Notify data"))
-   	{
+	/* Notify could be empty */
+	if(n_data != NULL) {
+	   if (!out_raw(n_data->ptr, n_data->len, &n_pbs, "Notify data")) {
 		openswan_log("error writing notify payload for notify message");
-   		return FALSE;
-    }
+		return FALSE;
+	   }
+	}
     close_output_pbs(&n_pbs);
 	return TRUE;
 }
