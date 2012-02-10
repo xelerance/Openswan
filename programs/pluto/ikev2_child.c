0864b8720fff9c4a978918d392a356000e050334
Paul Wouters <pwouters@redhat.com>
2012-02-10 15:33:44 -0500

    * IKEv2: Fix Traffic Selector length

--- programs/pluto/ikev2_child.c
+++ programs/pluto/ikev2_child.c
@@ -162,11 +162,11 @@ stf_status ikev2_emit_ts(struct msg_digest *md   UNUSED
     switch(ts->ts_type) {
     case IKEv2_TS_IPV4_ADDR_RANGE:
 	its1.isat1_type = IKEv2_TS_IPV4_ADDR_RANGE;
-	its1.isat1_sellen = 16;
+	its1.isat1_sellen = 2*16 + 8; /* See RFC 5669 SEction 13.3.1, 8 octet header plus 2 ip addresses */
 	break;
     case IKEv2_TS_IPV6_ADDR_RANGE:
 	its1.isat1_type = IKEv2_TS_IPV6_ADDR_RANGE;
-	its1.isat1_sellen = 40;
+	its1.isat1_sellen = 2*40 + 8; /* See RFC 5669 SEction 13.3.1, 8 octet header plus 2 ip addresses */
 	break;
    case IKEv2_TS_FC_ADDR_RANGE:
 	DBG_log("IKEv2 Traffic Selector IKEv2_TS_FC_ADDR_RANGE not yet supported");
