f3aa6949f5cad08428aff152cfcaa74c5dca8b66
Paul Wouters <pwouters@redhat.com>
2012-02-15 17:47:00 -0500

    * STF_FAIL off by one fix

--- programs/pluto/ikev2_parent.c
+++ programs/pluto/ikev2_parent.c
@@ -1639,7 +1639,7 @@ ikev2_parent_inI2outR2_continue(struct pluto_crypto_req_cont *pcrc
     st->st_calculating = FALSE;
 
     e = ikev2_parent_inI2outR2_tail(pcrc, r);
-    if( e >= STF_FAIL) {
+    if( e > STF_FAIL) {
 	/* we do not send a notify because we are the initiator that could be responding to an error notification */
 	int v2_notify_num = e - STF_FAIL;
 	e = STF_FAIL;
@@ -1926,7 +1926,7 @@ ikev2_parent_inI2outR2_tail(struct pluto_crypto_req_cont *pcrc
 	if(np == ISAKMP_NEXT_v2SA) {
 	    /* must have enough to build an CHILD_SA */
 	    ret = ikev2_child_sa_respond(md, RESPONDER, &e_pbs_cipher);
-	    if(ret >= STF_FAIL) {
+	    if(ret > STF_FAIL) {
 		int v2_notify_num = ret - STF_FAIL;
 		ret = STF_FAIL;
 		DBG_log("ikev2_child_sa_respond returned STF_FAIL with %s", enum_name(&ikev2_notify_names, v2_notify_num));
