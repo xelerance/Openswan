c97e5670e217329febc3c4262b0a3d93a4d407c7
Paul Wouters <pwouters@redhat.com>
2012-03-12 11:45:18 -0400

    * IS_CHILD_SA_ESTABLISHED() now also checks for st->st_clonedfrom != SOS_NOBODY
    While this should not be neccessary there is some confusion about
    child states in IKEv2 (and IKEv1 Aggressive Mode). In IKEv2, both
    the parent sa and a child sa show up as PARENT_STATE_I3/R2.
    
    Conflicts:
    
    	include/pluto_constants.h

--- include/pluto_constants.h
+++ include/pluto_constants.h
@@ -354,9 +354,12 @@ enum phase1_role {
 #define IS_MODE_CFG_ESTABLISHED(s) ((s) == STATE_MODE_CFG_R2)
 #endif
 
-#define IS_PARENT_SA_ESTABLISHED(s) ((s) == STATE_PARENT_I2 || (s) == STATE_PARENT_R1)
-#define IS_CHILD_SA_ESTABLISHED(st) (((st->st_state) == STATE_PARENT_I3 || (st->st_state) == STATE_PARENT_R2) && (st->st_childsa != NULL))
-
+#define IS_PARENT_SA_ESTABLISHED(s) ((s) == STATE_PARENT_I2 || (s) == STATE_PARENT_R1 || (s) == STATE_IKESA_DEL)
+/* 
+ * Issue here is that our child sa appears as a STATE_PARENT_I3/STATE_PARENT_R2 state which it should not 
+ * So we fall back to checking if it is cloned, and therefor really a child
+ */
+#define IS_CHILD_SA_ESTABLISHED(st) ( (((st->st_state == STATE_PARENT_I3) || (st->st_state == STATE_PARENT_R2)) && (st->st_clonedfrom != SOS_NOBODY)) || (st->st_state == STATE_CHILDSA_DEL) )
 
 #define IS_CHILD_SA(st)  ((st)->st_clonedfrom != SOS_NOBODY)
 #define IS_PARENT_SA(st) (!IS_CHILD_SA(st))
