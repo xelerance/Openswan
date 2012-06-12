080d189f49d1ed9366a854fe39f8b2cd200512fe
Paul Wouters <paul@libreswan.org>
2012-06-11 20:46:39 -0400

    Redone and simplified functions around strncat/snprintf for addrtot.c

--- linux/net/ipsec/addrtot.c
+++ linux/net/ipsec/addrtot.c
@@ -77,14 +77,12 @@ size_t dstlen;
 	char buf[1+ADDRTOT_BUF+1];	/* :address: */
 	char *p;
 	int t = addrtypeof(src);
-
-	passert(dstlen >= sizeof("<invalid>"));
 #	define	TF(t, f)	(((t)<<8) | (f))
 
 	n = addrbytesptr(src, &b);
 	if (n == 0) {
 	  dst[0]='\0';
-	  strncat(dst, "<invalid>", dstlen -1);
+	  strncat(dst, "<invalid>", dstlen -1); /* we hope possible truncation does not cause problems */
 	  return sizeof("<invalid>");
 	}
 
@@ -109,7 +107,7 @@ size_t dstlen;
 		break;
 	default:		/* including (AF_INET, 'R') */
 		dst[0]='\0';
-		strncat(dst, "<invalid>", dstlen - 1);
+		strncat(dst, "<invalid>", dstlen - 1); /* we hope possible truncation does not cause problems */
 		return sizeof("<invalid>");
 	}
 
@@ -136,7 +134,6 @@ size_t dstlen;
 	char buf[1+ADDRTOT_BUF+1];	/* :address: */
 	char *p;
 
-	passert(dstlen >= sizeof("<invalid>"));
 #	define	TF(t, f)	(((t)<<8) | (f))
 
 	switch (t) {
@@ -144,7 +141,7 @@ size_t dstlen;
 	case AF_INET6: n = IP6BYTES; break;
 	default:
 	  dst[0]='\0';
-	  strncat(dst, "<invalid>", dstlen - 1);
+	  strncat(dst, "<invalid>", dstlen - 1); /* we hope possible truncation does not cause problems */
 	  return sizeof("<invalid>");
 	}
 
@@ -169,7 +166,7 @@ size_t dstlen;
 		break;
 	default:		/* including (AF_INET, 'R') */
 	  	dst[0]='\0';
-	  	strncat(dst, "<invalid>", dstlen - 1);
+	  	strncat(dst, "<invalid>", dstlen - 1); /* we hope possible truncation does not cause problems */
 	  	return sizeof("<invalid>");
 	}
 
@@ -197,8 +194,6 @@ size_t dstlen;
 		struct sockaddr_in6 sin6;
 	} *sinp = (const union SINSIN6 *) src;
 
-	passert(dstlen >= sizeof("<invalid>"));
-
 	switch (sinp->sin.sin_family) {
 	case AF_INET:
 		return inet_addrtot(AF_INET,&sinp->sin.sin_addr,format,dst,dstlen);
@@ -206,7 +201,7 @@ size_t dstlen;
 		return inet_addrtot(AF_INET6,&sinp->sin6.sin6_addr,format,dst,dstlen);
 	default:
 		dst[0]='\0';
-		strncat(dst, "<invalid>", dstlen - 1);
+		strncat(dst, "<invalid>", dstlen - 1); /* we hope possible truncation does not cause problems */
 		return sizeof("<invalid>");
 	}
 }
