24911adb8e29e40d7de469757a3d302ec8f0d046
Michael Richardson <mcr@credil.org>
2013-05-23 11:42:07 -0400

    fix occurances of initrd.uml to initrd.cpio to express dependancy properly.  make -j4 discovers this problem

--- testing/utils/uml-functions.sh
+++ testing/utils/uml-functions.sh
@@ -38,8 +38,8 @@ setup_make() {
     fi
 
     # now describe how to build the initrd.
-    echo "initrd.uml: ${OPENSWANSRCDIR}/testing/utils/initrd-`uname -m`.list"
-    echo "$TAB fakeroot ${OPENSWANSRCDIR}/testing/utils/buildinitrd ${OPENSWANSRCDIR}/testing/utils/initrd-`uname -m`.list ${OPENSWANSRCDIR} ${BASICROOT}" 
+    echo "initrd.cpio: ${OPENSWANSRCDIR}/testing/utils/initrd-`uname -m`.list"
+    echo "$TAB fakeroot ${OPENSWANSRCDIR}/testing/utils/buildinitrd ${OPENSWANSRCDIR}/testing/utils/initrd-`uname -m`.list ${OPENSWANSRCDIR} ${BASICROOT}"
 }
 
 # output should directed to a Makefile
@@ -208,7 +208,7 @@ setup_host_make() {
 	    echo "$TAB echo . ${TESTINGROOT}/baseconfigs/net.$host.sh   >>$startscript"
 	    echo "$TAB echo ''          >>$startscript"
 	    echo "$TAB # the umlroot= is a local hack >>$startscript"
-	    # echo "$TAB echo '$POOLSPACE/plain${KERNVER}/linux load_ramdisk=1 ramdisk_size=98304 mem=256M initrd=$POOLSPACE/initrd.uml umlroot=$POOLSPACE/$hostroot testname=$TESTNAME root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT  selinux=0 init=/linuxrc gim\$\$*' >>$startscript"
+	    # echo "$TAB echo '$POOLSPACE/plain${KERNVER}/linux load_ramdisk=1 ramdisk_size=98304 mem=256M initrd=$POOLSPACE/initrd.cpio umlroot=$POOLSPACE/$hostroot testname=$TESTNAME root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT  selinux=0 init=/linuxrc gim\$\$*' >>$startscript"
 	    echo "$TAB echo '$POOLSPACE/plain${KERNVER}/linux initrd=$POOLSPACE/initrd.cpio rootfstype=hostfs umlroot=$POOLSPACE/$hostroot root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT  rdinit=/linuxrc gim\$\$*' >>$startscript"
 	    echo "$TAB chmod +x $startscript"
 	    echo
@@ -220,14 +220,14 @@ setup_host_make() {
     then 
      # make startup script for NETKEY uml (no modules)
      startscript=$POOLSPACE/$host/start-netkey.sh
-     echo "$startscript : $OPENSWANSRCDIR/umlsetup.sh initrd.uml"
+     echo "$startscript : $OPENSWANSRCDIR/umlsetup.sh initrd.cpio"
      echo "$TAB echo '#!/bin/bash' >$startscript"
      echo "$TAB echo ''          >>$startscript"
      echo "$TAB echo '# get $net value from baseconfig'          >>$startscript"
      echo "$TAB echo . ${TESTINGROOT}/baseconfigs/net.$host.sh   >>$startscript"
      echo "$TAB echo ''          >>$startscript"
      echo "$TAB # the umlroot= is a local hack >>$startscript"
-     #echo "$TAB echo '$NETKEY_KERNEL load_ramdisk=1 ramdisk_size=98304 mem=256M initrd=$POOLSPACE/initrd.uml rootfstype=hostfs umlroot=$POOLSPACE/$hostroot testname=$TESTNAME root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT  selinux=0 init=/linuxrc \$\$*' >>$startscript"
+     #echo "$TAB echo '$NETKEY_KERNEL load_ramdisk=1 ramdisk_size=98304 mem=256M initrd=$POOLSPACE/initrd.cpio rootfstype=hostfs umlroot=$POOLSPACE/$hostroot testname=$TESTNAME root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT  selinux=0 init=/linuxrc \$\$*' >>$startscript"
      echo "$TAB echo '$NETKEY_KERNEL initrd=$POOLSPACE/initrd.cpio rootfstype=hostfs umlroot=$POOLSPACE/$hostroot root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT  rdinit=/linuxrc \$\$*' >>$startscript"
      echo "$TAB echo 'if [ -n \"\$\$UML_SLEEP\" ]; then eval \$\$UML_SLEEP; fi'  >>$startscript"
      echo "$TAB chmod +x $startscript"
@@ -243,7 +243,7 @@ setup_host_make() {
     echo "$TAB echo . ${TESTINGROOT}/baseconfigs/net.$host.sh   >>$startscript"
     echo "$TAB echo ''          >>$startscript"
     echo "$TAB # the umlroot= is a local hack >>$startscript"
-    #echo "$TAB echo '$KERNEL load_ramdisk=1 ramdisk_size=98304 mem=256M initrd=$POOLSPACE/initrd.uml umlroot=$POOLSPACE/$hostroot testname=$TESTNAME root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT selinux=0  init=/linuxrc \$\$*' >>$startscript"
+    #echo "$TAB echo '$KERNEL load_ramdisk=1 ramdisk_size=98304 mem=256M initrd=$POOLSPACE/initrd.cpio umlroot=$POOLSPACE/$hostroot testname=$TESTNAME root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT selinux=0  init=/linuxrc \$\$*' >>$startscript"
     echo "$TAB echo '$KERNEL initrd=$POOLSPACE/initrd.cpio rootfstype=hostfs umlroot=$POOLSPACE/$hostroot root=/dev/ram0 rw ssl=pty umid=$host \$\$net \$\$UML_DEBUG_OPT \$\$UML_"${host}"_OPT  rdinit=/linuxrc \$\$*' >>$startscript"
     echo "$TAB echo 'if [ -n \"\$\$UML_SLEEP\" ]; then eval \$\$UML_SLEEP; fi'  >>$startscript"
     echo "$TAB chmod +x $startscript"
