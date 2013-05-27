621840a2f35eb6cf899a0fe235993feb37cacfb4
Michael Richardson <mcr@sandelman.ca>
2013-05-27 14:49:14 -0400

    change mac address back to 10:

--- testing/baseconfigs/net.pole.sh
+++ testing/baseconfigs/net.pole.sh
@@ -3,9 +3,9 @@
 #
 if [ -n "$UML_north_CTL" ]
 then
-    net_eth0="eth0=daemon,12:00:00:de:cd:01,unix,$UML_east_CTL,$UML_east_DATA";
+    net_eth0="eth0=daemon,10:00:00:de:cd:01,unix,$UML_east_CTL,$UML_east_DATA";
 else
-    net_eth0="eth0=mcast,12:00:00:de:cd:49,239.192.0.3,40810";
+    net_eth0="eth0=mcast,10:00:00:de:cd:49,239.192.0.3,40810";
 fi
 
 net="$net_eth0"
