libreswan/libreswan

* Don't refer to NETKEY as "2.6" or "experimental code"

programs/pluto/kernel.c
@@ -2224,12 +2224,12 @@ init_kernel(void)
     case USE_NETKEY:
 	if (stat("/proc/net/pfkey", &buf) == 0) {
 	    kern_interface = USE_NETKEY;
-	    libreswan_log("Using Linux 2.6 IPsec interface code on %s (experimental code)"
+	    libreswan_log("Using Linux XFRM/NETKEY IPsec interface code on %s"
 			 , kversion);
 	    kernel_ops = &netkey_kernel_ops;
 	    break;
 	} else
-	    libreswan_log("No Kernel NETKEY interface detected");
+	    libreswan_log("No Kernel XFRM/NETKEY interface detected");
 	/* FALL THROUGH */
 #endif
 

xelerance/Openswan

Conflicts:
	programs/pluto/kernel.c
	testing/pluto/netkey-pluto-01/west-console.txt
	testing/pluto/netkey-pluto-02/east-console.txt
	testing/pluto/netkey-pluto-02/west-console.txt
	testing/pluto/netkey-pluto-03-sourceip/east-console.txt
	testing/pluto/netkey-pluto-03-sourceip/west-console.txt
	testing/pluto/netkey-pluto-04/east-console.txt
	testing/pluto/netkey-pluto-04/west-console.txt
	testing/pluto/netkey-pluto-05/west-console.txt

programs/pluto/kernel.c
@@ -2261,12 +2261,12 @@ init_kernel(void)
     case USE_NETKEY:
 	if (stat("/proc/net/pfkey", &buf) == 0) {
 	    kern_interface = USE_NETKEY;
-	    openswan_log("Using Linux 2.6 IPsec interface code on %s (experimental code)"
+	    openswan_log("Using Linux XFRM/NETKEY IPsec interface code on %s"
 			 , kversion);
 	    kernel_ops = &netkey_kernel_ops;
 	    break;
 	} else
-	    openswan_log("No Kernel NETKEY interface detected");
+	    openswan_log("No Kernel XFRM/NETKEY interface detected");
 	/* FALL THROUGH */
 #endif
 