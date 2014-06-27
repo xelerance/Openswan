e743c52bb5a10dc7b92659a6987fdb9d90f930ef
Michael Richardson <mcr@credil.org>
2014-06-27 13:14:03 -0400

    buildlin should warn if WERROR set, and look for missing gmp.h

--- buildlin.sh
+++ buildlin.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
+if [ ! -f /usr/include/gmp.h ];
+then
+    echo You need to install libgmp-dev.
+    echo "    apt-get install libgmp-dev"
+    echo "or  yum install gmp-dev"
+    exit 1
+fi
+
+if [ -n "$WERROR" ];
+then
+    echo "Warning, you have \$WERROR set. I assume you are a developer"
+    sleep 5
+fi    
+
 make USE_OBJDIR=true programs
