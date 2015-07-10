bb08808a162e193e738e1c82c7cf55d729c23e95
Michael Richardson <mcr@credil.org>
2015-07-10 13:59:47 -0400

    initial test case for policy where end point can as for self/32 (or self/128) when right=%any

---
+++ tests/unit/libpluto/lp14-initiateself/initiateselfI1.c
@@ -0,0 +1,35 @@
+/* repeats existing test case */
+#include "../lp02-parentI1/parentI1_head.c"
+#include "../seam_host_rw.c"
+
+static void init_local_interface(void)
+{
+    init_rw_interface();
+}
+
+static void init_fake_secrets(void)
+{
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/rw.secrets"
+			       , NULL);
+}
+#include "../lp02-parentI1/parentI1_main.c"
+
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
+
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
