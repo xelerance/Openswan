86b48e0a7ef9f19094897d2ea9be5d20e65b79d8
Michael Richardson <mcr@credil.org>
2015-07-13 18:27:30 -0400

    test case for child policy is self/32

---
+++ tests/unit/libpluto/lp17-childselfpolicy/respondselfR2.c
@@ -0,0 +1,26 @@
+#include "../lp12-parentR2/parentR2_head.c"
+
+#define TESTNAME "respondselfR2"
+
+static void init_local_interface(void)
+{
+    init_jamesjohnson_interface();
+}
+
+static void init_fake_secrets(void)
+{
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/jj.secrets"
+			       , NULL);
+}
+
+#include "../lp12-parentR2/parentR2_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
