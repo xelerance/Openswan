94709d63f8880979d5c6bc23bcdb21c036d48680
Michael Richardson <mcr@credil.org>
2015-07-29 09:40:31 -0400

    initial take on dave + carol responder

---
+++ tests/unit/libpluto/lp24-certreplydaveR1/certreplydaveR2.c
@@ -0,0 +1,32 @@
+#include "../lp12-parentR2/parentR2_head.c"
+#include "seam_host_jamesjohnson.c"
+#include "seam_x509_list.c"
+
+#define TESTNAME "certreplyselfR2"
+
+static void init_local_interface(void)
+{
+    init_jamesjohnson_interface();
+}
+
+static void init_fake_secrets(void)
+{
+    prompt_pass_t pass;
+    memset(&pass, 0, sizeof(pass));
+    osw_init_ipsecdir("../samples/gatewaycert");
+
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/gateway.secrets"
+			       , &pass);
+}
+
+#include "../lp12-parentR2/parentR2_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
