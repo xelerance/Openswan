adb5014b79188fb7f52d1cefeac8b947e7d54a6e
Michael Richardson <mcr@credil.org>
2015-08-08 22:12:28 -0400

    added seam for kick_adns_connection_lookup

--- tests/unit/libpluto/seam_dnskey.c
+++ tests/unit/libpluto/seam_dnskey.c
@@ -2,4 +2,7 @@ void gw_addref(struct gw_info *gw) {}
 void gw_delref(struct gw_info **gwp) {}
 
 bool in_pending_use(struct connection *c) { return FALSE; }
+void kick_adns_connection_lookup(struct connection *c UNUSED
+                                 , struct end *end UNUSED) {}
+
 
