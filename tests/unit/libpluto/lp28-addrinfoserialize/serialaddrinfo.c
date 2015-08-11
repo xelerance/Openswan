c0b2bbdda775ad34b1c9f0d7120e17e9c2bb6d08
Michael Richardson <mcr@credil.org>
2015-08-11 15:15:38 -0400

    refactor dump_addr_info debug into seperate file

--- tests/unit/libpluto/lp28-addrinfoserialize/serialaddrinfo.c
+++ tests/unit/libpluto/lp28-addrinfoserialize/serialaddrinfo.c
@@ -38,22 +38,7 @@ int warningsarefatal = 0;
 
 #define TESTNAME "adnstest"
 
-void dump_addr_info(struct addrinfo *ans)
-{
-    unsigned int ansnum = 0;
-    while(ans) {
-        char addrbuf[ADDRTOT_BUF];
-        printf("ans %03u canonname=%s protocol=%u family=%u len=%u\n"
-               , ansnum, ans->ai_canonname, ans->ai_protocol, ans->ai_family, ans->ai_addrlen);
-        if(ans->ai_addrlen && ans->ai_addr) {
-            sin_addrtot(ans->ai_addr, 0, addrbuf, sizeof(addrbuf));
-            printf("        result=%s\n", addrbuf);
-        }
-
-        ans = ans->ai_next;
-        ansnum++;
-    }
-}
+#include "seam_dumpaddrinfo.c"
 
 main(int argc, char *argv[])
 {
