f949810240f2dc6d75716dd1da1973a1f3e373f3
Michael Richardson <mcr@sandelman.ca>
2015-10-09 11:45:25 -0400

    add test case for dns delayed, when there is no hint

---
+++ tests/unit/libpluto/lp33-dnsload2/dnscpeI1.c
@@ -0,0 +1,30 @@
+#include "../lp02-parentI1/parentI1_head.c"
+#include "seam_x509.c"
+#include "seam_keys.c"
+
+#include "seam_host_parker.c"
+#define TESTNAME "parentI1"
+
+static void init_local_interface(void)
+{
+    init_parker_interface();
+}
+
+static void init_fake_secrets(void)
+{
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/parker.secrets"
+			       , NULL);
+}
+
+#include "../lp02-parentI1/parentI1_main.c"
+
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
