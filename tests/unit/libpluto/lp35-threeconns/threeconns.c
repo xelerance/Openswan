145f7bdbe9671a7ab1e24ac5e0ca3892c019f7a9
Michael Richardson <mcr@credil.org>
2015-10-22 13:37:39 -0400

    attempt to reproduce crash when loading three conns

---
+++ tests/unit/libpluto/lp35-threeconns/threeconns.c
@@ -0,0 +1,51 @@
+#include "../lp02-parentI1/parentI1_head.c"
+#include "seam_x509.c"
+#include "seam_keys.c"
+
+#define TESTNAME "threeconns"
+
+struct iface_dev  vzhost_ifd1 = {
+	.id_count = 1,
+	.id_vname = "ipsec0",
+	.id_rname = "eth0"
+};
+
+struct iface_port vzhost_if1 = {
+	.ip_dev = &vzhost_ifd1,
+	.port   = 500,
+	.ip_addr.u.v4.sin_family = AF_INET,
+	.ip_addr.u.v4.sin_addr.s_addr = 0xADE68547, /* 173.230.133.71 -- see htonl() below */
+	.fd     = -1,
+	.next   = NULL,
+	.ike_float = 0,
+	.change    = IFN_KEEP
+};
+
+static void init_local_interface(void)
+{
+    vzhost_if1.ip_addr.u.v4.sin_addr.s_addr = htonl(vzhost_if1.ip_addr.u.v4.sin_addr.s_addr);
+    vzhost_if1.next = interfaces;
+    interfaces = &vzhost_if1;
+}
+
+static void init_fake_secrets(void)
+{
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/parker.secrets"
+			       , NULL);
+}
+
+#define SKIP_ORIENT_ASSERT 1
+#define SKIP_INITIATE      1
+
+#include "../lp02-parentI1/parentI1_main.c"
+
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
