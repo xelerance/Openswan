c51a794598d5c0970080b883e94d45c156cf3f8b
Michael Richardson <mcr@credil.org>
2016-04-11 13:50:00 -0400

    added appropriate seams for responding to ikev1 messages, when no ikev1 permitted

--- tests/unit/libpluto/lp44-h2hR1-deny-ikev1/h2hR1-deny-ikev1.c
+++ tests/unit/libpluto/lp44-h2hR1-deny-ikev1/h2hR1-deny-ikev1.c
@@ -1,10 +1,20 @@
+#define INCLUDE_IKEV1_PROCESSING
 /* repeats existing test case */
 #include "../lp08-parentR1/parentR1_head.c"
 #include "seam_x509.c"
-#include "../seam_host_jamesjohnson.c"
+#include "seam_host_jamesjohnson.c"
+#include "seam_ikev1_phase2.c"
+#include "seam_ikev1_crypto.c"
+#include "seam_natt_vid.c"
+#include "seam_rsa_check.c"
+#include "seam_dpd.c"
+#include "seam_command.c"
+#include "seam_ikev1_aggr.c"
+#include "seam_unpend.c"
 
+#define TESTNAME "h2h-deny-ikev1"
 
-#define TESTNAME "respondselfR1"
+bool no_cr_send = 0;
 
 static void init_local_interface(void)
 {
