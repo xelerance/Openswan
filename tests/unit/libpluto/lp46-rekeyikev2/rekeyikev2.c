445d6038a08543c020e333ee0c8d1dc249d9af57
Michael Richardson <mcr@credil.org>
2016-05-02 13:50:41 -0400

    initial test case base for rekey experiment

---
+++ tests/unit/libpluto/lp46-rekeyikev2/rekeyikev2.c
@@ -0,0 +1,13 @@
+#include "../lp13-parentI3/parentI3_head.c"
+
+#define TESTNAME "rekeyikev2"
+
+#include "../lp13-parentI3/parentI3_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
