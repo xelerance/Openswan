c24e6c8c170ac0b50477fd259f0f93bd9e4b4b19
Michael Richardson <mcr@credil.org>
2016-05-06 14:25:27 -0400

    include IKEv2 states in IS_ISAKMP_SA_ESTABLISHED

--- include/pluto_constants.h
+++ include/pluto_constants.h
@@ -323,20 +323,16 @@ enum phase1_role {
 #define IS_PHASE15(s) (STATE_XAUTH_R0 <= (s) && (s) <= STATE_XAUTH_I1)
 #define IS_QUICK(s) (STATE_QUICK_R0 <= (s) && (s) <= STATE_QUICK_R2)
 #define IS_ISAKMP_ENCRYPTED(s)     (STATE_MAIN_R2 <= (s) && STATE_AGGR_R0!=(s) && STATE_AGGR_I1 != (s) && STATE_INFO != (s))
-#define IS_ISAKMP_AUTHENTICATED(s) (STATE_MAIN_R3 <= (s) && STATE_AGGR_R0!=(s) && STATE_AGGR_I1 != (s))
-#define IS_ISAKMP_SA_ESTABLISHED(s) ((s) == STATE_MAIN_R3 || (s) == STATE_MAIN_I4 \
-				  || (s) == STATE_AGGR_I2 || (s) == STATE_AGGR_R2 \
-				  || (s) == STATE_XAUTH_R0 || (s) == STATE_XAUTH_R1 \
-				  || (s) == STATE_MODE_CFG_R0 || (s) == STATE_MODE_CFG_R1 \
-				  || (s) == STATE_MODE_CFG_R2 || (s) == STATE_MODE_CFG_I1 \
-                                  || (s) == STATE_XAUTH_I0 || (s) == STATE_XAUTH_I1)
+#define IS_ISAKMP_AUTHENTICATED(s) ((STATE_MAIN_R3 <= (s) && STATE_AGGR_R0!=(s) && STATE_AGGR_I1 != (s)) || STATE_PARENT_I3 == (s) || STATE_PARENT_R2)
+#define IS_ISAKMP_SA_ESTABLISHED(s) LHAS(ISAKMP_SA_ESTABLISHED_STATES, (s))
 #define ISAKMP_SA_ESTABLISHED_STATES  (LELEM(STATE_MAIN_R3) | \
 				       LELEM(STATE_MAIN_I4) | \
 				       LELEM(STATE_AGGR_I2) | \
 				       LELEM(STATE_AGGR_R2) | \
 				       LELEM(STATE_XAUTH_R0) | \
 				       LELEM(STATE_XAUTH_R1) | \
-				       LELEM(STATE_MODE_CFG_R0) | \
+                                       LELEM(STATE_PARENT_I3) | LELEM(STATE_PARENT_R2) | \
+				       LELEM(STATE_MODE_CFG_R0) |       \
 				       LELEM(STATE_MODE_CFG_R1) | \
 				       LELEM(STATE_MODE_CFG_R2) | \
 				       LELEM(STATE_MODE_CFG_I1) | \
