a1cc9fc7c9a87339e9c712e2cca749234e32f177
Michael Richardson <mcr@credil.org>
2016-06-20 14:23:48 -0400

    move pcap_recv_packet to per-test .c file, as per lp13, and update for reduced debugging in setup portion

--- tests/unit/libpluto/lp46-rekeyikev2-I1/rekeyikev2-I1.c
+++ tests/unit/libpluto/lp46-rekeyikev2-I1/rekeyikev2-I1.c
@@ -1,7 +1,36 @@
 #include "../lp13-parentI3/parentI3_head.c"
 
+static void init_loaded(void)
+{   /* nothing */ }
+
 #define TESTNAME "rekeyikev2"
 #define AFTER_CONN rekeyit
+
+/* this is replicated in the unit test cases since the patching up of the crypto values is case specific */
+void recv_pcap_packet(u_char *user
+		      , const struct pcap_pkthdr *h
+		      , const u_char *bytes)
+{
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    recv_pcap_packet_gen(user, h, bytes);
+
+    /* find st involved */
+    st = state_with_serialno(1);
+    if(st != NULL) {
+        passert(st != NULL);
+        st->st_connection->extra_debugging = DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE|DBG_CRYPT|DBG_PRIVATE;
+    }
+
+    run_continuation(crypto_req);
+}
+
+#define PCAP_INPUT_COUNT 2
+recv_pcap recv_inputs[PCAP_INPUT_COUNT]={
+    recv_pcap_packet,
+    recv_pcap_packet,
+};
 
 void rekeyit()
 {
