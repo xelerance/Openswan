f7cef4a6c24a7e970046e232727774257c7d71bd
Michael Richardson <mcr@sandelman.ca>
2017-01-15 23:06:16 -0500

    updated self-signed certificates

--- tests/unit/libpluto/samples/selfsigned/generate-certs.sh
+++ tests/unit/libpluto/samples/selfsigned/generate-certs.sh
@@ -7,7 +7,6 @@ set -x
 for HOST in moon dave
 do
     openssl genrsa -out private/${HOST}Key.pem 2048
-    (echo "CA"; echo "Ontario"; echo "Ottawa"; echo "Xelerance Corporation"; echo "Testing Devision"; echo $HOST.openswan.org; echo "testing@xelerance.com"; echo; echo; echo; echo ) | openssl req -new -sha256 -key private/${HOST}Key.pem -out private/$HOST.req
-    openssl req -x509 -deys 1024 -in private/$HOST.req -key private/${HOST}Key.pem -out certs/${HOST}Cert.pem
+    (echo "CA"; echo "Ontario"; echo "Ottawa"; echo "Xelerance Corporation"; echo "Testing Devision"; echo $HOST.openswan.org; echo "testing@xelerance.com"; echo; echo; echo; echo ) | openssl req -x509 -days 1024 -in private/$HOST.req -key private/${HOST}Key.pem -out certs/${HOST}Cert.pem
 done
 
