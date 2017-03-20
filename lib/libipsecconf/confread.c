3277bce8fb7665d79ac64d120d00ab292c2896cf
Michael Richardson <mcr@credil.org>
2017-03-20 15:41:34 -0400

    wo #5451 - use correct enum type for str2pubkey, so that ckaid gets calculated

--- lib/libipsecconf/confread.c
+++ lib/libipsecconf/confread.c
@@ -537,26 +537,26 @@ static bool validate_end(struct starter_conn *conn_st
 
         case PUBKEY_PREEXCHANGED:
 	    /* validate the KSCF_RSAKEY1/RSAKEY2 */
-	    if(end->strings[KSCF_RSAKEY1] != NULL)
+	    if(end->strings_set[KSCF_RSAKEY1])
 	    {
 		char *value = end->strings[KSCF_RSAKEY1];
                 osw_public_key opk1;
 
                 pfreeany(end->rsakey1);
                 end->rsakey1 = (unsigned char *)clone_str(value,"end->rsakey1");
-                if(str2pubkey(end->rsakey1, PPK_RSA, &opk1) == NULL) {
+                if(str2pubkey(end->rsakey1, PUBKEY_ALG_RSA, &opk1) == NULL) {
                     end->rsakey1_ckaid = clone_str(opk1.key_ckaid, "end->rsakey2_ckaid");
                     free_public_key(&opk1);
                 }
 	    }
-	    if(end->strings[KSCF_RSAKEY2] != NULL)
+	    if(end->strings_set[KSCF_RSAKEY2])
 	    {
 		char *value = end->strings[KSCF_RSAKEY2];
                 osw_public_key opk2;
 
                 pfreeany(end->rsakey2);
                 end->rsakey2 = (unsigned char *)clone_str(value,"end->rsakey2");
-                if(str2pubkey(end->rsakey2, PPK_RSA, &opk2) == NULL) {
+                if(str2pubkey(end->rsakey2, PUBKEY_ALG_RSA, &opk2) == NULL) {
                     end->rsakey2_ckaid = clone_str(opk2.key_ckaid, "end->rsakey2_ckaid");
                     free_public_key(&opk2);
                 }
