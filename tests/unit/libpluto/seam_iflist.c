8777cd1f146b2e1e32c6cacba7bf592ce5bca0e5
Michael Richardson <mcr@credil.org>
2017-04-11 14:10:43 -0400

    move creation of 6 interfaces into data structures so that it can be permutted better

--- tests/unit/libpluto/seam_iflist.c
+++ tests/unit/libpluto/seam_iflist.c
@@ -1,8 +1,12 @@
-void add_if_to_list(int  family,
-                    char *ifaddr,
-                    unsigned int  port,
-                    bool ike_float,
-                    char *devname)
+struct ifmake {
+  int  family;
+  char *ifaddr;
+  unsigned int  port;
+  bool ike_float;
+  char *devname;
+};
+
+void add_if_to_list(struct ifmake *ifm)
 {
   static unsigned int virtnum = 0;
   char buf1[256];
@@ -11,32 +15,38 @@ void add_if_to_list(int  family,
   struct iface_port *ifport = alloc_thing(*ifport, "device port");
   snprintf(buf1, sizeof(buf1), "ipsec%u", ++virtnum);
   dev->id_vname = clone_str(buf1, "vname");
-  dev->id_rname = clone_str(devname, "rname");
+  dev->id_rname = clone_str(ifm->devname, "rname");
 
   ifport->ip_dev = dev;
-  ifport->port   = port;
-  ifport->socktypename = (family == AF_INET6 ? "AF_INET" : "AF_INET6");
+  ifport->port   = ifm->port;
+  ifport->socktypename = (ifm->family == AF_INET6 ? "AF_INET" : "AF_INET6");
 
-  ttoaddr_num(ifaddr, 0, family, &ifport->ip_addr);
+  ttoaddr_num(ifm->ifaddr, 0, ifm->family, &ifport->ip_addr);
   ifport->fd = -1;
-  ifport->ike_float = ike_float;
+  ifport->ike_float = ifm->ike_float;
   ifport->change = IFN_KEEP;
 
   ifport->next = interfaces;
   interfaces = ifport;
 }
 
+
+struct ifmake if_01 = {AF_INET, "93.184.216.34", 500, FALSE, "eth0"};
+struct ifmake if_02 = {AF_INET6, "2606:2800:220:1:248:1893:25c8:1946", 500, FALSE, "eth0"};
+struct ifmake if_03 = {AF_INET, "127.0.0.1", 500, FALSE, "lo"};
+struct ifmake if_04 = {AF_INET6, "::1", 500, FALSE, "lo"};
+struct ifmake if_05 = {AF_INET, "132.213.238.7", 500, FALSE, "eth1"};
+struct ifmake if_06 = {AF_INET6, "fd68:c9f9:4157::1234", 500, FALSE, "eth3"};
+
+
 void init_gatefun_interface(void)
 {
-  add_if_to_list(AF_INET, "93.184.216.34", 500, FALSE, "eth0");
-  add_if_to_list(AF_INET6, "2606:2800:220:1:248:1893:25c8:1946", 500, FALSE, "eth0");
-
-  add_if_to_list(AF_INET, "127.0.0.1", 500, FALSE, "lo");
-  add_if_to_list(AF_INET6, "::1", 500, FALSE, "lo");
-
-  add_if_to_list(AF_INET, "132.213.238.7", 500, FALSE, "eth1");
-  add_if_to_list(AF_INET6, "fd68:c9f9:4157::1234", 500, FALSE, "eth3");
-
+  add_if_to_list(&if_01);
+  add_if_to_list(&if_02);
+  add_if_to_list(&if_03);
+  add_if_to_list(&if_04);
+  add_if_to_list(&if_05);
+  add_if_to_list(&if_06);
 }
 
 
