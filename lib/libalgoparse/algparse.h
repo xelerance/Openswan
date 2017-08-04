42668a6953c21db9d6e016a6ee735c18ce089cb7
Michael Richardson <mcr@credil.org>
2017-08-04 17:22:20 -0400

    wo#6269 . algorithm parse deals with legacy ikev1-3-tuples properly

---
+++ lib/libalgoparse/algparse.h
@@ -0,0 +1,47 @@
+
+/*
+ *	Creates a new alg_info by parsing passed string
+ */
+struct parser_context {
+  unsigned state, old_state;
+  unsigned protoid;
+  char ealg_buf[16];
+  char aalg_buf[16];
+  char prfalg_buf[16];
+  char modp_buf[16];
+  int (*ealg_getbyname)(const char *const str, int len);
+  int (*aalg_getbyname)(const char *const str, int len);
+  int (*prfalg_getbyname)(const char *const str, int len);
+  int (*modp_getbyname)(const char *const str, int len);
+  char *ealg_str;
+  char *aalg_str;
+  char *prfalg_str;
+  char *modp_str;
+  int eklen;
+  int aklen;
+  bool ealg_permit;
+  bool aalg_permit;
+  int ch;
+  const char *err;
+};
+
+enum parser_state_esp {
+        ST_INI,         /* parse esp= string */
+	ST_INI_AA,      /* parse ah= string */
+	ST_EA,		/* encrypt algo   */
+	ST_EA_END,
+	ST_EK,		/* enc. key length */
+	ST_EK_END,
+	ST_AA,		/* auth algo */
+	ST_AA_END,
+	ST_AK,		/* auth. key length */
+	ST_AK_END,
+        ST_PRF,
+        ST_PRF_END,
+	ST_MODP,	/* modp spec */
+	ST_FLAG_STRICT,
+	ST_END,
+	ST_EOF,
+	ST_ERR
+};
+
