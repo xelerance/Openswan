3423fa2757831a143de030b162c8bd7fd5b2fda4
Michael Richardson <mcr@credil.org>
2017-08-07 13:32:52 -0400

    wo#6269 . added unit test case for rfc4307bis algorithm lists

---
+++ tests/unit/libalgoparse/ap02-algonames/algonames.c
@@ -0,0 +1,102 @@
+/*
+ * test case for
+ *    converting from text form of ike=
+ *    to a structure to represent it.
+ *
+ * Copyright (C) 2017 Michael Richardson <mcr@xelerance.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Library General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.  See <http://www.fsf.org/copyleft/lgpl.txt>.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+ * License for more details.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "openswan.h"
+#include "ike_alg.h"
+#include "plutoalg.h"
+#include "oswlog.h"
+
+const char *progname;
+
+void exit_tool(int x)
+{
+  exit(x);
+}
+
+struct artab {
+    int   trans_type;
+    char *ascii;		/* string to process */
+    int   proto;
+} atodatatab[] = {
+    { IKEv2_TRANS_TYPE_ENCR,  "aes", IKEv2_ENCR_AES_CBC },
+    { 0,		NULL, FALSE, },
+};
+
+static void regress(void)
+{
+    int status = 0;
+    struct artab *r;
+    char buf[100];
+    size_t n;
+
+    for (r = atodatatab; r->ascii != NULL; r++) {
+        enum_names *lookup = NULL;
+        unsigned int item;
+
+        if(r->trans_type < ikev2_transid_val_descs_size) {
+           lookup = ikev2_transid_val_descs[r->trans_type];
+        }
+
+        if(!lookup) {
+            fprintf(stderr, "invalid trans_type: %u\n", r->trans_type);
+            continue;
+        }
+
+        item = enum_search_nocase(lookup, r->ascii, strlen(r->ascii));
+        passert(item == r->proto);
+    }
+
+    exit(status);
+}
+
+/*
+  - main - enumerate all the names of each algorithm, and use enum_search to look for some we care about.
+ */
+int
+main(int argc, char *argv[])
+{
+	char buf[1024];
+	char buf2[1024];
+	size_t n;
+	size_t i;
+	char *p = buf;
+	char *p2 = buf2;
+	char *pgm = argv[0];
+        int   result;
+	const char *oops;
+	struct alg_info_ike *alg_info_ike;
+        const  char *err;
+
+        progname = argv[0];
+        set_debugging(DBG_ALL);
+
+        regress();
+
+	exit(0);
+}
+
+
+
+/*
+ * Local Variables:
+ * c-basic-offset:4
+ * c-style: pluto
+ * End:
+ */
