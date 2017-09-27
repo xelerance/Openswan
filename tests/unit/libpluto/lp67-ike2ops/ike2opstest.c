7f19fd4e632fc6242ddc457a0308f86552b878af
Michael Richardson <mcr@credil.org>
2017-09-27 18:21:09 -0400

    wo#6269 . test case for for creating an IKEv2 sadb from an algo_info

--- tests/unit/libpluto/lp67-ike2ops/ike2opstest.c
+++ tests/unit/libpluto/lp67-ike2ops/ike2opstest.c
@@ -6,6 +6,7 @@
 
 #include "constants.h"
 #include "oswalloc.h"
+#include "oswlog.h"
 #include "pluto/defs.h"
 #include "pluto/db2_ops.h"
 #include "pluto/state.h"
@@ -23,6 +24,7 @@ int main(int argc, char *argv[])
     int i;
     err_t e = NULL;
     struct db2_context *dc;
+    struct db_sa *sadb;
     struct alg_info *ai;
     const char *ikepolicy;
 
@@ -32,50 +34,52 @@ int main(int argc, char *argv[])
     tool_init_log();
 
     ikepolicy="aes128-sha1-prfsha1-modp2048";
-    printf("for input ike=%s", ikepolicy);
+    DBG_log("for input ike=%s", ikepolicy);
     ai = (struct alg_info *)alg_info_ike_create_from_str(ikepolicy, &e);
 
     if(e) {
-        printf("failed to parse %s: %s\n", ikepolicy, e);
-        exit(10);
-    }
-    passert(ai != NULL);
-
-    dc = alginfo2db2(ai);
-
-    db2_print(dc);
-    db2_free(dc);
-
-    ikepolicy="aes128-sha1-sha1-modp2048";
-    printf("for input ike=%s", ikepolicy);
-    ai = (struct alg_info *)alg_info_ike_create_from_str(ikepolicy, &e);
-
-    if(e) {
-        printf("failed to parse %s: %s\n", ikepolicy, e);
-        exit(10);
-    }
-    passert(ai != NULL);
-
-    dc = alginfo2db2(ai);
-
-    db2_print(dc);
-    db2_free(dc);
-
-    /* now do the defaults */
-    printf("IKEv2 defaults\n");
-    ai = alg_info_ike_defaults();
-
-    if(e) {
-        printf("failed to parse %s: %s\n", ikepolicy, e);
+        DBG_log("failed to parse %s: %s\n", ikepolicy, e);
         exit(10);
     }
     passert(ai != NULL);
 
     sadb = alginfo2db2(ai);
+    alg_info_free(ai);
 
     sa_v2_print(sadb);
-    db2_free(dc);
+    free_sa(sadb);
 
+    ikepolicy="aes128-sha1-sha1-modp2048";
+    DBG_log("for input ike=%s", ikepolicy);
+    ai = (struct alg_info *)alg_info_ike_create_from_str(ikepolicy, &e);
+
+    if(e) {
+        DBG_log("failed to parse %s: %s\n", ikepolicy, e);
+        exit(10);
+    }
+    passert(ai != NULL);
+
+    sadb = alginfo2db2(ai);
+    alg_info_free(ai);
+
+    sa_v2_print(sadb);
+    free_sa(sadb);
+
+    /* now do the defaults */
+    DBG_log("IKEv2 defaults\n");
+    ai = alg_info_ike_defaults();
+
+    if(e) {
+        DBG_log("failed to parse %s: %s\n", ikepolicy, e);
+        exit(10);
+    }
+    passert(ai != NULL);
+
+    sadb = alginfo2db2(ai);
+    alg_info_free(ai);
+
+    sa_v2_print(sadb);
+    free_sa(sadb);
 
     report_leaks();
     tool_close_log();
