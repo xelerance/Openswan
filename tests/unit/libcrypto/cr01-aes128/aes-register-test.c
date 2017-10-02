d406aa44e214339881c9db550d3d38003972813c
Michael Richardson <mcr@credil.org>
2017-10-02 14:51:03 -0400

    wo#6269 . first test case of ike_alg plugable xfrm module

---
+++ tests/unit/libcrypto/cr01-aes128/aes-register-test.c
@@ -0,0 +1,82 @@
+#define LEAK_DETECTIVE
+#define AGGRESSIVE 1
+#define XAUTH 1
+#define PRINT_SA_DEBUG 1
+#define DEBUG 1
+#include <stdlib.h>
+
+#include "constants.h"
+#include "hexdump.c"
+#include "oswalloc.h"
+#include "oswlog.h"
+#include "ike_alg.h"
+
+const char *progname;
+
+void exit_tool(int stat)
+{
+    exit(stat);
+}
+
+extern int ike_alg_aes_init(void);
+
+int main(int argc, char *argv[])
+{
+    int i;
+    err_t e = NULL;
+    const struct ike_encr_desc *aes;
+    char  inbuf[256];
+    char  outbuf[256];
+    char  keybuf[16] = "thisthisthisthis";
+    char  ivbuf[16];
+
+    progname = argv[0];
+    leak_detective=1;
+    tool_init_log();
+
+    /* register it! */
+    ike_alg_aes_init();
+
+    passert(ike_alg_enc_present(IKEv2_ENCR_AES_CBC, 128));
+    passert(ike_alg_enc_present(IKEv2_ENCR_AES_CBC, 256));
+
+    aes = ike_alg_get_encrypter(IKEv2_ENCR_AES_CBC);
+
+    /* initialize the sample */
+    for(i=0; i<sizeof(inbuf); i++) {
+        inbuf[i] = i&0xff;
+    }
+    memcpy(outbuf, inbuf, sizeof(outbuf));
+
+    printf("plaintext input:\n");
+    hexdump(outbuf, 0, sizeof(outbuf));
+
+    /* now encrypt! */
+    aes->do_crypt(outbuf, sizeof(outbuf),
+                  keybuf, sizeof(keybuf),
+                  ivbuf, TRUE);
+
+    printf("ciphertext output:\n");
+    hexdump(outbuf, 0, sizeof(outbuf));
+
+    /* now decrypt! */
+    aes->do_crypt(outbuf, sizeof(outbuf),
+                  keybuf, sizeof(keybuf),
+                  ivbuf, FALSE);
+
+
+    printf("plaintext output: %s\n",
+           memcmp(inbuf, outbuf, sizeof(outbuf))==0 ? "matches" : "failed");
+    hexdump(outbuf, 0, sizeof(outbuf));
+
+    report_leaks();
+    tool_close_log();
+    exit(0);
+}
+
+/*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * End:
+ */
