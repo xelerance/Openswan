40fc902e242aa8f84f660627dc64bd025ee965e3
Michael Richardson <mcr@credil.org>
2017-10-02 14:21:18 -0400

    wo#6269 . do not keep looping if there is a core dump in unit test

--- tests/unit/libpluto/run-unit-tests.sh
+++ tests/unit/libpluto/run-unit-tests.sh
@@ -2,8 +2,8 @@
 
 for f in $(make testlist)
 do
-    (cd $f; figlet -t $f
-     while ! make check;
+    (cd $f; figlet -t $f; rm -f core;
+     while ! make check && ! [ -f core ];
      do
          make update && git add -p .
      done
