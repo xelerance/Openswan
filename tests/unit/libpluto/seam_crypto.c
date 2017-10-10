3a9081907dd8f90c3cd380c78343f614f4826665
Michael Richardson <mcr@credil.org>
2017-10-10 11:42:11 -0400

    wo#6269 . removed all crypto libraries from lp67, as this is about policy

--- tests/unit/libpluto/seam_crypto.c
+++ tests/unit/libpluto/seam_crypto.c
@@ -1,5 +1,44 @@
+#include "pluto/crypto.h"
+
 void
 init_crypto(void)
 {
   /* nothing */
 }
+
+const struct oakley_group_desc unset_group = {0, NULL, NULL, 0};      /* magic signifier */
+
+const struct oakley_group_desc *
+lookup_group(u_int16_t group)
+{
+  return &unset_group;
+}
+
+
+bool ike_alg_enc_present(int ealg, unsigned int keysize) { return TRUE; }
+bool ike_alg_integ_present(int halg, unsigned int keysize)  { return TRUE; }
+bool ike_alg_prf_present(int halg)  { return TRUE; }
+bool ike_alg_group_present(int modpid) { return TRUE; }
+
+bool ike_alg_enc_ok(int ealg, unsigned key_len, struct alg_info_ike *alg_info_ike, const char **n, char *m, size_t len) { return TRUE; }
+bool ike_alg_ok_final(int ealg, unsigned key_len, int aalg, unsigned int group, struct alg_info_ike *alg_info_ike) { return TRUE; }
+
+
+int dntoa(char *dst, size_t dstlen, chunk_t dn) { return 0; }
+err_t atodn(char *src, chunk_t *dn) { return "FAIL"; }
+bool match_dn(chunk_t a, chunk_t b, int *wildcards) { return FALSE; }
+int dn_count_wildcards(chunk_t dn) { return 0; }
+bool same_dn(chunk_t a, chunk_t b) { return FALSE; }
+
+void sha256_hash_buffer(const unsigned char *ib, int ile, unsigned char *ob, int ole) {}
+void calculate_rsa_ckaid(osw_public_key *pub) {}
+rsa_privkey_t* load_rsa_private_key(const char* filename
+                                    , int verbose
+                                    , prompt_pass_t *pass) { return NULL; }
+
+
+
+
+
+
+
