6a3d3a0d4125c5eeda66d21fb1f0381c508bff01
Michael Richardson <mcr@credil.org>
2017-10-23 14:15:22 -0400

    wo#6269 . working ap03-espinfo test case for parsing of esp= lines

--- tests/unit/libalgoparse/ap03-espinfo/espinfo_test.c
+++ tests/unit/libalgoparse/ap03-espinfo/espinfo_test.c
@@ -50,7 +50,7 @@ main(int argc, char *argv[])
 	char *pgm = argv[0];
         int   result;
 	const char *oops;
-	struct alg_info_ike *alg_info_ike;
+	struct alg_info_esp *alg_info_esp;
         const  char *err;
 
         progname = argv[0];
@@ -68,10 +68,10 @@ main(int argc, char *argv[])
 		exit(1);
 	}
 
-        alg_info_ike = alg_info_ike_create_from_str(argv[1],
+        alg_info_esp = alg_info_esp_create_from_str(argv[1],
                                                     &err);
 
-        if(alg_info_ike == NULL) {
+        if(alg_info_esp == NULL) {
           fprintf(stderr
                   , "%s: unable to parse %s as algorithm identifier: %s\n"
                   , argv[0]
@@ -90,33 +90,33 @@ struct artab {
   char *decode;		/* expected result after output */
   bool  err;            /* if TRUE, then err shall be set */
 } atodatatab[] = {
-  { "3des-md5-modp1024",
+  { "3des-md5",
     IKEv2_ENCR_3DES, 0, IKEv2_AUTH_HMAC_MD5_96,
-    OAKLEY_GROUP_MODP1024,
-    "3des(3)-hmac_md5_96(1)-prfmd5(1)-MODP1024(2); flags=-strict", FALSE,},
-  { "3des_cbc-md5-modp1024",      /* test out the alias for encr algorithms */
+    0,
+    "3des(3)_000-hmac_md5_96(1)_000; flags=-strict", FALSE,},
+  { "3des_cbc-md5",          /* test out the alias for encr algorithms */
     IKEv2_ENCR_3DES, 0, IKEv2_AUTH_HMAC_MD5_96,
-    OAKLEY_GROUP_MODP1024,
-    "3des(3)-hmac_md5_96(1)-prfmd5(1)-MODP1024(2); flags=-strict", FALSE,},
+    0,
+    "3des(3)_000-hmac_md5_96(1)_000; flags=-strict", FALSE,},
   { "aes-md5-modp1024",
     IKEv2_ENCR_AES_CBC, 128, IKEv2_AUTH_HMAC_MD5_96,
     OAKLEY_GROUP_MODP1024,
-    "aes_cbc(12)_128-hmac_md5_96(1)-prfmd5(1)-MODP1024(2); flags=-strict",  FALSE,},
-  { "aes-sha1-modp1024",
+    "aes_cbc(12)_128-hmac_md5_96(1)_000-MODP1024(2); flags=-strict", FALSE,},
+  { "aes-sha1",
     IKEv2_ENCR_AES_CBC, 128, IKEv2_AUTH_HMAC_SHA1_96,
-    OAKLEY_GROUP_MODP1024,
-    "aes_cbc(12)_128-hmac_sha1_96(2)-prfsha1(2)-MODP1024(2); flags=-strict", FALSE,},
-  { "aes-sha1-modp1536",
+    0,
+    "aes_cbc(12)_128-hmac_sha1_96(2)_000; flags=-strict", FALSE,},
+  { "aes-sha1-modp2048",
     IKEv2_ENCR_AES_CBC, 128, IKEv2_AUTH_HMAC_SHA1_96,
-    OAKLEY_GROUP_MODP1536,
-    "aes_cbc(12)_128-hmac_sha1_96(2)-prfsha1(2)-MODP1536(5); flags=-strict", FALSE,},
+    OAKLEY_GROUP_MODP2048,
+    "aes_cbc(12)_128-hmac_sha1_96(2)_000-MODP2048(14); flags=-strict", FALSE},
 
   /* a modern definition from draft-ietf-ipsecme-rfc7321bis/ */
-  { "aes256-sha256-prfsha256-modp2048",
+  { "aes256-sha256",
     IKEv2_ENCR_AES_CBC, 256, IKEv2_AUTH_HMAC_SHA2_256_128,
-    OAKLEY_GROUP_MODP2048,
-    "aes_cbc(12)_256-hmac_sha2_256_128(12)-prfsha2_256(5)-MODP2048(14); flags=-strict", FALSE,},
-  { NULL, 0,0,0,0,0,		NULL, FALSE, },
+    0,
+    "aes_cbc(12)_256-hmac_sha2_256_128(12)_000; flags=-strict", FALSE,},
+  { NULL, 0, 0,0,0,		NULL, FALSE, },
 };
 
 static void			/* should not return at all, in fact */
@@ -127,7 +127,7 @@ char *pgm;
 	struct artab *r;
 	char buf[100];
 	size_t n;
-	struct alg_info_ike *alg_info_esp;
+	struct alg_info_esp *alg_info_esp;
         const  char *err;
 
 	for (r = atodatatab; r->ascii != NULL; r++) {
@@ -146,35 +146,27 @@ char *pgm;
             continue;
           }
 
-          if(alg_info_esp->esp[0].esp_ealg != r->encr_id) {
+          if(alg_info_esp->esp[0].transid != r->encr_id) {
               fprintf(stderr, "failed to decode: %s\n"
                       "  expected encr_id: %d\n"
                       "  got:              %d\n",
-                      r->ascii, r->encr_id, alg_info_esp->esp[0].esp_ealg);
+                      r->ascii, r->encr_id, alg_info_esp->esp[0].transid);
           }
-          if(alg_info_esp->esp[0].esp_eklen != r->eklen) {
+          if(alg_info_esp->esp[0].enckeylen != r->eklen) {
               fprintf(stderr, "failed to decode: %s\n"
                       "  expected enc_len: %d\n"
                       "  got:              %d\n",
-                      r->ascii, (int)r->eklen, (int)alg_info_esp->esp[0].esp_eklen);
+                      r->ascii, (int)r->eklen, (int)alg_info_esp->esp[0].enckeylen);
           }
 
-          if(alg_info_esp->esp[0].esp_halg != r->hash_id) {
+          if(alg_info_esp->esp[0].auth!= r->hash_id) {
               fprintf(stderr, "failed to decode: %s\n"
                       "  expected hash_id: %d\n"
                       "  got:              %d\n",
-                      r->ascii, r->hash_id, alg_info_esp->esp[0].esp_halg);
+                      r->ascii, r->hash_id, alg_info_esp->esp[0].auth);
           }
 
-          if(alg_info_esp->esp[0].esp_modp != r->modp_id) {
-              fprintf(stderr, "failed to decode: %s\n"
-                      "  expected encr_id: %d\n"
-                      "  got:              %d\n",
-                      r->ascii, r->modp_id, alg_info_esp->esp[0].esp_modp);
-          }
-
-
-          alg_info_snprint(buf, sizeof(buf), ESPTOINFO(alg_info_esp), TRUE);
+          alg_info_snprint(buf, sizeof(buf), ESPTOINFO(alg_info_esp));
 
           if(r->decode != NULL && strcmp(r->decode, buf) != 0) {
             fprintf(stderr, "failed to decode: %s\n"
