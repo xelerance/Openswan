90ab9f4df8f2d0e4603653b27934cc3ad9b60c73
Michael Richardson <mcr@credil.org>
2017-11-03 14:41:56 -0400

    wo#6269 . skeleton for test case where TS are wrong, and authenticated NO_PROPOSAL_CHOSEN should be returned

---
+++ tests/unit/libpluto/lp71-childTSwrong/nattR2.c
@@ -0,0 +1,31 @@
+#include "../lp12-parentR2/parentR2_head.c"
+#include "seam_host_jamesjohnson.c"
+#include "seam_x509.c"
+
+#define TESTNAME "h2hR2"
+
+static void init_local_interface(void)
+{
+    init_jamesjohnson_interface();
+}
+
+static void init_fake_secrets(void)
+{
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/jj.secrets"
+			       , NULL, NULL);
+}
+
+static void init_loaded(void)
+{   /* nothing */ }
+
+#include "../lp12-parentR2/parentR2_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
