03166edf3963ae72de0d95ebf18b1df65e12a66b
Michael Richardson <mcr@credil.org>
2017-11-03 15:32:42 -0400

    wo#6269 . created another goal test case for responder that does not chose a proposal

---
+++ tests/unit/libpluto/lp72-msg1nopropchosen/parentR2.c
@@ -0,0 +1,31 @@
+#include "parentR2_head.c"
+#include "seam_host_jamesjohnson.c"
+#include "seam_x509.c"
+
+#define TESTNAME "parentR2"
+
+static void init_local_interface(void)
+{
+    init_jamesjohnson_interface();
+}
+
+static void init_fake_secrets(void)
+{
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/jj.secrets"
+			       , NULL, NULL);
+}
+
+static void init_loaded(void)
+{   /* nothing */ }
+
+#include "parentR2_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
