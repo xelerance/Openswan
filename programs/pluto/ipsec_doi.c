84e0fd923cc41c9744c5cb7afd4ad44eb091e159
Michael Richardson <mcr@credil.org>
2017-11-08 16:23:51 -0500

    wo#6269 . fixed fmt of ipsec_sa to use correct ikev2 names

--- programs/pluto/ipsec_doi.c
+++ programs/pluto/ipsec_doi.c
@@ -821,14 +821,14 @@ void fmt_ipsec_sa_established(struct state *st, char *sadetails, int sad_len)
 	    natinfo="/NAT";
 	}
 	snprintf(b, sad_len-(b-sadetails)-1
-		 , "%sESP%s=>0x%08lx <0x%08lx xfrm=%s_%d-%s"
+		 , "%sESP%s=>0x%08lx <0x%08lx xfrm=%s[%d]-%s"
 		 , ini
 		 , natinfo
 		 , (unsigned long)ntohl(st->st_esp.attrs.spi)
 		 , (unsigned long)ntohl(st->st_esp.our_spi)
-		 , enum_show(&esp_transformid_names, st->st_esp.attrs.transattrs.encrypt)+strlen("ESP_")
+		 , enum_show(&trans_type_encr_names, st->st_esp.attrs.transattrs.encrypt)
 		 , st->st_esp.attrs.transattrs.enckeylen
-		 , enum_show(&auth_alg_names, st->st_esp.attrs.transattrs.integ_hash)+strlen("AUTH_ALGORITHM_"));
+		 , enum_show(&trans_type_integ_names, st->st_esp.attrs.transattrs.integ_hash));
 	ini = " ";
 	fin = "}";
     }
