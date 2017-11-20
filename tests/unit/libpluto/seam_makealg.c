48c121e8d3eb4b9d8d51c6ad86dce466291947d9
Michael Richardson <mcr@credil.org>
2017-11-19 22:48:14 -0500

    wo#6269 . fix seam for kernel_makealg

--- tests/unit/libpluto/seam_makealg.c
+++ tests/unit/libpluto/seam_makealg.c
@@ -1,5 +1,5 @@
 struct db_sa *
-kernel_alg_makedb(lset_t policy UNUSED, struct alg_info_esp *ei)
+kernel_alg_makedb(lset_t policy UNUSED, struct alg_info_esp *ei, enum phase1_role role)
 {
   return NULL;
 }
