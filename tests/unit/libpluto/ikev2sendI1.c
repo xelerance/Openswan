5412249237e12ad303f2bd0dd774055679a560ee
Michael Richardson <mcr@credil.org>
2017-11-27 16:19:52 -0500

    wo#6269 . sort out group14 and finish_dh_v2() seam for faked DH work

--- tests/unit/libpluto/ikev2sendI1.c
+++ tests/unit/libpluto/ikev2sendI1.c
@@ -1,32 +1,4 @@
-struct state *sendI1_short(struct connection *c1, int debugging)
-{
-	struct state *st;
-	struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;  /* r is a global in the seams */
-
-	c1->extra_debugging = DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE;
-	ipsecdoi_initiate(/* whack-sock=stdout */1
-                          , NULL, NULL
-			  , c1
-			  , c1->policy
-			  , 0
-			  , FALSE
-			  , pcim_demand_crypto, USER_SEC_CTX_NULL);
-
-	/* find st involved */
-	st = state_with_serialno(1);
-
-	cur_debugging = debugging;
-	c1->extra_debugging = debugging;
-
-	/* now fill in the KE values from a constant.. not calculated */
-	clonetowirechunk(&kn->thespace, kn->space, &kn->secret, tc3_secret,tc3_secret_len);
-	clonetowirechunk(&kn->thespace, kn->space, &kn->n,   tc3_ni, tc3_ni_len);
-	clonetowirechunk(&kn->thespace, kn->space, &kn->gi,  tc3_gi, tc3_gi_len);
-
-	run_continuation(crypto_req);
-
-	return st;
-}
+#include "seam_gr_sha1_group14.c"
 
 void sendI1b(struct connection *c1, int debugging, int calculate)
 {
--- tests/unit/libpluto/lp02-parentI1/parentI1_head.c
+++ tests/unit/libpluto/lp02-parentI1/parentI1_head.c
@@ -76,8 +76,6 @@
 
 u_int8_t reply_buffer[MAX_OUTPUT_UDP_SIZE];
 
-#include "seam_gi_sha1.c"
-#include "seam_gi_sha1_group14.c"
 #include "seam_finish.c"
 
 #include "ikev2sendI1.c"
