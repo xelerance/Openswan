7846b92042b48ae5cba630007678e2b60a145842
Michael Richardson <mcr@credil.org>
2017-12-04 14:27:20 -0500

    wo#6269 . clarified rsa signature creation and checking, lengths

--- programs/pluto/ikev2_rsa.c
+++ programs/pluto/ikev2_rsa.c
@@ -80,9 +80,12 @@ void ikev2_calculate_sighash(struct state *st
 	}
 
 	DBG(DBG_CRYPT
-	    , DBG_dump_chunk("inputs to hash1 (first packet)", firstpacket);
+	    , DBG_log("calculate sighash");
+              DBG_dump_chunk("inputs to hash1 (first packet)", firstpacket);
 	      DBG_dump_chunk(nonce_name, *nonce);
-	    DBG_dump("idhash", idhash, st->st_oakley.prf_hasher->hash_digest_len));
+              DBG_log("pidhash len: %u",
+                      (unsigned int)st->st_oakley.prf_hasher->hash_digest_len);
+  	      DBG_dump("idhash", idhash, st->st_oakley.prf_hasher->hash_digest_len));
 
 	SHA1Init(&ctx_sha1);
 	SHA1Update(&ctx_sha1
@@ -114,7 +117,7 @@ ikev2_verify_rsa_sha1(struct state *st
     ikev2_calculate_sighash(st, invertrole, idhash, st->st_firstpacket_him, calc_hash);
 
     DBG(DBG_CRYPT,
-        DBG_dump("v2rsa calculated octets", calc_hash, hash_len);
+        DBG_dump("v2rsa calculated octets (sans ASN.1)", calc_hash, hash_len);
         DBG_dump_pbs(sig_pbs);
         );
 
