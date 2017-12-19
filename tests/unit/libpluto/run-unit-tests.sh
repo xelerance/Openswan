7708f2f3f60c655f3a60cf3784f19fcbc20adc3b
Bart Trojanowski <bart@jukie.net>
2017-12-19 13:24:10 -0500

    run-unit-tests.sh - use toilet if available, and defer to figlet otherwise

--- tests/unit/libpluto/run-unit-tests.sh
+++ tests/unit/libpluto/run-unit-tests.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
+# set some funky toilet options
+toilet_options=
+[ -t 0 ] && toilet_options="--metal --width $(tput cols) --font future"
+
+header() {
+    # use tilet if possible
+    toilet $toilet_options $@ \
+    || figlet -t $@
+}
+
 for f in $(make testlist)
 do
-    (cd $f; figlet -t $f
+    (cd $f ; header $f
      while ! make check;
      do
          make update && git add -p .
