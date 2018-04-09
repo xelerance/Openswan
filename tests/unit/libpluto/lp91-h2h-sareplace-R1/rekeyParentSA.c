073d6e6404472bec638d7318c01bf33b32dce03e
Michael Richardson <mcr@sandelman.ca>
2018-04-09 15:55:28 -0400

    initial work on step 2 of #7011

---
+++ tests/unit/libpluto/lp91-h2h-sareplace-R1/rekeyParentSA.c
@@ -0,0 +1,62 @@
+#include "../lp13-parentI3/parentI3_head.c"
+
+#define TESTNAME "rekeyParentSA"
+
+/* this is replicated in the unit test cases since the patching up of the crypto values is case specific */
+void recv_pcap_packet(u_char *user
+		      , const struct pcap_pkthdr *h
+		      , const u_char *bytes)
+{
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    recv_pcap_packet_gen(user, h, bytes);
+
+    /* find st involved */
+    st = state_with_serialno(1);
+    if(st != NULL) {
+        passert(st != NULL);
+        st->st_connection->extra_debugging = DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE|DBG_CRYPT|DBG_PRIVATE;
+    }
+
+    run_continuation(crypto_req);
+}
+
+void recv_pcap_packet2(u_char *user
+                      , const struct pcap_pkthdr *h
+                      , const u_char *bytes)
+{
+    static int contnum = 0;
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    recv_pcap_packet_gen(user, h, bytes);
+
+    /* find st involved */
+    st = state_with_serialno(1);
+    st->st_connection->extra_debugging = DBG_PRIVATE|DBG_CRYPT|DBG_PARSING|DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE;
+
+    DBG_log("continuation %d", ++contnum);
+    run_continuation(crypto_req);
+
+}
+
+static void init_loaded(void)
+{   /* nothing */ }
+
+#define PCAP_INPUT_COUNT 2
+recv_pcap recv_inputs[PCAP_INPUT_COUNT]={
+    recv_pcap_packet,
+    recv_pcap_packet2,
+};
+
+
+#include "../lp13-parentI3/parentI3_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
