35f726ecae078b6caf4c98f4f32e95ae6d227f21
Bart Trojanowski <bart@jukie.net>
2018-05-24 13:30:39 -0400

    wo#7092 . define lp{71,72,73,74}* which use ike=3des-md5 / phase2alg=aes128-sha1

---
+++ tests/unit/libpluto/lp75-alg-h2hI3/h2hI3.c
@@ -0,0 +1,63 @@
+#include "../lp13-parentI3/parentI3_head.c"
+#include "seam_gi_md5.c"
+#include "seam_finish.c"
+#include "seam_ikev2_sendI1.c"
+
+#define TESTNAME "h2hI3"
+
+/* this is replicated in the unit test cases since the patching up of the crypto values is case specific */
+void recv_pcap_packet(u_char *user
+		      , const struct pcap_pkthdr *h
+		      , const u_char *bytes)
+{
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    enable_debugging();
+    enable_debugging_on_sa(1);
+
+    recv_pcap_packet_gen(user, h, bytes);
+
+    /* find st involved */
+    st = state_with_serialno(1);
+    if(st != NULL) {
+        passert(st != NULL);
+        st->st_connection->extra_debugging = DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE|DBG_CRYPT|DBG_PRIVATE;
+    }
+
+    run_continuation(crypto_req);
+}
+
+void recv_pcap_packet2(u_char *user
+                      , const struct pcap_pkthdr *h
+                      , const u_char *bytes)
+{
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    enable_debugging();
+    enable_debugging_on_sa(1);
+    enable_debugging_on_sa(2);
+
+    recv_pcap_packet_gen(user, h, bytes);
+
+    /* find st involved */
+    st = state_with_serialno(1);
+    st->st_connection->extra_debugging = DBG_PRIVATE|DBG_CRYPT|DBG_PARSING|DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE;
+
+    run_continuation(crypto_req);
+
+}
+
+static void init_loaded(void)
+{   /* nothing */ }
+
+#include "../lp13-parentI3/parentI3_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
