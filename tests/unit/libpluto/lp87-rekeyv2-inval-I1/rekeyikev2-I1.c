217d5b99bad5e27acd100f1714a3d98981e53631
Bart Trojanowski <bart@jukie.net>
2019-02-19 12:10:45 -0500

    wo#5579 - clone lp{46~48} to lp{87~89} to handle invalid nonce notification

---
+++ tests/unit/libpluto/lp87-rekeyv2-inval-I1/rekeyikev2-I1.c
@@ -0,0 +1,24 @@
+#define WANT_THIS_DBG DBG_EMITTING|DBG_CONTROL|DBG_CONTROLMORE|DBG_CRYPT|DBG_PRIVATE|DBG_LIFECYCLE|DBG_PARSING
+#include "../lp13-parentI3/parentI3_head.c"
+#include "seam_gi_sha1.c"
+#include "seam_gi_sha1_group14.c"
+#include "seam_finish.c"
+#include "seam_ikev2_sendI1.c"
+#include "seam_debug.c"
+
+static void init_loaded(void)
+{   /* nothing */ }
+
+#define TESTNAME "rekeyikev2"
+#define AFTER_CONN rekeyit
+
+#include "../lp87-rekeyv2-inval-I1/rekeyit.c"
+#include "../lp13-parentI3/parentI3_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
---
+++ tests/unit/libpluto/lp88-rekeyv2-inval-R1/rekeyit.c
@@ -0,0 +1,41 @@
+void recv_pcap_packetC1(u_char *user
+                      , const struct pcap_pkthdr *h
+                      , const u_char *bytes)
+{
+    struct state *st;
+    struct pcr_kenonce *kn = &crypto_req->pcr_d.kn;
+
+    recv_pcap_packet_gen(user, h, bytes);
+
+    enable_debugging();
+    enable_debugging_on_sa(3);
+
+    /* find st involved */
+    st = state_with_serialno(3);
+    if (st) st->st_connection->extra_debugging = WANT_THIS_DBG;
+
+    /* now fill in the KE values from a constant.. not calculated */
+    clonetowirechunk(&kn->thespace, kn->space, &kn->n,   SS(nr.ptr), SS(nr.len));
+    clonetowirechunk(&kn->thespace, kn->space, &kn->gi,  SS(gr.ptr), SS(gr.len));
+
+    run_one_continuation(crypto_req);
+
+    /* now do the second calculation */
+    clonetowirechunk(&kn->thespace, kn->space, &kn->secret, SS(secret.ptr),SS(secret.len));
+    run_one_continuation(crypto_req);
+}
+
+#define PCAP_INPUT_COUNT 3
+recv_pcap recv_inputs[PCAP_INPUT_COUNT]={
+    recv_pcap_packet,
+    recv_pcap_packet2,
+    recv_pcap_packetC1
+};
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
