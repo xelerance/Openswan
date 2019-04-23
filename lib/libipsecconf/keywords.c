76ca62ac98b153564b084941cf182474a8093d60
Bart Trojanowski <bart@jukie.net>
2019-04-23 12:26:10 -0400

    wo#8451 - dpddelay and dpdtimeout arguments accept time

--- lib/libipsecconf/keywords.c
+++ lib/libipsecconf/keywords.c
@@ -444,9 +444,9 @@ struct keyword_def ipsec_conf_keywords_v2[]={
     {"metric",         kv_conn|kv_auto, kt_number, KBF_METRIC, NOT_ENUM},
 
     /* DPD */
-    {"dpddelay",       kv_conn|kv_auto,kt_number, KBF_DPDDELAY, NOT_ENUM},
-    {"dpdtimeout",     kv_conn|kv_auto,kt_number,KBF_DPDTIMEOUT , NOT_ENUM},
-    {"dpdaction",      kv_conn|kv_auto,kt_enum, KBF_DPDACTION , &kw_dpdaction_list},
+    {"dpddelay",       kv_conn|kv_auto, kt_time, KBF_DPDDELAY,   NOT_ENUM},
+    {"dpdtimeout",     kv_conn|kv_auto, kt_time, KBF_DPDTIMEOUT, NOT_ENUM},
+    {"dpdaction",      kv_conn|kv_auto, kt_enum, KBF_DPDACTION,  &kw_dpdaction_list},
 
     {"mtu",            kv_conn|kv_auto,kt_number, KBF_CONNMTU, NOT_ENUM},
 
