5d36d29e0411337678c5ed8bd7acc8bd3921cb02
Michael Richardson <mcr@sandelman.ca>
2019-07-12 16:43:58 -0400

    bring in t7257 test cases from libpluto

---
+++ tests/unit/libpluto/lp102-v1rwnatM1/v1rwnatM1.c
@@ -0,0 +1,51 @@
+#define OMIT_MAIN_MODE 1
+#define FIREWALL_OUTSIDE "192.168.0.1"
+#define NAPT_ENABLED 1
+#include "../lp02-parentI1/parentI1_head.c"
+#include "seam_host_alice.c"
+#include "seam_demux.c"
+#include "seam_x509.c"
+#include "seam_pending.c"
+#include "seam_whack.c"
+#include "seam_initiate.c"
+#include "seam_dnskey.c"
+#include "seam_gi_sha1.c"
+#include "seam_ikev2_sendI1.c"
+#include "seam_finish.c"
+#include "seam_ikev1_phase2.c"
+#include "seam_ikev1_crypto.c"
+#include "seam_rsa_check.c"
+
+
+#define TESTNAME "v1rwnatM1"
+
+static void init_local_interface(void)
+{
+    init_alice_interface(TRUE);
+}
+
+static void init_fake_secrets(void)
+{
+    prompt_pass_t pass;
+    memset(&pass, 0, sizeof(pass));
+
+    osw_init_ipsecdir(SAMPLEDIR "alice");
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , SAMPLEDIR "alice.secrets"
+			       , &pass, NULL);
+
+}
+
+bool no_cr_send = FALSE;
+
+#include "../lp02-parentI1/parentI1_main.c"
+
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
