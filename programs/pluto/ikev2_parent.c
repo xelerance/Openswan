32ff5698fe5c4fde181438475c719e69f271f954
Michael Richardson <mcr@sandelman.ca>
2019-07-12 11:24:23 -0400

    change #include to reflect spdb.h -> pluto/spdb.h, so that unit tests compile

--- programs/pluto/ikev2_parent.c
+++ programs/pluto/ikev2_parent.c
@@ -49,7 +49,7 @@
 #include "demux.h"
 #include "ikev2.h"
 #include "log.h"
-#include "spdb.h"          /* for out_sa */
+#include "pluto/spdb.h"          /* for out_sa */
 #include "ipsec_doi.h"
 #include "vendor.h"
 #include "timer.h"
--- programs/pluto/spdb_v2_struct.c
+++ programs/pluto/spdb_v2_struct.c
@@ -43,7 +43,7 @@
 #include "secrets.h"
 #include "kernel.h"	/* needs connections.h */
 #include "log.h"
-#include "spdb.h"
+#include "pluto/spdb.h"
 #include "whack.h"	/* for RC_LOG_SERIOUS */
 #include "plutoalg.h"
 
