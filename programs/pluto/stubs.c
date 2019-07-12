14f23a8721611cfb41a054761212548762ae8f8a
Michael Richardson <mcr@sandelman.ca>
2019-07-12 11:24:23 -0400

    change #include to reflect spdb.h -> pluto/spdb.h, so that unit tests compile

--- programs/pluto/stubs.c
+++ programs/pluto/stubs.c
@@ -58,7 +58,7 @@
 #include "dnskey.h"	/* needs keys.h and adns.h */
 #include "whack.h"
 #include "alg_info.h"
-#include "spdb.h"
+#include "pluto/spdb.h"
 #include "ike_alg.h"
 #include "kernel_alg.h"
 #include "plutoalg.h"
