bad86415635c5b5ee21da4214860de64d5d7af8a
Michael Richardson <mcr@sandelman.ca>
2019-09-24 21:54:16 -0400

    normalize base test cases: link connections.c, use lp13 makefile object list consistently

--- tests/unit/libpluto/lp12-parentR2/parentR2_head.c
+++ tests/unit/libpluto/lp12-parentR2/parentR2_head.c
@@ -6,16 +6,7 @@
 #define PRINT_SA_DEBUG 1
 #define USE_KEYRR 1
 
-#include <pcap.h>
-
-#include "constants.h"
-#include "oswalloc.h"
-#include "oswcrypto.h"
-#include "whack.h"
-#include "oswconf.h"
-#include "../../programs/pluto/rcv_whack.h"
-
-#include "../../programs/pluto/connections.c"
+#include "unit_test_includes.h"
 
 #include "whackmsgtestlib.c"
 #include "seam_debug.c"
