8b1be3b16ec8a4876c4428bcee6d9a5c2aa511c0
Michael Richardson <mcr@sandelman.ca>
2019-09-25 17:04:42 -0400

    import lp72-msg1nopropchosen test case from rebase branch

---
+++ tests/unit/libpluto/lp72-msg1nopropchosen/parentR2_head.c
@@ -0,0 +1,45 @@
+#define LEAK_DETECTIVE
+#define AGGRESSIVE 1
+#define XAUTH
+#define MODECFG
+#define DEBUG 1
+#define PRINT_SA_DEBUG 1
+#define USE_KEYRR 1
+
+#include <pcap.h>
+
+#include "constants.h"
+#include "oswalloc.h"
+#include "oswcrypto.h"
+#include "whack.h"
+#include "oswconf.h"
+#include "../../programs/pluto/rcv_whack.h"
+
+#include "../../programs/pluto/connections.c"
+
+#include "whackmsgtestlib.c"
+#include "seam_timer.c"
+#include "seam_fakevendor.c"
+#include "seam_pending.c"
+#include "seam_ikev1.c"
+#include "seam_ikev1_aggr.c"
+#include "seam_crypt.c"
+#include "seam_rsasig.c"
+#include "seam_kernel.c"
+#include "seam_rnd.c"
+#include "seam_log.c"
+#include "seam_xauth.c"
+#include "seam_terminate.c"
+#include "seam_spdbstruct.c"
+#include "seam_demux.c"
+#include "seam_commhandle.c"
+#include "seam_whack.c"
+#include "seam_initiate.c"
+#include "seam_exitlog.c"
+#include "seam_natt.c"
+#include "seam_ikealg.c"
+#include "seam_dnskey.c"
+
+
+void recv_pcap_packet(u_char *user		      , const struct pcap_pkthdr *h		      , const u_char *bytes);
+void recv_pcap_packet2(u_char *user                      , const struct pcap_pkthdr *h                      , const u_char *bytes);
