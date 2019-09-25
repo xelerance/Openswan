a11e921f190df0c395608c39f7f81c899ad193be
Michael Richardson <mcr@sandelman.ca>
2019-09-25 18:05:52 -0400

    import lp81-ikev1derived test case from rebase branch

---
+++ tests/unit/libpluto/lp81-ikev1derived/ikev1derived.c
@@ -0,0 +1,48 @@
+#define OMIT_MAIN_MODE 1
+#define NAPT_ENABLED 1
+
+#include "../lp02-parentI1/parentI1_head.c"
+#include "seam_demux.c"
+#include "seam_x509.c"
+#include "seam_pending.c"
+#include "seam_whack.c"
+#include "seam_initiate.c"
+#include "seam_dnskey.c"
+#include "seam_rsasig.c"
+#include "seam_keys.c"
+#include "seam_gi_sha256_group14.c"
+#include "seam_ikev2_sendI1.c"
+#include "seam_finish.c"
+#include "seam_ikev1_phase2.c"
+#include "seam_ikev1_crypto.c"
+#include "seam_rsa_check.c"
+
+#include "seam_host_parker.c"
+
+#define TESTNAME "ikev1derived"
+
+static void init_local_interface(void)
+{
+    init_parker_interface(TRUE);
+}
+
+static void init_fake_secrets(void)
+{
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/parker.secrets"
+			       , NULL, NULL);
+}
+
+bool no_cr_send = FALSE;
+
+#include "../lp02-parentI1/parentI1_main.c"
+
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
