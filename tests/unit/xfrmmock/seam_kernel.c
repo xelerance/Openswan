5d0bbbd123a6382b12f526c787ba7dce69153e70
Michael Richardson <mcr@sandelman.ca>
2019-10-01 17:49:56 -0400

    xformmock unit tests now compile correctly

--- tests/unit/xfrmmock/seam_kernel.c
+++ tests/unit/xfrmmock/seam_kernel.c
@@ -1,3 +1,6 @@
+#ifndef __seam_kernel_c__
+#define __seam_kernel_c__
+#ifndef NO_SEAM_KERNEL
 #include "kernel.h"
 
 bool can_do_IPcomp = TRUE;  /* can system actually perform IPCOMP? */
@@ -45,49 +48,6 @@ bool get_sa_info(struct state *st, bool inbound, time_t *ago)
 	return FALSE;
 }
 
-bool
-unit_do_command(struct connection *c, const struct spd_route *sr
-                  , const char *verb, const char *verb_suffix
-                  , struct state *st)
-{
-  DBG_log("executing %s", verb);
-  return TRUE;
-}
-
-
-/* empty structure */
-struct kernel_ops unit_kernel_ops = {
-    kern_name: "netkeyunit",
-    type: USE_NETKEY,
-    inbound_eroute:  TRUE,
-    policy_lifetime: TRUE,
-    async_fdp: -1,
-    replay_window: 32,
-
-    init: init_netlink,
-    pfkey_register: NULL,
-    pfkey_register_response: NULL,
-    process_msg: netlink_process_msg,
-    raw_eroute: netlink_raw_eroute,
-    add_sa: netlink_add_sa,
-    del_sa: netlink_del_sa,
-    get_sa: netlink_get_sa,
-    process_queue: NULL,
-    grp_sa: NULL,
-    get_spi: netlink_get_spi,
-    exceptsocket: NULL,
-    docommand: unit_do_command,
-    process_ifaces: netlink_process_raw_ifaces,
-    shunt_eroute: netlink_shunt_eroute,
-    sag_eroute: netlink_sag_eroute,
-    eroute_idle: netlink_eroute_idle,
-    set_debug: NULL,
-    remove_orphaned_holds: NULL,
-    overlap_supported: FALSE,
-    sha2_truncbug_support: FALSE,
-};
-const struct kernel_ops *kernel_ops = &unit_kernel_ops;
-
 bool route_and_eroute(struct connection *c USED_BY_KLIPS
                       , const struct spd_route *sr USED_BY_KLIPS
                       , struct spd_route *orig_sr USED_BY_KLIPS
@@ -109,3 +69,5 @@ bool assign_hold(struct connection *c USED_BY_DEBUG
 
 bool has_bare_hold(const ip_address *src, const ip_address *dst, int transport_proto) { return FALSE;}
 
+#endif /* NO_SEAM_KERNEL */
+#endif
