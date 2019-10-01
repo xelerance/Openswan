a5822590a3fc4e610ea091137a7ea2a15e8aba4d
Michael Richardson <mcr@sandelman.ca>
2019-10-01 16:10:49 -0400

    move unroute and ike ports to seam_kernel

--- tests/unit/libpluto/seam_kernel.c
+++ tests/unit/libpluto/seam_kernel.c
@@ -1,6 +1,12 @@
 #ifndef __seam_kernel_c__
 #define __seam_kernel_c__
 #include "kernel.h"
+bool can_do_IPcomp = TRUE;  /* can system actually perform IPCOMP? */
+
+u_int16_t pluto_port500  = IKE_UDP_PORT;	/* Pluto's port */
+u_int16_t pluto_port4500 = NAT_IKE_UDP_PORT;	/* Pluto's port NAT */
+
+void unroute_connection(struct connection *c) {}
 void delete_ipsec_sa(struct state *st USED_BY_KLIPS, bool inbound_only USED_BY_KLIPS) {}
 
 bool install_inbound_ipsec_sa(struct state *parent_st, struct state *st) { return TRUE; }
@@ -69,5 +75,6 @@ bool assign_hold(struct connection *c USED_BY_DEBUG
                  , const ip_address *src, const ip_address *dst) { return TRUE; }
 
 bool has_bare_hold(const ip_address *src, const ip_address *dst, int transport_proto) { return FALSE;}
+bool trap_connection(struct connection *c) { return TRUE; }
 
 #endif
