09fed249e338aaa723b5fb25ddc92e6322a45b88
Michael Richardson <mcr@sandelman.ca>
2019-12-09 11:58:56 -0500

    updated parentI2 with correct R1 pcap file, and seam_keys.c change now works

--- tests/unit/libpluto/lp10-parentI2/parentI2.c
+++ tests/unit/libpluto/lp10-parentI2/parentI2.c
@@ -4,7 +4,6 @@
 #include "seam_finish.c"
 #include "seam_ikev2_sendI1.c"
 #include "seam_natt.c"
-#include "seam_keys.c"
 #include "seam_rsasig.c"
 #include "seam_x509.c"
 #include "seam_host_parker.c"
