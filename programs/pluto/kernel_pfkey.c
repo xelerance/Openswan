bb843fe4b6efa4c6a5b6d71d358dcd148636a183
anatoli <anatoli@ipsec-buster-amd64.dmz.dp.tkb>
2019-12-17 09:35:19 +0200

    Minus one more warn:     programs/pluto/kernel_pfkey.c: In function ‘finish_pfkey_msg’:     programs/pluto/kernel_pfkey.c:682:14: warning: this statement may fall through [-Wimplicit-fallthrough=]            success=FALSE;     programs/pluto/kernel_pfkey.c:685:5: note: here          default:          ^~~~~~~     Without any changes in logic

--- programs/pluto/kernel_pfkey.c
+++ programs/pluto/kernel_pfkey.c
@@ -680,7 +680,7 @@ finish_pfkey_msg(struct sadb_ext *extensions[K_SADB_EXT_MAX + 1]
 		  case ENOENT:
 		    loglog(RC_LOG_SERIOUS, "requested algorithm is not available in the kernel");
 		    success=FALSE;
-		    /* fall through to get error message */
+		    goto logerr;
 
 		  default:
 		  logerr:
