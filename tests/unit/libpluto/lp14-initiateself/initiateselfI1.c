2302806e35a3629258cfcb6045c5d3ad8264c8c1
Michael Richardson <mcr@sandelman.ca>
2020-01-04 18:23:14 -0500

    move to consistently use ${UNITTEST1ARGS}

--- tests/unit/libpluto/lp14-initiateself/initiateselfI1.c
+++ tests/unit/libpluto/lp14-initiateself/initiateselfI1.c
@@ -1,7 +1,7 @@
 /* repeats existing test case */
 #include "../lp02-parentI1/parentI1_head.c"
 #include "seam_gi_sha1.c"
-#include "seam_gi_sha1_group14.c"
+#include "seam_gi_sha256_group14.c"
 #include "seam_finish.c"
 #include "seam_ikev2_sendI1.c"
 #include "seam_demux.c"
