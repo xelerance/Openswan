b5e08ef572b4c62e761672bc5283d32ffa5e84a9
Michael Richardson <mcr@sandelman.ca>
2020-01-10 14:55:23 -0500

    updated ikev1 basic sequence

--- tests/unit/libpluto/lp75-parentN2/parentN2.c
+++ tests/unit/libpluto/lp75-parentN2/parentN2.c
@@ -11,6 +11,7 @@
 #include "seam_ikev1_phase2.c"
 #include "seam_ikev1_crypto.c"
 #include "seam_gi_sha1.c"
+#include "seam_gi_sha256_group14.c"
 #include "seam_finish.c"
 #include "seam_unpend.c"
 #include "seam_command.c"
