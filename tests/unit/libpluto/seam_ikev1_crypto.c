f890fa659bcc866b20fa42d0606460d82d9d6860
Michael Richardson <mcr@sandelman.ca>
2020-01-12 18:17:48 -0500

    wo#7257 . lp73 and lp15 have sanity.sed added to filters

--- tests/unit/libpluto/seam_ikev1_crypto.c
+++ tests/unit/libpluto/seam_ikev1_crypto.c
@@ -6,7 +6,13 @@ stf_status start_dh_secret(struct pluto_crypto_req_cont *cn
 			   , enum phase1_role init
 			   , u_int16_t oakley_group2)
 {
-  /* it will get get done */
+    struct pluto_crypto_req r;
+    struct pcr_skeyid_q *dhq;
+    err_t e;
+    bool toomuch = FALSE;
+
+    continuation = cn;
+    pcr_init(&r, pcr_compute_dh, importance);
   return STF_SUSPEND;
 }
 
@@ -16,7 +22,11 @@ stf_status start_dh_secretiv(struct pluto_crypto_req_cont *cn
 			     , enum phase1_role init       /* TRUE=g_init,FALSE=g_r */
 			     , u_int16_t oakley_group2)
 {
+    struct pluto_crypto_req r;
+
+    continuation = cn;
+    pcr_init(&r, pcr_compute_dh, importance);
   return STF_SUSPEND;
 }
 
-#endif
+#endif /* __seam_ikev1_crypto_c__ */
