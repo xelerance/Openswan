ee1fdc6f19d8e5874ec2528a8fca71aa988d0955
Michael Richardson <mcr@sandelman.ca>
2020-01-14 02:59:38 -0500

    fixup! wo#7257 . updated policy and input files for responder lp106

--- tests/unit/libpluto/lp106-v1rwnatM3/v1rwnatM3.c
+++ tests/unit/libpluto/lp106-v1rwnatM3/v1rwnatM3.c
@@ -7,7 +7,7 @@
 #include "../lp10-parentI2/parentI2_head.c"
 #include "seam_host_alice.c"
 #include "seam_kernel.c"
-#include "seam_gi_sha1.c"
+#include "seam_gi_md5.c"
 #include "seam_pending.c"
 #include "nat_traversal.h"
 #include "seam_rsasig.c"
