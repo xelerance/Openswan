59f8f127d1ce0d208e3c0f2b8aca37802d885aa0
Michael Richardson <mcr@sandelman.ca>
2020-01-20 01:10:45 -0500

    wo#7257 . update policy to sha256

--- tests/unit/libpluto/lp75-parentN2/parentN2.c
+++ tests/unit/libpluto/lp75-parentN2/parentN2.c
@@ -10,7 +10,6 @@
 #include "seam_dpd.c"
 #include "seam_ikev1_phase2.c"
 #include "seam_ikev1_crypto.c"
-#include "seam_gi_sha1.c"
 #include "seam_gi_sha256_group14.c"
 #include "seam_finish.c"
 #include "seam_unpend.c"
