5897942688c6c00d4ff01725c1cb96344b03a0d9
Michael Richardson <mcr@sandelman.ca>
2020-01-26 17:34:56 -0500

    copy of lp15, with the hope of turning it into the invalid input test case, not yet enabled

---
+++ tests/unit/libpluto/lp117-childanypolicy/respondselfR2.c
@@ -0,0 +1,35 @@
+#include "../lp12-parentR2/parentR2_head.c"
+#include "seam_natt.c"
+#include "seam_host_jamesjohnson.c"
+#include "seam_x509.c"
+#include "seam_gr_sha1_group14.c"
+#include "seam_finish.c"
+#include "seam_kernel.c"
+
+#define TESTNAME "respondselfR2"
+
+static void init_local_interface(void)
+{
+    init_jamesjohnson_interface();
+}
+
+static void init_fake_secrets(void)
+{
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/jj.secrets"
+			       , NULL, NULL);
+}
+
+static void init_loaded(void)
+{   /* nothing */ }
+
+#include "../lp12-parentR2/parentR2_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
