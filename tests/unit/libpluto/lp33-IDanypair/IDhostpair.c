c05e304ab3722c426d15c1a6c5ed648ed1bc367b
Michael Richardson <mcr@sandelman.ca>
2020-01-26 14:28:09 -0500

    wo#7257 . move init_fake_vendor into vendor.c, to permit vendorid_built to be set

--- tests/unit/libpluto/lp33-IDanypair/IDhostpair.c
+++ tests/unit/libpluto/lp33-IDanypair/IDhostpair.c
@@ -18,7 +18,6 @@
 #include "whackmsgtestlib.c"
 #include "seam_timer.c"
 #include "seam_vendor.c"
-#include "seam_fakevendor.c"
 #include "seam_pending.c"
 #include "seam_initiate.c"
 #include "seam_ikev1.c"
