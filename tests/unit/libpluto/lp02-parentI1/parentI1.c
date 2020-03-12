7dd89fc165cb79984b85a27ebb76faee05a2dd24
Michael Richardson <mcr@sandelman.ca>
2020-03-12 13:41:24 -0400

    use proper ke/nonce seam in I1/R1, to get repeatable nonce

--- tests/unit/libpluto/lp02-parentI1/parentI1.c
+++ tests/unit/libpluto/lp02-parentI1/parentI1.c
@@ -1,6 +1,5 @@
 #include "parentI1_head.c"
-#include "seam_gi_sha1.c"
-#include "seam_gi_sha1_group14.c"
+#include "seam_gi_sha256_group14.c"
 #include "seam_finish.c"
 #include "seam_ikev2_sendI1.c"
 #include "seam_demux.c"
