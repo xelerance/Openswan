213361496d3ab34f21af5cf2548ed1812c0d90ff
Michael Richardson <mcr@sandelman.ca>
2020-03-14 17:03:17 -0400

    updates seams and pcap files

--- tests/unit/libpluto/seam_host_moon.c
+++ tests/unit/libpluto/seam_host_moon.c
@@ -17,11 +17,27 @@ struct iface_port moon_if1 = {
 	.change    = IFN_KEEP
 };
 
+struct iface_port moon_if2 = {
+	.ip_dev = &moon_ifd1,
+	.port   = 4500,
+        .socktypename = "AF_INET",
+	.ip_addr.u.v4.sin_family = AF_INET,
+	.fd     = -1,
+	.next   = NULL,
+	.ike_float = 1,
+	.change    = IFN_KEEP
+};
+
 void init_moon_interface(bool doipv6 UNUSED)
 {
   init_iface_port(&moon_if1);
   moon_if1.next = interfaces;
   inet_pton(AF_INET, "192.168.0.1", &moon_if1.ip_addr.u.v4.sin_addr);
   interfaces = &moon_if1;
+
+  init_iface_port(&moon_if2);
+  moon_if2.next = interfaces;
+  inet_pton(AF_INET, "192.168.0.1", &moon_if2.ip_addr.u.v4.sin_addr);
+  interfaces = &moon_if2;
 }
 #endif
