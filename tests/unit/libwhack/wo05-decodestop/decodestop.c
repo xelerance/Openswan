21bca0e1715a76b16810c69f6092031655bfd572
Michael Richardson <mcr@sandelman.ca>
2021-02-03 15:43:27 -0500

    QCBOR based encoder and decoder, probably almost feature complete except for RSA key values

---
+++ tests/unit/libwhack/wo05-decodestop/decodestop.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include "constants.h"
+#include "oswlog.h"
+#include "oswalloc.h"
+#include "whack.h"
+
+#include "seam_exitlog.c"
+
+const char *progname=NULL;
+int verbose=0;
+int warningsarefatal = 0;
+
+int main(int argc, char *argv[])
+{
+    err_t err = NULL;
+    char  wm_buf[4096];
+    char *conn_name;
+    struct whack_message wm1;
+    size_t outsize = 0;
+    size_t insize;
+
+    progname = argv[0];
+    leak_detective = 1;
+
+    if(argc != 1) {
+	fprintf(stderr, "Usage: %s .. \n", progname);
+	exit(10);
+    }
+    tool_init_log();
+
+    FILE *fin = fopen("wm04.bin", "rb");
+    if(fin==NULL) { perror("wm04"); exit(4); }
+    insize = fread(wm_buf, 1, sizeof(wm_buf), fin);
+
+    /* */
+    memset(&wm1, 0, sizeof(wm1));
+
+    /* should complain about missing magic tag */
+    err = whack_cbor_decode_msg(&wm1, wm_buf+12, insize-12);
+    passert(err != NULL);
+
+    err = whack_cbor_decode_msg(&wm1, wm_buf, insize);
+    if(err) { printf("decode error: %s\n", err); exit(4); }
+
+    /* encode it again, and write it out */
+    err = whack_cbor_encode_msg(&wm1, wm_buf, &insize);
+    if(err) { printf("recode: error: %s\n", err); exit(5); }
+
+    FILE *omsg = fopen("OUTPUT/wm05o.bin", "wb");
+    if(omsg == NULL) { perror("output"); exit(4); }
+    fwrite(wm_buf, insize, 1, omsg);
+    fclose(omsg);
+
+    report_leaks();
+
+    tool_close_log();
+    exit(0);
+}
+
+
+/*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
