94207cacfa2655f60662ec480a88f9a042149f7e
Michael Richardson <mcr@sandelman.ca>
2021-04-11 18:25:49 -0400

    lp123 --- send authentication payload along with certificate

---
+++ tests/unit/libpluto/lp123-peerA-I1/peerA-I2.c
@@ -0,0 +1,49 @@
+#include "../lp10-parentI2/parentI2_head.c"
+#include "seam_gi_sha1.c"
+#include "seam_gi_sha256_group14.c"
+#include "seam_finish.c"
+#include "seam_ikev2_sendI1.c"
+#include "seam_x509_list.c"
+#include "seam_host_peerA.c"
+#include "seam_natt.c"
+#include "seam_rsasig.c"
+
+#define TESTNAME "peerA-RI"
+
+static void init_local_interface(void)
+{
+    init_peerA_interface();
+}
+
+static void init_fake_secrets(void)
+{
+    prompt_pass_t pass;
+    memset(&pass, 0, sizeof(pass));
+    osw_init_ipsecdir_str("../samples/davecert");
+
+    rnd_offset = 13;
+
+    osw_load_preshared_secrets(&pluto_secrets
+			       , TRUE
+			       , "../samples/davecert.secrets"
+			       , &pass, NULL);
+}
+
+static void init_loaded(void)
+{
+    //struct connection *c;
+
+    /* loading X.509 CA certificates */
+    load_authcerts("CA cert", oco->cacerts_dir, AUTH_CA);
+}
+
+#include "seam_parentI2.c"
+#include "../lp10-parentI2/parentI2_main.c"
+
+ /*
+ * Local Variables:
+ * c-style: pluto
+ * c-basic-offset: 4
+ * compile-command: "make check"
+ * End:
+ */
