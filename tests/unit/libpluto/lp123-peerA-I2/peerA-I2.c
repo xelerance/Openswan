4b4d813cf73ca3ff78598d673f908e5fa8dd8924
Michael Richardson <mcr@sandelman.ca>
2021-04-15 10:49:15 -0400

    change responder to use inverted policy

--- tests/unit/libpluto/lp123-peerA-I2/peerA-I2.c
+++ tests/unit/libpluto/lp123-peerA-I2/peerA-I2.c
@@ -29,12 +29,26 @@ static void init_fake_secrets(void)
 			       , &pass, NULL);
 }
 
-static void init_loaded(void)
-{
-    //struct connection *c;
+static void init_loaded(void) {
+    cur_debugging |= DBG_X509;
+
+    oco = osw_init_options();
 
     /* loading X.509 CA certificates */
     load_authcerts("CA cert", oco->cacerts_dir, AUTH_CA);
+    /* loading X.509 AA certificates */
+    load_authcerts("AA cert", oco->aacerts_dir, AUTH_AA);
+    /* loading X.509 OCSP certificates */
+    load_authcerts("OCSP cert", oco->ocspcerts_dir, AUTH_OCSP);
+
+    /* loading X.509 CRLs */
+    load_crls();
+    /* loading attribute certificates (experimental) */
+    load_acerts();
+
+    list_certs(1);
+
+    list_authcerts("CA", AUTH_CA, 1);
 }
 
 #include "seam_parentI2.c"
--- tests/unit/libpluto/lp124-peerB-R2/peerB-R2.c
+++ tests/unit/libpluto/lp124-peerB-R2/peerB-R2.c
@@ -24,7 +24,28 @@ static void init_fake_secrets(void)
 			       , "../samples/carol.secrets"
 			       , &pass, NULL);
 }
-static void init_loaded(void) {}
+
+static void init_loaded(void) {
+    cur_debugging |= DBG_X509;
+
+    oco = osw_init_options();
+
+    /* loading X.509 CA certificates */
+    load_authcerts("CA cert", oco->cacerts_dir, AUTH_CA);
+    /* loading X.509 AA certificates */
+    load_authcerts("AA cert", oco->aacerts_dir, AUTH_AA);
+    /* loading X.509 OCSP certificates */
+    load_authcerts("OCSP cert", oco->ocspcerts_dir, AUTH_OCSP);
+
+    /* loading X.509 CRLs */
+    load_crls();
+    /* loading attribute certificates (experimental) */
+    load_acerts();
+
+    list_certs(1);
+
+    list_authcerts("CA", AUTH_CA, 1);
+}
 
 #include "../lp12-parentR2/parentR2_main.c"
 
